a18670dfa701907ffd2040657b60f4a7cda4e26d
Ammon Smith <ammon.i.smith@gmail.com>
2019-04-01 20:11:02 -0400

    Let main executable accept multiple files.

--- ftml/src/main.rs
+++ ftml/src/main.rs
@@ -25,36 +25,91 @@ extern crate clap;
 extern crate wikidot_html;
 
 use clap::{App, Arg};
-use std::fs::File;
-use std::io::{self, Read};
+use std::fs::{self, File};
+use std::io::{self, Read, Write};
+use std::path::{Path, PathBuf};
+use std::process;
 use wikidot_html::prelude::*;
 
-fn main() -> Result<()> {
+fn main() {
     let matches = App::new("Wikidot to HTML")
         .version(env!("CARGO_PKG_VERSION"))
         .author("Ammon Smith")
         .about("Utility to convert Wikidot code into HTML")
         .max_term_width(110)
         .arg(
+            Arg::with_name("output-directory")
+                .short("d")
+                .long("directory")
+                .default_value(".")
+                .help("Specify an output directory to place rendered files in. Defaults to the current directory."),
+        )
+        .arg(
             Arg::with_name("FILE")
+                .multiple(true)
                 .required(true)
                 .help("Input file for the program. Set to \"-\" for stdin."),
         )
         .get_matches();
 
-    let text = {
-        let mut contents = String::new();
-        let mut file: Box<io::Read> = match matches.value_of("FILE").unwrap() {
-            "-" => Box::new(io::stdin()),
-            path => Box::new(File::open(path)?),
+    let output_dir = matches.value_of_os("output-directory").unwrap();
+    if let Err(err) = fs::create_dir_all(&output_dir) {
+        let output_dir = Path::new(output_dir);
+        eprintln!("Error creating directories for \"{}\": {}", output_dir.display(), &err);
+        process::exit(1);
+    }
+
+    for in_path in matches.values_of_os("FILE").unwrap() {
+        if in_path == "-" {
+            if let Err(err) = process_stdin() {
+                eprintln!("Error transforming from stdin: {}", &err);
+            }
+
+            process::exit(1);
+        }
+
+        let in_path = Path::new(in_path);
+        let out_path = match in_path.file_stem() {
+            Some(stem) => {
+                let mut path = PathBuf::from(output_dir);
+                path.push(stem);
+                path.set_extension("html");
+                path
+            },
+            None => {
+                eprintln!("Path \"{}\" does not refer to a file", in_path.display());
+                process::exit(1);
+            },
         };
 
+        if let Err(err) = process_file(in_path, &out_path) {
+            eprintln!("Error transforming \"{}\": {}", in_path.display(), &err);
+        }
+    }
+}
+
+fn process_file(in_path: &Path, out_path: &Path) -> Result<()> {
+    let text = {
+        let mut contents = String::new();
+        let mut file = File::open(in_path)?;
         file.read_to_string(&mut contents)?;
         contents
     };
 
-    let html = transform(text.as_str())?;
-    println!("{}", &html);
+    let html = transform(text)?;
+    let mut file = File::create(out_path)?;
+    file.write_all(html.as_bytes())?;
+    Ok(())
+}
 
+fn process_stdin() -> Result<()> {
+    let text = {
+        let mut contents = String::new();
+        io::stdin().read_to_string(&mut contents)?;
+        contents
+    };
+
+    let html = transform(text)?;
+    println!("{}", &html);
     Ok(())
 }
