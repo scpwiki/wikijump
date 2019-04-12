f906101e0529ae40d119a6d35744911a8499555d
Ammon Smith <ammon.i.smith@gmail.com>
2019-04-11 23:52:34 -0400

    Add more test strings.

--- ftml/src/parse/test.rs
+++ ftml/src/parse/test.rs
@@ -23,9 +23,11 @@ use super::{Rule, WikidotParser};
 
 #[test]
 fn test_strings() {
-    const INPUT_STRINGS: [&str; 10] = [
+    const INPUT_STRINGS: [&str; 22] = [
         "@@ apple @@ @@banana@@",
         "@@ [!-- literal comment @@ durian",
+        "@@@@@@ at signs `````` tildes",
+        "apple `` legacy raw @@ `` banana",
         "[!-- [[ footnote invalid formatting in here-- [[ eref --] test",
         "__**test** cherry {{ durian (?) }}__ ^^up!^^",
         "** [[date 0]] **",
@@ -34,10 +36,22 @@ fn test_strings() {
         "--[[*user rounderhouse]] [[# test-anchor ]]-- [[ eref equation_id ]]",
         "[[ image tree.png link = \"https://example.com\" alt=\"A tree.\" class=\"image-block\"  ]]",
         "__**--^^,,{{super formatted}},,^^--**__",
+        "//[[date -100]] number// [[footnote]]Content **cherry** [[*user aismallard]][[/footnote]] [[footnote]]Content **cherry** [[*user aismallard]][[/footnote]]",
+        "apple\n[[module Rate]]\nbanana",
+        "apple\n[[module CSS]]\n@import url('https://example.com/style.css');\ndiv.container { display: none; }\n[[/module]]\nbanana",
+        "[[form]]\nform data here\nmore stuff\n[[/form]]",
+        "[[note]]\nnote internal information here\napple\ndurian\nbanana\n[[/note]]",
+        "apple\n[[note]]\ninternal\n[[/note]]\nbanana",
+        "^^**alpha** beta ,,gama,,^^",
+        "= {{apple}} banana",
+        "++ header\n+++ apple __banana__\n++++ @@ RAW @@\ndurian",
+        "internal [[# anchor-name]] [[date 1000]] **apple** _",
     ];
 
     for string in &INPUT_STRINGS[..] {
-        println!("Parse test: {}", string);
-        let _ = WikidotParser::parse(Rule::page, string).unwrap();
+        println!("Testing parser: {:?}", string);
+        if let Err(err) = WikidotParser::parse(Rule::page, string) {
+            panic!("Failed to parse test string:\n{}\nProduced error: {}", string, err);
+        }
     }
 }
