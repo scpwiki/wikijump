4b396a0221355bc21f28c5b997737ca519cc5fa4
Ammon Smith <ammon.i.smith@gmail.com>
2019-06-02 22:50:56 -0400

    Adjust macros.rs

--- ftml/src/macros.rs
+++ ftml/src/macros.rs
@@ -4,7 +4,8 @@
  * ftml - Convert Wikidot code to HTML
  * Copyright (C) 2019 Ammon Smith for Project Foundation
  *
- * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
