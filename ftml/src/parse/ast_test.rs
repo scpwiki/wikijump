a3d45bc167386cf02449416a66207e03a98ba504
Ammon Smith <ammon.i.smith@gmail.com>
2019-09-17 13:39:06 -0400

    Move invalid parse tests.

--- ftml/src/parse/ast_test.rs
+++ ftml/src/parse/ast_test.rs
@@ -72,4 +72,78 @@ fn test_invalid() {
     invalid!("]]]");
 
     invalid!("[!-- alpha --] [[ eref ");
+
+    invalid!("@@ raw value");
+    invalid!("@@ @@ @@");
+    invalid!("@@ raw \n multiline @@");
+    invalid!("apple @@ banana");
+    invalid!("__**test** cherry {{ durian ^^up^^ __");
+    invalid!("[[ unknown block ]]");
+    invalid!("[[ ]]");
+    invalid!("kiwi [[date 0]");
+    invalid!("kiwi [[ date 0 ] ]");
+
+    invalid!("[[span id=\"a\"]] [[span id=\"b\"]] incomplete span [[/span]]");
+    invalid!("[[ * user rounder house ]]");
+    invalid!("[[ user rounder house ]]");
+    invalid!("[[module CustomMod bad_argument=\"value ]]");
+    invalid!("[[module CustomMod bad_argument=value ]]");
+    invalid!("[[module CustomMod]] [[/module]]");
+    invalid!("[[image filename_with_a_space in_it.jpeg]]");
+    invalid!("[[==image filename.png]]");
+    invalid!("[[f=image filename.png]]");
+
+    invalid!("// Incomplete italics");
+    invalid!("** Incomplete bold");
+    invalid!("__ Incomplete underline");
+    invalid!("^^ Incomplete superscript");
+    invalid!(",, Incomplete subscript");
+    invalid!("---- Empty strikethrough"); // Conflicts with horiz separator
+    invalid!("##NOT&A&COLOR|test##");
+
+    invalid!("[[footnote]]");
+    invalid!("[[>]]\nPINEAPPLE");
+    invalid!("[[<]]\nPINEAPPLE");
+    invalid!("[[=]]\nPINEAPPLE");
+    invalid!("[[==]]\nPINEAPPLE");
+    invalid!("[[>]]\nCHERRY\n[[/<]]");
+    invalid!("[[<]]\nCHERRY\n[[/>]]");
+    invalid!("[[=]]\nCHERRY\n[[/==]]");
+    invalid!("[[==]]\nCHERRY\n[[/=]]");
+
+    invalid!("[[date 1000 invalid_arg=\"test\"]]");
+    invalid!("[[span invalid_arg=\"test\"]]apple[[/span]]");
+    invalid!("[[user rounderhouse invalid_arg=\"test\"]]");
+    invalid!("[[image director-sharp.jpeg invalid_arg=\"test\"]]");
+    invalid!("[[code invalid_arg=\"test\"]]\napple\n[[/code]]");
+    invalid!("[[div invalid_arg=\"test\"]][[/div]]");
+    invalid!("an [[div]] inline div [[/div]]");
+
+    invalid!("[[F>image filename.png]]");
+    invalid!("[[F<image filename.png]]");
+    invalid!("[[tablist]]");
+    invalid!("[[tablist]] [[tab A]] [[/tablist]]");
+    invalid!("[[tabview]] [[/tab]] [[/tabview]]");
+    invalid!("[[gallery]] contents [[/gallery]]");
+
+    invalid!("+++++++ h7 heading doesn't exist");
+    invalid!("[[quote]] inline [[/quote]]");
+    // Split and disjoint groups are explicitly unsupported
+    invalid!("[[div]]\n[[quote]]\ncontents\n[[/div]]\n[[/quote]]");
+    invalid!("[[div]]\n> contents\n> [[/div]]");
+    invalid!("[[js]]");
+    invalid!("[[javascript]]");
+
+    invalid!("[[# false anchor]");
+    invalid!("[# false empty link]]");
+    invalid!("[[a herf=\"https://example.com/\"]]link[[/a]]");
+    invalid!("[[# anchor-name-with-|-bad-ident]]");
+    invalid!("[[=]]\n[[collapsible]]\n[[/=]]\n[[/collapsible]]\n");
+    invalid!("hello [[html]] world\n");
+    invalid!("hello [[iframe https://example.com]] world\n");
+    invalid!("[[html]]");
+    invalid!("[[iframe]]");
+    invalid!("[[iframe https://example.com]]\ncontents\n[[/iframe]]\n");
+    invalid!("[[css]]");
+    invalid!("[[style]]");
 }
