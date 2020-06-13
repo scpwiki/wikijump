86db7bfb0646c62c8be95c25e8081fbbfe5946b5
pxdnbluesoul <github@nags.me>
2020-06-13 16:37:02 +0000

    Streamline workflow down to a build override.

--- php/modules/newmodules/css/CSSModule.php
+++ php/modules/newmodules/css/CSSModule.php
@@ -12,45 +12,9 @@ class CSSModule extends SmartyModule
 
     public function build($runData)
     {
-    }
-
-    public function render($runData)
-    {
         $pl = $runData->getParameterList();
         $this->stylesheet = $pl->getParameterValue("module_body");
 
-        if($runData->getModuleTemplate() == null){return;}
-
-        $this->build($runData);
-
-        $template = $runData->getModuleTemplate();
-        $templateFile  = PathManager::moduleTemplate($template);
-        // render!
-
-        $smarty = Ozone::getSmartyPlain();
-
-        $page = $runData->getPage();
-        $smarty->assign("page", $page);
-
-        // put context into context
-
-        $context = $runData->getContext();
-        if($context !== null){
-            foreach($context as $key => $value){
-                $smarty->assign($key, $value);
-            }
-        }
-
-        // put errorMessages and messages into the smarty's context as well.
-        $dataMessages = $runData->getMessages();
-        $dataErrorMessages = $runData->getErrorMessages();
-        if(count($dataMessages) > 0) {
-            $smarty->assign('data_messages', $dataMessages);
-        }
-
-        if(count($dataErrorMessages) > 0) {
-            $smarty->assign('data_errorMessages', $dataErrorMessages);
-        }
         $csstidy = new csstidy();
         $csstidy->set_cfg('preserve_css', true);
         $csstidy->parse($this->stylesheet);
@@ -58,10 +22,6 @@ class CSSModule extends SmartyModule
 
         $this->stylesheet = $csstidy->print->output_css_plain;
 
-        $smarty->assign('stylesheet', $this->stylesheet);
-        $out = $smarty->fetch($templateFile);
-
-        return $out;
-
+        $runData->contextAdd('stylesheet', $this->stylesheet);
     }
 }
