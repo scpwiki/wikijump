64a595e6f59d29dc90fe2acde7ec0cc0710c4bbe
Ammon Smith <ammon.i.smith@gmail.com>
2020-07-01 12:16:43 -0400

    Create parse/ahead.rs

---
+++ ftml/src/parse/ahead.rs
@@ -0,0 +1,29 @@
+/*
+ * parse/ahead.rs
+ *
+ * ftml - Library to parse Wikidot code
+ * Copyright (C) 2019-2020 Ammon Smith
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+//! Module for look-ahead checking.
+//!
+//! This contains implementations of eager functions that try to interpret the
+//! upcoming tokens as a particular object (e.g. seeing a `[[` and you see if it's a module).
+//!
+//! The parser is not disambiguous because any string of tokens can be interpreted
+//! as raw text as a fallback, which is how Wikidot does it.
+
+// TODO
