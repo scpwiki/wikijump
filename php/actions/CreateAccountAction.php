1f3a6c079ccf79725c02d16fc79df583b2064363
ealevine <davidwallack1@gmail.com>
2020-07-12 15:53:11 -0500

    Update CreateAccountAction.php

--- php/actions/CreateAccountAction.php
+++ php/actions/CreateAccountAction.php
@@ -219,7 +219,7 @@ class CreateAccountAction extends SmartyAction
         $oe->setBodyTemplate('RegistrationEmailVerification');
 
         if (!$oe->Send()) {
-            throw new ProcessException(_("The email can not be sent to this address."), "email_failed");
+            throw new ProcessException(_("The email cannot be sent to this address."), "email_failed");
         }
         $runData->sessionAdd('evcode', $evcode);
     }
@@ -251,7 +251,7 @@ class CreateAccountAction extends SmartyAction
         $oe->setBodyTemplate('RegistrationEmailVerification');
 
         if (!$oe->Send()) {
-            throw new ProcessException(_("The email can not be sent to this address."), "email_failed");
+            throw new ProcessException(_("The email cannot be sent to this address."), "email_failed");
         }
         $runData->sessionAdd('evcode', $evcode);
     }
