72005c1d06b10f8adc5f8846ae7936b29c34ff5c
bluesoul <atlassian@nags.me>
2020-07-19 11:57:56 -0600

    Remove docblocks.

--- php/actions/ManageUsersAction.php
+++ php/actions/ManageUsersAction.php
@@ -17,11 +17,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * @category Wikidot
- * @package Wikidot
- * @version $Id$
- * @copyright Copyright (c) 2008-2020, Wikidot Inc., SCP Wiki Technical Team
- * @license http://www.gnu.org/licenses/agpl-3.0.html GNU Affero General Public License
- */
+
+
 
 
 use DB\OzoneUserPeer;
