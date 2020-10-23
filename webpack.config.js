ddb0925494ad1f9d369a29767132e342f94b1945
Ross Williams <rossjrw@gmail.com>
2020-10-23 18:33:52 +0100

    Remove more unnecessary info from package

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './web/files--common/javascript/index.ts',
+  output: {
+    filename: 'bundle.[contenthash].js',
+    path: path.resolve(__dirname, 'web/files--common/javascript/dist')
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+    alias: {
+      '@': path.resolve(__dirname, 'web/files--common')
+    }
+  },
+  plugins: [
+    new CleanWebpackPlugin()
+  ]
+}
