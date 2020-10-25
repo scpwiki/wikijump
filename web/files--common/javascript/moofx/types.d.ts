683b2aaffd38e82eb1768aa1aff8927aa63320ea
Ross Williams <rossjrw@gmail.com>
2020-10-25 05:06:02 +0000

    Add temporary types to Moo FX and Yahoo UI

---
+++ web/files--common/javascript/moofx/types.d.ts
@@ -0,0 +1,5 @@
+// This is a quick, temporary declaration for Moo FX.
+// It is not type-safe.
+
+declare const fx: any;
+export default fx;
---
+++ web/files--common/javascript/yahooui/types.d.ts
@@ -0,0 +1,5 @@
+// This is a quick, temporary declaration for the YAHOO UI library.
+// It is not type-safe.
+
+declare const YAHOO: any;
+export default YAHOO;
