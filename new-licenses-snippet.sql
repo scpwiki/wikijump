bfe629ae2dc4a9c5da298dc95e79432949eeadbf
Ammon Smith <ammon.i.smith@gmail.com>
2020-11-10 13:26:58 -0500

    Add initial license SQL.

---
+++ new-licenses-snippet.sql
@@ -0,0 +1,14 @@
+--
+-- Data for Name: license; Type: TABLE DATA; Schema: public; Owner: wd
+--
+
+COPY public.license (license_id, name, description, sort) FROM stdin;
+1	Creative Commons Attribution-ShareAlike 3.0 License (recommended)	%%UNLESS%% <a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/">Creative Commons Attribution-ShareAlike 3.0 License</a>	1
+2	Creative Commons Attribution 3.0 License	%%UNLESS%% <a rel="license" href="http://creativecommons.org/licenses/by/3.0/">Creative Commons Attribution 3.0 License</a>	2
+3	Creative Commons Attribution-NoDerivs 3.0 License	%%UNLESS%% <a rel="license" href="http://creativecommons.org/licenses/by-nd/3.0/">Creative Commons Attribution-NoDerivs 3.0 License</a>	3
+4	Creative Commons Attribution-NonCommercial 3.0 License	%%UNLESS%% <a rel="license" href="http://creativecommons.org/licenses/by-nc/3.0/">Creative Commons Attribution-NonCommercial 3.0 License</a>	4
+5	Creative Commons Attribution-NonCommercial-ShareAlike 3.0 License	%%UNLESS%% <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/">Creative Commons Attribution-NonCommercial-ShareAlike 3.0 License</a>	5
+6	Creative Commons Attribution-NonCommercial-NoDerivs 3.0 License	%%UNLESS%% <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/3.0/">Creative Commons Attribution-NonCommercial-NoDerivs 3.0 License</a>	6
+7	GNU Free Documentation License 1.2	%%UNLESS%%  \n<a href="http://www.gnu.org/copyleft/fdl.html" title="http://www.gnu.org/copyleft/fdl.html" rel="license">GNU \nFree Documentation License</a>.	100
+8	Standard copyright (not recommended)	\N	1000
+\.
