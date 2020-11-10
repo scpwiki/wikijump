05c55b53906e33093f67cedf5a9c4d0b355c592e
Ammon Smith <ammon.i.smith@gmail.com>
2020-11-10 13:27:47 -0500

    Add new licenses.

--- new-licenses-snippet.sql
+++ new-licenses-snippet.sql
@@ -1,14 +1,21 @@
 --
--- Data for Name: license; Type: TABLE DATA; Schema: public; Owner: wd
+-- Data for Name: license; Type: TABLE DATA; Schema: public; Owner: wikijump
 --
 
 COPY public.license (license_id, name, description, sort) FROM stdin;
-1	Creative Commons Attribution-ShareAlike 3.0 License (recommended)	%%UNLESS%% <a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/">Creative Commons Attribution-ShareAlike 3.0 License</a>	1
-2	Creative Commons Attribution 3.0 License	%%UNLESS%% <a rel="license" href="http://creativecommons.org/licenses/by/3.0/">Creative Commons Attribution 3.0 License</a>	2
-3	Creative Commons Attribution-NoDerivs 3.0 License	%%UNLESS%% <a rel="license" href="http://creativecommons.org/licenses/by-nd/3.0/">Creative Commons Attribution-NoDerivs 3.0 License</a>	3
-4	Creative Commons Attribution-NonCommercial 3.0 License	%%UNLESS%% <a rel="license" href="http://creativecommons.org/licenses/by-nc/3.0/">Creative Commons Attribution-NonCommercial 3.0 License</a>	4
-5	Creative Commons Attribution-NonCommercial-ShareAlike 3.0 License	%%UNLESS%% <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/">Creative Commons Attribution-NonCommercial-ShareAlike 3.0 License</a>	5
-6	Creative Commons Attribution-NonCommercial-NoDerivs 3.0 License	%%UNLESS%% <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/3.0/">Creative Commons Attribution-NonCommercial-NoDerivs 3.0 License</a>	6
-7	GNU Free Documentation License 1.2	%%UNLESS%%  \n<a href="http://www.gnu.org/copyleft/fdl.html" title="http://www.gnu.org/copyleft/fdl.html" rel="license">GNU \nFree Documentation License</a>.	100
-8	Standard copyright (not recommended)	\N	1000
+1	Creative Commons Attribution-ShareAlike 4.0 License (recommended)	%%UNLESS%% <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 3.0 License</a>	1
+2	Creative Commons Attribution 4.0 License	%%UNLESS%% <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 License</a>	2
+3	Creative Commons Attribution-NoDerivs 4.0 License	%%UNLESS%% <a rel="license" href="http://creativecommons.org/licenses/by-nd/4.0/">Creative Commons Attribution-NoDerivs 4.0 License</a>	3
+4	Creative Commons Attribution-NonCommercial 4.0 License	%%UNLESS%% <a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/">Creative Commons Attribution-NonCommercial 4.0 License</a>	4
+5	Creative Commons Attribution-NonCommercial-ShareAlike 4.0 License	%%UNLESS%% <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons Attribution-NonCommercial-ShareAlike 4.0 License</a>	5
+6	Creative Commons Attribution-NonCommercial-NoDerivs 4.0 License	%%UNLESS%% <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/4.0/">Creative Commons Attribution-NonCommercial-NoDerivs 4.0 License</a>	6
+7	Creative Commons Attribution-ShareAlike 3.0 License	%%UNLESS%% <a rel="license" href="http://creativecommons.org/licenses/by-sa/3.0/">Creative Commons Attribution-ShareAlike 3.0 License</a>	7
+8	Creative Commons Attribution 3.0 License	%%UNLESS%% <a rel="license" href="http://creativecommons.org/licenses/by/3.0/">Creative Commons Attribution 3.0 License</a>	8
+9	Creative Commons Attribution-NoDerivs 3.0 License	%%UNLESS%% <a rel="license" href="http://creativecommons.org/licenses/by-nd/3.0/">Creative Commons Attribution-NoDerivs 3.0 License</a>	9
+10	Creative Commons Attribution-NonCommercial 3.0 License	%%UNLESS%% <a rel="license" href="http://creativecommons.org/licenses/by-nc/3.0/">Creative Commons Attribution-NonCommercial 3.0 License</a>	10
+11	Creative Commons Attribution-NonCommercial-ShareAlike 3.0 License	%%UNLESS%% <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/">Creative Commons Attribution-NonCommercial-ShareAlike 3.0 License</a>	11
+12	Creative Commons Attribution-NonCommercial-NoDerivs 3.0 License	%%UNLESS%% <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/3.0/">Creative Commons Attribution-NonCommercial-NoDerivs 3.0 License</a>	12
+13	CC0 (Public Domain)	%%UNLESS%%  \n<a rel="license" href="https://creativecommons.org/publicdomain/zero/1.0/">CC0 (Public Domain)</a>.	100
+14	GNU Free Documentation License 1.3	%%UNLESS%%  \n<a rel="license" href="http://www.gnu.org/copyleft/fdl.html">GNU \nFree Documentation License</a>.	101
+15	Standard copyright (not recommended)	\N	1000
 \.
