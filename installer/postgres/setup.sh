6c62a5a3815b229d32fbe2493c67b343f2c04e72
Ammon Smith <ammon.i.smith@gmail.com>
2020-11-16 00:17:20 -0500

    Add createdb to setup script.

--- installer/postgres/setup.sh
+++ installer/postgres/setup.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 set -eux
 
+# Create postgres cluster
+pg_createcluster -u postgres 12 main
+
+# Start service
 service postgresql start
+
+# Create postgres database
+createdb \
+	--echo
+	--encoding='UTF-8' \
+	--owner=wikijump \
+	wikijump \
+	'Wikijump system database'
+
+# Ingest database seed
 su postgres -c 'psql < ingest.sql'
