61fa89175efc4ef2f57ab308873e853b8dba6fc3
Ross Williams <rossjrw@gmail.com>
2020-11-16 22:40:16 +0000

    Add a touch of documenting to typing utility

--- web/bin/utilities/jsts.sh
+++ web/bin/utilities/jsts.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+# Sets a groundwork for converting a module JS to TS
+# TODO Delete after modules conversion
 standardx --fix $1
 (printf "import Wikijump from \"@/javascript/Wikijump\";\nimport OZONE from \"@/javascript/OZONE\";\ndeclare const YAHOO: any;\ndeclare const fx: any;\n"; cat $1) > ${1%.js}.ts
-nvim -O ${1%.js}.ts $1
+$EDITOR -O ${1%.js}.ts $1
