f8b60405e82c579bd3d0952c2b6755fc93d72a86
bluesoul <atlassian@nags.me>
2020-11-19 19:33:26 -0700

    ope

--- infra/terraform/prod/rds.tf
+++ infra/terraform/prod/rds.tf
@@ -18,5 +18,5 @@ resource "aws_rds_cluster" "wikijump_aurora" {
 
 resource "aws_db_subnet_group" "wikijump_aurora_subnet" {
   name       = "wikijump-aurora-${var.environment}"
-  subnet_ids = [aws_subnet.database_subnet.id]
+  subnet_ids = [aws_subnet.database_subnet_a.id, aws_subnet.database_subnet_b.id]
 }
