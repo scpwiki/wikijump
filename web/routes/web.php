3a008fb19ebf388f704a1ab32ec6dc35e8fca1c0
bluesoul <atlassian@nags.me>
2020-11-20 21:51:40 -0700

    Working config

--- web/routes/web.php
+++ web/routes/web.php
@@ -12,7 +12,8 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('wikijump')->group(function () {
+    Route::get('/', function () {
+        return view('welcome');
+    });
 });
