7bf0891147571a19521caefc4b19be3eabbde718
bluesoul <atlassian@nags.me>
2020-11-24 15:05:50 -0700

    Skip S3 origin in Cloudfront for now

--- infra/terraform/prod/cloudfront.tf
+++ infra/terraform/prod/cloudfront.tf
@@ -14,10 +14,10 @@ resource "aws_cloudfront_distribution" "wikijump_cf_distro" {
         }
     }
 
-    origin {
-      domain_name = aws_s3_bucket.wikijump_assets.bucket_domain_name
-      origin_id = "wikijump_s3"
-    }
+    # origin {
+    #   domain_name = aws_s3_bucket.wikijump_assets.bucket_domain_name
+    #   origin_id = "wikijump_s3"
+    # }
 
     restrictions {
       geo_restriction {
@@ -25,49 +25,49 @@ resource "aws_cloudfront_distribution" "wikijump_cf_distro" {
       }
     }
 
- ordered_cache_behavior {
-    path_pattern     = "/local--files/*"
-    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-    cached_methods   = ["GET", "HEAD", "OPTIONS"]
-    target_origin_id = "wikijump_s3"
+#  ordered_cache_behavior {
+#     path_pattern     = "/local--files/*"
+#     allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+#     cached_methods   = ["GET", "HEAD", "OPTIONS"]
+#     target_origin_id = "wikijump_s3"
 
-    forwarded_values {
-      query_string = false
-      headers      = ["Origin"]
+#     forwarded_values {
+#       query_string = false
+#       headers      = ["Origin"]
 
-      cookies {
-        forward = "none"
-      }
-    }
+#       cookies {
+#         forward = "none"
+#       }
+#     }
 
-    min_ttl                = 0
-    default_ttl            = 86400
-    max_ttl                = 31536000
-    compress               = true
-    viewer_protocol_policy = "redirect-to-https"
-  }
+#     min_ttl                = 0
+#     default_ttl            = 86400
+#     max_ttl                = 31536000
+#     compress               = true
+#     viewer_protocol_policy = "redirect-to-https"
+#   }
 
-  # Cache behavior with precedence 1
-  ordered_cache_behavior {
-    path_pattern     = "/local--code/*"
-    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "wikijump_elb"
+#   # Cache behavior with precedence 1
+#   ordered_cache_behavior {
+#     path_pattern     = "/local--code/*"
+#     allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+#     cached_methods   = ["GET", "HEAD"]
+#     target_origin_id = "wikijump_elb"
 
-    forwarded_values {
-      query_string = false
+#     forwarded_values {
+#       query_string = false
 
-      cookies {
-        forward = "all"
-      }
-    }
+#       cookies {
+#         forward = "all"
+#       }
+#     }
 
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
-    compress               = true
-    viewer_protocol_policy = "redirect-to-https"
-  }
+#     min_ttl                = 0
+#     default_ttl            = 3600
+#     max_ttl                = 86400
+#     compress               = true
+#     viewer_protocol_policy = "redirect-to-https"
+#   }
 
    default_cache_behavior {
     allowed_methods         = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
