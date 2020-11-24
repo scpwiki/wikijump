d80e917ca07475361c7b23debfdfe798e361dc2b
bluesoul <atlassian@nags.me>
2020-11-24 12:17:19 -0700

    Cloudfront and ACM changes

--- infra/terraform/prod/cloudfront.tf
+++ infra/terraform/prod/cloudfront.tf
@@ -3,7 +3,7 @@ resource "aws_cloudfront_distribution" "wikijump_cf_distro" {
     is_ipv6_enabled         = true
     default_root_object     = "index.php"
 
-    aliases                 = [var.web_domain, var.files_domain]
+    aliases                 = [var.files_domain]
 
     origin {
         domain_name         = aws_lb.wikijump_elb.dns_name
@@ -14,31 +14,60 @@ resource "aws_cloudfront_distribution" "wikijump_cf_distro" {
         }
     }
 
+    origin {
+      domain_name = aws_s3_bucket.wikijump_assets.bucket_domain_name
+      origin_id = "wikijump_s3"
+    }
+
     restrictions {
       geo_restriction {
         restriction_type  = "none"
       }
     }
 
-    default_cache_behavior {
-    allowed_methods         = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
-    cached_methods          = ["GET", "HEAD"]
-    target_origin_id        = "wikijump_elb"
+ ordered_cache_behavior {
+    path_pattern     = "/local--files/*"
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id = "wikijump_s3"
 
     forwarded_values {
-      query_string          = true
+      query_string = false
+      headers      = ["Origin"]
 
       cookies {
-        forward             = "all"
+        forward = "none"
       }
     }
 
-    viewer_protocol_policy  = "redirect-to-https"
-    min_ttl                 = 0
-    default_ttl             = 60
-    compress                = true
-    max_ttl                 = 60
+    min_ttl                = 0
+    default_ttl            = 86400
+    max_ttl                = 31536000
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  # Cache behavior with precedence 1
+  ordered_cache_behavior {
+    path_pattern     = "/local--code/*"
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "wikijump_elb"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "all"
+      }
     }
+
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+  }
 
     viewer_certificate {
         acm_certificate_arn = aws_acm_certificate.cf_wildcard_cert.arn
--- infra/terraform/prod/s3.tf
+++ infra/terraform/prod/s3.tf
@@ -2,3 +2,8 @@ resource "aws_s3_bucket" "elb_logs" {
     bucket  = "wikijump-elb-logs-${var.environment}"
     acl     =  "log-delivery-write"
 }
+
+resource "aws_s3_bucket" "wikijump_assets" {
+    bucket = "wikijump-assets-${var.environment}"
+    acl = "public-read"
+}
