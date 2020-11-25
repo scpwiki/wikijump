f5eba442c09c3d7188e2dfb63818bcbc842ad320
Ammon Smith <ammon.i.smith@gmail.com>
2020-11-25 16:36:03 -0500

    Add password hash back.

--- install/postgres/seed.sql
+++ install/postgres/seed.sql
@@ -4176,7 +4176,7 @@ COPY public.ozone_session (session_id, started, last_accessed, ip_address, check
 COPY public.ozone_user (user_id, name, nick_name, password, email, unix_name, last_login, registered_date, super_admin, super_moderator, language) FROM stdin;
 -1	Automatic	Automatic	\N	automatic@wikidot	automatic	\N	\N	f	f	en
 0	Anonymous	Anonymous	\N	anonymous@wikidot	anonymous	\N	\N	f	f	en
-1	Administrator	Admin	\N	admin@wikidot	admin	\N	\N	t	f	en
+1	Administrator	Admin	$2y$11$lqdZZxU8FYqV2LJNaCQTGeI4V9dW/.cPd.DXpx4geF99bhzzpqQMy	admin@wikidot	admin	\N	\N	t	f	en
 \.
 
 
