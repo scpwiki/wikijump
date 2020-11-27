8c5f5b92432786d89537c87fcfaf346309b5da2d
Ross Williams <rossjrw@gmail.com>
2020-11-27 08:27:17 +0000

    Add more utilities to helper script

--- web/bin/utilities/jsts.sh
+++ web/bin/utilities/jsts.sh
@@ -1,30 +1,75 @@
 #!/bin/bash
+# vim: set noexpandtab:
 # Sets a groundwork for converting a module JS to TS
 # TODO Delete after modules conversion
 
 set -e
 
-standardx --fix $1 || true
+standardx --fix "$1" || true
 
-(printf "import Wikijump from \"@/javascript/Wikijump\";\nimport OZONE from \"@/javascript/OZONE\";\nimport { RequestModuleParameters } from \"@/javascript/OZONE/ajax\";\ndeclare const YAHOO: any;\ndeclare type YahooResponse = any;\ndeclare const fx: any;\n"; cat $1) > ${1%.js}.ts
+(
+	printf "import Wikijump from \"@/javascript/Wikijump\";\nimport OZONE from \"@/javascript/OZONE\";\nimport { RequestModuleParameters } from \"@/javascript/OZONE/ajax\";\ndeclare const YAHOO: any;\ndeclare type YahooResponse = any;\ndeclare const fx: any;\n";
+	cat "$1"
+) > "${1%.js}.ts"
 
-$EDITOR -s /dev/stdin <<<"
-:/Wikijump\\.modules\\./s!!//@ts-expect-error\rexport const !
-:%s/Wikijump\\.modules\\.${1%.js}/${1%.js}/g
-:%s/\\_s*};\\_s*${1%.js}\\.\\(\\S\\+\\) =/\r},\r  \1:/
-:/export const/s/{\n},/{/
-:%s/\$(\\([^)]\\+\\))/document.getElementById(\1)!/g
-:%s/\\<var\\>/let/g
-:%s/\\<p\\>/params/g
-:%s/let params =/let params: RequestModuleParameters =/g
-:%s/let params: RequestModuleParameters = null/let params: RequestModuleParameters = {}/g
-:%s/new Object()/{}/g
-:g/function/s/\\<e\\>/_event?: Event | null/
-:%s/\\<e\\>/event/g
-:g/function/s/\\<r\\>/response: YahooResponse/
-:%s/\\<r\\>/response/g
-:g/function/s/) {/): void {/
-:%s/setTimeout(\\(['\"]\\)\\(.*\\)\1/setTimeout(() => \2/
-" ${1%.js}.ts
+$EDITOR -s /dev/stdin <<<-"
+	:\" Expect an error until syntax issues are fixed
+	:/Wikijump\\.modules\\./s!!//@ts-expect-error\rexport const !
+
+	:\" No need to absolute-reference the current module
+	:%s/Wikijump\\.modules\\.${1%.js}/${1%.js}/g
+
+	:\" Merge individual module sections into a single object definition
+	:%s/\\_s*};\\_s*${1%.js}\\.\\(\\S\\+\\) =/\r},\r  \1:/
+	:\" Remove an extra closing brace at the start of the module (rebalance later)
+	:/export const/s/{\n},/{/
+
+	:\" Replace the jQuery ID wrapper with a builtin
+	:%s/\$(\\([^)]\\+\\))/document.getElementById(\1)!/g
+
+	:\" 'p' by itself is shorthand for module request params
+	:%s/\\<p\\>/params/g
+	:%s/new Object()/{}/g
+	:%s/var params =/var params: RequestModuleParameters =/
+	:%s/var params: RequestModuleParameters = null/var params: RequestModuleParameters = {}/
+
+	:\" Recursively move individual parameters into a single object definition
+	:%s/^\\(\\s*\\)params\\.\\(\\S\\+\\) = \\(.*\\);/\\1  \\2: \\3,/
+	qqqqq:%s/^\\(\\s*\\)\\(.*\\)};\\n\\1\\(  \\S*: .*,\\)/\\1\\2\\r\\1\\3\\r\\1};/@qq@q
+	:%s/,\\n\\s*\\n/,\\r/
+
+	:\" 'e' by itself is short for event, but it's usually unused
+	:g/function/s/\\<e\\>,/_event: Event | null,/
+	:g/function/s/\\<e\\>/_event?: Event | null/
+	:%s/\\<e\\>/event/g
+
+	:\" 'r' by itself is short for response
+	:g/function/s/\\<r\\>/response: YahooResponse/
+	:%s/\\<r\\>/response/g
+
+	:\" The vast majority of functions return void, others can be manually set
+	:g/function/s/) {/): void {/
+
+	:\" Wikidot loves passing eval strings to setTimeout
+	:%s/setTimeout(\\(['\"]\\)\\(.*\\)\\1/setTimeout(() => \\2/
+
+	:\" I changed requestModule to not accept null but an empty object
+	:%s/OZONE\\.ajax\\.requestModule([^,]*, \\zsnull/{}/
+
+	:\" Rebalance the brackets by adding one to the end
+	gg?};
+	:s/};/}\\r};/
+
+	:\" Remove trailing commas
+	:%s/,\\(\\_s*}\\)/\1/
+
+	:\" Autoindent
+	G=gg
+
+	:\" Update var to let & const
+	:%s/\\<var\\>/let/g
+	:\" Queue up (and immediately cancel) a manual let->const replace
+	:%s/\\<let\\>/const/c
+	" "${1%.js}.ts"
 # Exit editor with error (:cq) to block deletion
-rm $1
+rm "$1"
