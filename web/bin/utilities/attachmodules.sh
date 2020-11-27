56f8b1271c779cff02996228ef17748ccbf53ef4
Ross Williams <rossjrw@gmail.com>
2020-11-27 17:19:26 +0000

    Add a helper script for compiling index.ts

---
+++ web/bin/utilities/attachmodules.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# vim: set noexpandtab:
+# Adds modules from the given directory to index.ts, recursively
+# TODO Delete after modules conversion
+
+set -e
+
+INDEX="index.ts"
+
+if [ ! -f $INDEX ]; then
+    echo "no index!"
+    exit 1
+fi
+
+find "$1" -type f -print0 | while IFS= read -r -d '' file; do
+    echo "import { ### } from \"./$file\";" >> $INDEX
+    echo "!!!  $file" >> $INDEX
+done
+
+$EDITOR -s /dev/stdin <<<"
+qqqqqgg?###
+ddggPf.;hvbyF#viwp:s/\\.ts//
+@qq@q:%s;!!!.*/\\([A-z]*\\)\\.ts;  \\1,;
+" $INDEX
