e5e860d3e9c6e512c428753abf9259ede1a464e0
Ammon Smith <ammon.i.smith@gmail.com>
2020-11-30 00:22:34 -0500

    Add serialization to ParseResult.
    If type T is also serializable.

--- ftml/src/parse/result.rs
+++ ftml/src/parse/result.rs
@@ -19,6 +19,7 @@
  */
 
 use super::ParseError;
+use serde::{Serialize, Serializer};
 use std::borrow::{Borrow, BorrowMut};
 
 #[derive(Debug)]
@@ -113,5 +114,23 @@ impl<T> Into<(T, Vec<ParseError>)> for ParseResult<T> {
         let ParseResult { value, errors } = self;
 
         (value, errors)
+    }
+}
+
+impl<T> Serialize for ParseResult<T>
+where
+    T: Serialize,
+{
+    #[inline]
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        use serde::ser::SerializeStruct;
+
+        let mut obj = serializer.serialize_struct("ParseResult", 2)?;
+        obj.serialize_field("value", &self.value)?;
+        obj.serialize_field("errors", &self.errors)?;
+        obj.end()
     }
 }
