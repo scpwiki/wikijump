01948944f13ca95fece7e3c309497daab6c46e1b
Ammon Smith <ammon.i.smith@gmail.com>
2020-12-01 01:31:31 -0500

    Add Tokenization struct definition.

--- ftml/src/token.rs
+++ ftml/src/token.rs
@@ -18,12 +18,51 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+//! Module for functionality related to the tokenization step in parsing.
+
 use crate::{ExtractedToken, Token};
 
+/// Take an input string and produce a list of tokens for consumption by the parser.
 pub fn tokenize<'t>(log: &slog::Logger, text: &'t str) -> Vec<ExtractedToken<'t>> {
     let log = &log.new(slog_o!("function" => "tokenize", "text" => str!(text)));
 
     info!(log, "Running lexer on text");
 
     Token::extract_all(log, text)
+}
+
+/// Output of `tokenize()` to be consumed by `parse()`.
+///
+/// This is a wrapper struct around `Vec<ExtractedToken>` but which also
+/// preserves the original input text along with it. This allows some
+/// text operations (such as joining raw token slices) that would not
+/// be possible otherwise.
+///
+/// Because it is internal, we can be sure that the passed `input` definitely
+/// corresponds to the `tokens` produces by lexing.
+#[derive(Debug)]
+pub struct Tokenization<'t> {
+    tokens: Vec<ExtractedToken<'t>>,
+    input: &'t str,
+}
+
+impl<'t> Tokenization<'t> {
+    #[inline]
+    pub fn tokens(&self) -> &[ExtractedToken<'t>] {
+        &self.tokens
+    }
+
+    #[inline]
+    pub fn input(&self) -> &'t str {
+        self.input
+    }
+}
+
+impl<'t> Into<Vec<ExtractedToken<'t>>> for Tokenization<'t> {
+    #[inline]
+    fn into(self) -> Vec<ExtractedToken<'t>> {
+        let Tokenization { tokens, .. } = self;
+
+        tokens
+    }
 }
