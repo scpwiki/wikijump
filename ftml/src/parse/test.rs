09d2f78b7f9b2a776e2b77b0489caa074716f99d
Ammon Smith <ammon.i.smith@gmail.com>
2020-12-07 00:31:46 -0500

    Add bare link tests.

--- ftml/src/parse/test.rs
+++ ftml/src/parse/test.rs
@@ -18,6 +18,7 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+use crate::enums::{AnchorTarget, LinkLabel};
 use crate::parse::{ParseError, ParseErrorKind, Token};
 use crate::tree::{Container, ContainerType, Element, SyntaxTree};
 
@@ -523,6 +524,26 @@ fn ast() {
                 ParseErrorKind::NoRulesMatch,
             ),
         ],
+    );
+
+    test!(
+        "[https://example.com/ Some link!]",
+        vec![Element::Link {
+            url: "https://example.com/",
+            label: LinkLabel::Text("Some link!"),
+            anchor: AnchorTarget::Same,
+        }],
+        vec![],
+    );
+
+    test!(
+        "[*http://scp-sandbox-3.wikidot.com/system:recent-changes Sandbox: Recent Changes ]",
+        vec![Element::Link {
+            url: "http://scp-sandbox-3.wikidot.com/system:recent-changes",
+            label: LinkLabel::Text("Sandbox: Recent Changes"),
+            anchor: AnchorTarget::NewTab,
+        }],
+        vec![],
     );
 }
 
