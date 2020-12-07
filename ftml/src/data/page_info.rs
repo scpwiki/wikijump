dcf5eaebd3d5071ed2c4a1209eb701d837cc450d
Ammon Smith <ammon.i.smith@gmail.com>
2020-12-07 13:08:04 -0500

    Remove PageInfo and make it deserializable.

--- ftml/src/data/page_info.rs
+++ ftml/src/data/page_info.rs
@@ -18,6 +18,8 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+use std::borrow::Cow;
+
 /// Metadata information on the article being rendered.
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
 #[serde(rename_all = "kebab-case")]
@@ -25,61 +27,29 @@ pub struct PageInfo<'a> {
     /// The title of this page.
     ///
     /// For SCPs this is "SCP-XXXX".
-    pub title: &'a str,
+    pub title: Cow<'a, str>,
 
     /// The alternate title of this page.
     ///
     /// For SCPs this is its series listing title.
     /// If this is None then the main title is used instead.
-    pub alt_title: Option<&'a str>,
+    pub alt_title: Option<Cow<'a, str>>,
 
     /// The header of this page, if it's setting one.
     ///
     /// For regular pages this is "SCP Foundation".
     /// Previously this value was overriden using custom CSS.
-    pub header: Option<&'a str>,
+    pub header: Option<Cow<'a, str>>,
 
     /// The sub-header of this page, if it's setting one.
     ///
     /// For regular pages this is "Secure, Contain, Protect".
     /// Previously this value was overriden using custom CSS.
-    pub subheader: Option<&'a str>,
+    pub subheader: Option<Cow<'a, str>>,
 
     /// The current rating the page has.
     pub rating: f32,
 
     /// The current set of tags this page has.
-    pub tags: Vec<&'a str>,
-}
-
-/// An owned version of [`PageInfo`]. See there for field information.
-/// [`PageInfo`]: ./struct.PageInfo.html
-#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
-#[serde(rename_all = "kebab-case")]
-pub struct PageInfoOwned {
-    pub title: String,
-    pub alt_title: Option<String>,
-    pub header: Option<String>,
-    pub subheader: Option<String>,
-    pub rating: f32,
-    pub tags: Vec<String>,
-}
-
-impl PageInfoOwned {
-    pub fn as_borrow(&self) -> PageInfo {
-        macro_rules! borrow {
-            ($field:expr) => {
-                $field.as_ref().map(|s| s.as_ref())
-            };
-        }
-
-        PageInfo {
-            title: &self.title,
-            alt_title: borrow!(self.alt_title),
-            header: borrow!(self.header),
-            subheader: borrow!(self.subheader),
-            rating: self.rating,
-            tags: self.tags.iter().map(|s| s.as_ref()).collect(),
-        }
-    }
+    pub tags: Vec<Cow<'a, str>>,
 }
