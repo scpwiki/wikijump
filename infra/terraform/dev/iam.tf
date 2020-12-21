8312cb6114d7e9967c3d5f167dde0c02d863eb3a
bluesoul <atlassian@nags.me>
2020-12-21 14:43:31 -0700

    Missing data blocks

--- infra/terraform/dev/iam.tf
+++ infra/terraform/dev/iam.tf
@@ -34,3 +34,46 @@ resource "aws_iam_role_policy" "log_agent" {
   role   = aws_iam_role.task[0].id
   policy = data.aws_iam_policy_document.task_permissions.json
 }
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "task_permissions" {
+  statement {
+    effect = "Allow"
+
+    resources = ["*"]
+
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "read_repository_credentials" {
+  count = var.create_repository_credentials_iam_policy && var.enabled ? 1 : 0
+
+  statement {
+    effect = "Allow"
+
+    resources = [
+      var.repository_credentials,
+      data.aws_kms_key.secretsmanager_key[0].arn,
+    ]
+
+    actions = [
+      "secretsmanager:GetSecretValue",
+      "kms:Decrypt",
+    ]
+  }
+}
