17a0ee93c6722aa67373426fd58081f9390f73df
bluesoul <atlassian@nags.me>
2020-12-21 17:16:28 -0700

    Bring in jetbrains module and rewrite

--- infra/terraform/dev/ecs.tf
+++ infra/terraform/dev/ecs.tf
@@ -1,28 +1,59 @@
-module "ecs_cluster" {
-  source       = "github.com/jetbrains-infra/terraform-aws-ecs-cluster?ref=v0.4.8" // see https://github.com/jetbrains-infra/terraform-aws-ecs-cluster/releases
-  cluster_name = "wikijump-dev"
-  spot         = true
-  instance_types = {
-    "t3.medium"  = 1
-  }
-  target_capacity = 1
+resource "aws_ecs_cluster" "wikijump-ecs" {
+  name               = "wikijump-${var.environment}"
+  capacity_providers = [aws_ecs_capacity_provider.asg.name]
 
-  // subnets with ALB and bastion host e.g..
-  trusted_cidr_blocks = [
-    var.elb_subnet,
-    var.container_subnet
-  ]
-
-  subnets_ids = [
-    aws_subnet.container_subnet.id
-  ]
-
-  tags = {
-    Stack     = "Dev",
-    Terraform = "true"
+  default_capacity_provider_strategy {
+    capacity_provider = aws_ecs_capacity_provider.asg.name
+    weight = 1
   }
 }
 
+resource "aws_ecs_capacity_provider" "asg" {
+  name = aws_autoscaling_group.ecs_nodes.name
+
+  auto_scaling_group_provider {
+    auto_scaling_group_arn         = aws_autoscaling_group.ecs_nodes.arn
+    managed_termination_protection = "DISABLED"
+
+    managed_scaling {
+      maximum_scaling_step_size = 1
+      minimum_scaling_step_size = 1
+      status                    = "ENABLED"
+      target_capacity           = 1
+    }
+  }
+}
+
+resource "aws_autoscaling_group" "ecs_nodes" {
+  name_prefix           = "CLUSTER_NODES_"
+  max_size              = 1
+  min_size              = 1
+  vpc_zone_identifier   = aws_subnet.container_subnet.id
+  protect_from_scale_in = false
+
+  mixed_instances_policy {
+    instances_distribution {
+      on_demand_percentage_above_base_capacity = 0
+    }
+    launch_template {
+      launch_template_specification {
+        launch_template_id = aws_launch_template.node.id
+        version            = "$Latest"
+      }
+
+      dynamic "override" {
+        for_each = var.instance_type
+        content {
+          instance_type     = override.key
+          weighted_capacity = 1
+        }
+      }
+    }
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 
 resource "aws_ecs_task_definition" "wikijump_task" {
   family                   = "wikijump-${var.environment}-ec2"
---
+++ infra/terraform/dev/launch.tf
@@ -0,0 +1,83 @@
+data "template_cloudinit_config" "config" {
+  gzip          = false
+  base64_encode = true
+
+  part {
+    content_type = "text/x-shellscript"
+    content      = <<EOT
+#!/bin/bash
+echo ECS_CLUSTER="wikijump-${var.environment}" >> /etc/ecs/ecs.config
+echo ECS_ENABLE_CONTAINER_METADATA=true >> /etc/ecs/ecs.config
+echo ECS_ENABLE_SPOT_INSTANCE_DRAINING=true >> /etc/ecs/ecs.config
+EOT
+  }
+
+  dynamic "part" {
+    for_each = var.user_data
+    content {
+      content_type = "text/x-shellscript"
+      content      = part.value
+    }
+  }
+}
+
+data "aws_ssm_parameter" "ecs_ami" {
+  name = "/aws/service/ecs/optimized-ami/amazon-linux-2/recommended/image_id"
+}
+
+resource "aws_launch_template" "node" {
+  name_prefix            = "ecs_node_"
+  image_id               = data.aws_ssm_parameter.ecs_ami.value
+  instance_type          = var.instance_type[0]
+  vpc_security_group_ids = [aws_security_group.ecs_nodes.id]
+  user_data              = data.template_cloudinit_config.config.rendered
+  update_default_version = true
+
+  iam_instance_profile {
+    name = aws_iam_instance_profile.ecs_node.name
+  }
+}
+
+resource "aws_security_group" "ecs_nodes" {
+  name   = "ECS nodes for Wikijump dev"
+  vpc_id = aws_vpc.wikijump_vpc.id
+}
+
+resource "aws_security_group_rule" "ingress" {
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = [var.container_subnet, var.elb_subnet]
+  security_group_id = aws_security_group.ecs_nodes.id
+  type              = "ingress"
+}
+
+resource "aws_security_group_rule" "egress" {
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.ecs_nodes.id
+  type              = "egress"
+}
+
+resource "aws_iam_instance_profile" "ecs_node" {
+  name = "wikijump-ecs-ec2-dev"
+  role = aws_iam_role.ec2_instance_role.name
+}
+
+resource "aws_iam_role" "ec2_instance_role" {
+  assume_role_policy = data.aws_iam_policy_document.ec2_instance_assume_role_policy.json
+  name               = "wikijump-ec2-role-dev"
+}
+
+data "aws_iam_policy_document" "ec2_instance_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
