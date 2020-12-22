71ace8dac5ef11eba48e4e7367875dcf561fd1c2
bluesoul <atlassian@nags.me>
2020-12-21 20:30:48 -0700

    Add EFS stuff

--- infra/terraform/dev/efs.tf
+++ infra/terraform/dev/efs.tf
@@ -3,3 +3,11 @@ resource "aws_efs_file_system" "traefik_efs" {
   encrypted      = true
 }
 
+resource "aws_efs_access_point" "access" {
+  file_system_id = aws_efs_file_system.traefik_efs.id
+}
+
+resource "aws_efs_mount_target" "traefik_mount" {
+  file_system_id = aws_efs_file_system.traefik_efs.id
+  subnet_id      = aws_subnet.container_subnet.id
+}
