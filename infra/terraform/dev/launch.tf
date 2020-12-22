21bb0c75bfe37290a69c41d4eccf244422e83b8b
bluesoul <atlassian@nags.me>
2020-12-21 21:36:22 -0700

    More granular security groups

--- infra/terraform/dev/launch.tf
+++ infra/terraform/dev/launch.tf
@@ -48,7 +48,24 @@ resource "aws_security_group_rule" "ingress" {
   to_port           = 0
   protocol          = "-1"
   cidr_blocks       = [var.container_subnet, var.elb_subnet]
-  self              = true
+  security_group_id = aws_security_group.ecs_nodes.id
+  type              = "ingress"
+}
+
+resource "aws_security_group_rule" "http" {
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.ecs_nodes.id
+  type              = "ingress"
+}
+
+resource "aws_security_group_rule" "https" {
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.ecs_nodes.id
   type              = "ingress"
 }
@@ -58,6 +75,23 @@ resource "aws_security_group_rule" "egress" {
   to_port           = 0
   protocol          = "-1"
   cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.ecs_nodes.id
+  type              = "egress"
+}
+
+resource "aws_security_group_rule" "ingress_self" {
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  self              = true
+  security_group_id = aws_security_group.ecs_nodes.id
+  type              = "ingress"
+}
+
+resource "aws_security_group_rule" "egress_self" {
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
   self              = true
   security_group_id = aws_security_group.ecs_nodes.id
   type              = "egress"
