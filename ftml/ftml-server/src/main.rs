235b863ff0e860d7dece4e55e621d24f2f7a1fc1
Ammon Smith <ammon.i.smith@gmail.com>
2020-12-22 19:51:54 -0500

    Create ftml-server stub.

---
+++ ftml/ftml-server/src/main.rs
@@ -0,0 +1,36 @@
+/*
+ * main.rs
+ *
+ * ftml - Library to parse Wikidot code
+ * Copyright (C) 2019-2020 Ammon Smith
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#![deny(missing_debug_implementations)]
+#![forbid(unsafe_code)]
+
+//! REST web server to allow clients to preprocess, parse, and render Wikidot text.
+//!
+//! This is a wrapper around the `ftml` crate to provide its Rust library API over
+//! a REST interface, usable by any programming language, or via network.
+
+extern crate ftml;
+
+#[macro_use]
+extern crate serde;
+extern crate serde_json;
+extern crate warp;
+
+fn main() {}
