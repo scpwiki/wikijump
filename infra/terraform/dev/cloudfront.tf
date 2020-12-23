74ef392a1a3b72c6b52d616e022f66fefd19255e
bluesoul <atlassian@nags.me>
2020-12-23 09:49:12 -0700

    hooked on phonics

--- infra/terraform/dev/cloudfront.tf
+++ infra/terraform/dev/cloudfront.tf
@@ -36,7 +36,7 @@ resource "aws_cloudfront_distribution" "wikijump_cf_distro" {
     }
     custom_origin_config {
       http_port                = 80
-      https_post               = 443
+      https_port               = 443
       origin_protocol_policy   = "http-only"
       origin_ssl_protocols     = ["TLSv1.2"]
       origin_keepalive_timeout = 15
