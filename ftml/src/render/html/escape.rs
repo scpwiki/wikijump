42d25afa78874707520c06da2de01003569ce23a
Ammon Smith <ammon.i.smith@gmail.com>
2020-12-27 03:06:17 -0500

    Rename test macro.

--- ftml/src/render/html/escape.rs
+++ ftml/src/render/html/escape.rs
@@ -40,7 +40,7 @@ pub fn escape(buffer: &mut String, s: &str) {
 
 #[test]
 fn test() {
-    macro_rules! check {
+    macro_rules! test {
         ($input:expr, $expected:expr) => {{
             let mut buffer = String::new();
             escape(&mut buffer, $input);
@@ -49,14 +49,14 @@ fn test() {
         }};
     }
 
-    check!("", "");
-    check!("Hello, world!", "Hello, world!");
-    check!("x + 3 > 19, solve for x", "x + 3 &gt; 19, solve for x");
-    check!(
+    test!("", "");
+    test!("Hello, world!", "Hello, world!");
+    test!("x + 3 > 19, solve for x", "x + 3 &gt; 19, solve for x");
+    test!(
         "<script>alert('test');</script>",
         "&lt;script&gt;alert(&#39;test&#39;);&lt;/script&gt;"
     );
-    check!(
+    test!(
         "S & C Plastic's location",
         "S &amp; C Plastic&#39;s location"
     );
