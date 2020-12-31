189037bd27086dcaa8814552241c8e24b66079ae
Ross Williams <rossjrw@gmail.com>
2020-12-31 19:49:29 +0000

    Set up Tailwind

---
+++ org/tailwind.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  purge: [ './src/**/*.html', './src/**/*.css' ]
+}
--- org/webpack.common.js
+++ org/webpack.common.js
@@ -13,6 +13,7 @@ module.exports = {
       {
         test: /\.css$/,
         use: [
+          'style-loader',
           'css-loader',
           {
             loader: 'postcss-loader',
