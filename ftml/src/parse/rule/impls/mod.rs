b1017e48292869985a2ed6b02ad94dbfc45a8ba8
Ammon Smith <ammon.i.smith@gmail.com>
2020-12-31 03:43:15 -0500

    Add Parser to impls prelude.

--- ftml/src/parse/rule/impls/mod.rs
+++ ftml/src/parse/rule/impls/mod.rs
@@ -21,9 +21,10 @@
 mod prelude {
     pub use crate::parse::consume::consume;
     pub use crate::parse::error::{ParseError, ParseErrorKind, ParseException};
+    pub use crate::parse::parser::Parser;
     pub use crate::parse::result::{ParseResult, ParseSuccess};
     pub use crate::parse::rule::collect::*;
-    pub use crate::parse::rule::{Rule, TryConsumeFn};
+    pub use crate::parse::rule::Rule;
     pub use crate::parse::token::{ExtractedToken, Token};
     pub use crate::text::FullText;
     pub use crate::tree::{Container, ContainerType, Element};
