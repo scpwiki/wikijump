5fb94b430d3c99a9d6e0f3d0aee3a4886a55fb96
Ammon Smith <ammon.i.smith@gmail.com>
2021-01-02 18:01:25 -0500

    Remove temporary lint suppression in BlockParser.

--- ftml/src/parse/rule/impls/block/mod.rs
+++ ftml/src/parse/rule/impls/block/mod.rs
@@ -18,9 +18,6 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-// TODO while refactoring Parser
-#![allow(dead_code)]
-
 //! Meta-rule for all block constructs.
 //!
 //! This matches `[[` or `[[*` and runs the block parsing
