6586b37d97762c26b02df5ba7ef87ba515101824
Ammon Smith <ammon.i.smith@gmail.com>
2021-01-15 23:05:16 -0500

    Add [[ins]] block.

---
+++ ftml/src/parse/rule/impls/block/blocks/ins.rs
@@ -0,0 +1,67 @@
+/*
+ * parse/rule/impls/block/blocks/ins.rs
+ *
+ * ftml - Library to parse Wikidot text
+ * Copyright (C) 2019-2021 Ammon Smith
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+use super::prelude::*;
+
+pub const BLOCK_INS: BlockRule = BlockRule {
+    name: "block-ins",
+    accepts_names: &["ins"],
+    accepts_special: false,
+    newline_separator: false,
+    parse_fn,
+};
+
+fn parse_fn<'r, 't>(
+    log: &slog::Logger,
+    parser: &mut Parser<'r, 't>,
+    name: &'t str,
+    special: bool,
+    in_head: bool,
+) -> ParseResult<'r, 't, Element<'t>> {
+    debug!(
+        log,
+        "Parsing insert block";
+        "in-head" => in_head,
+        "name" => name,
+    );
+
+    assert_eq!(special, false, "Ins doesn't allow special variant");
+    assert_block_name(&BLOCK_INS, name);
+
+    let mut arguments = parser.get_head_map(&BLOCK_INS, in_head)?;
+
+    // Get styling arguments
+    let id = arguments.get("id");
+    let class = arguments.get("class");
+    let style = arguments.get("style");
+
+    // Get body content, without paragraphs
+    let (elements, exceptions) = parser.get_body_elements(&BLOCK_INS, false)?.into();
+
+    // Build and return element
+    let element = Element::Insertion {
+        elements,
+        id,
+        class,
+        style,
+    };
+
+    ok!(element, exceptions)
+}
