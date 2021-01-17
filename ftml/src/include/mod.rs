c2ba862813e5f6a8f0f697ad18362c95e622cb22
Ammon Smith <ammon.i.smith@gmail.com>
2021-01-16 23:22:10 -0500

    Start include implementation.

--- ftml/src/include/mod.rs
+++ ftml/src/include/mod.rs
@@ -18,6 +18,50 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+use std::borrow::Cow;
+use std::collections::HashMap;
+
 #[derive(Parser, Debug)]
 #[grammar = "include/grammar.pest"]
 struct Parser;
+
+#[derive(Serialize, Deserialize, Debug, Clone, Hash)]
+pub struct PageRef<'t> {
+    site: Option<Cow<'t, str>>,
+    page: Cow<'t, str>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct IncludeRef<'t> {
+    page: PageRef<'t>,
+    variables: HashMap<Cow<'t, str>, Cow<'t, str>>,
+}
+
+pub trait Includer<'t> {
+    fn include_pages(
+        &mut self,
+        includes: &[IncludeRef<'t>],
+    ) -> HashMap<PageRef<'t>, Cow<'t, str>>;
+
+    fn no_such_include(&mut self) -> Cow<'t, str>;
+}
+
+pub fn include<'t>(
+    log: &slog::Logger,
+    text: &'t mut String,
+    includer: &mut dyn Includer<'t>,
+) {
+    let log = &log.new(slog_o!(
+        "filename" => slog_filename!(),
+        "lineno" => slog_lineno!(),
+        "function" => "include",
+        "text" => str!(text),
+    ));
+
+    info!(
+        log,
+        "Finding and replacing all instances of include blocks in text"
+    );
+
+    todo!()
+}
