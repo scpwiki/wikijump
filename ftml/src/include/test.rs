8acc901906b06dd7de276af93816367fdb42484b
Ammon Smith <ammon.i.smith@gmail.com>
2021-01-18 02:30:04 -0500

    Add missing import.

--- ftml/src/include/test.rs
+++ ftml/src/include/test.rs
@@ -18,7 +18,7 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-use super::{include, NullIncluder};
+use super::{include, NullIncluder, PageRef};
 
 #[test]
 fn includes() {
