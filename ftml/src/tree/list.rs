e83afad6cfd5cddea7a7f17039d0e38572e8772d
Ammon Smith <ammon.i.smith@gmail.com>
2021-02-02 19:50:34 -0500

    Use an untagged representation of ListItem.

--- ftml/src/tree/list.rs
+++ ftml/src/tree/list.rs
@@ -22,6 +22,7 @@ use super::Element;
 use strum_macros::IntoStaticStr;
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
+#[serde(rename_all = "kebab-case", untagged)]
 pub enum ListItem<'t> {
     /// This item is a series of elements.
     ///
