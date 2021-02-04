67af9efa746e3358ce79d86a82e9ee4615242dde
Ammon Smith <ammon.i.smith@gmail.com>
2021-02-03 22:57:52 -0500

    Pass (currently unused) list_type to depths.

--- ftml/src/parsing/rule/impls/list.rs
+++ ftml/src/parsing/rule/impls/list.rs
@@ -146,7 +146,7 @@ fn try_consume_fn<'p, 'r, 't>(
         .chain(&mut exceptions);
 
         // Append bullet line
-        depths.push((depth, (), elements));
+        depths.push((depth, list_type, elements));
     }
 
     // Our rule is in another castle
