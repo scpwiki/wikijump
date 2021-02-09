5b7c9ca4739806668ff8dd895541a7aca9c28ddc
Ammon Smith <ammon.i.smith@gmail.com>
2021-02-08 20:37:18 -0500

    Add left/right double angle tokens.

--- ftml/src/parsing/rule/mapping.rs
+++ ftml/src/parsing/rule/mapping.rs
@@ -44,6 +44,8 @@ lazy_static! {
             Token::RightBlock => vec![],
             Token::DoubleDash => vec![RULE_STRIKETHROUGH, RULE_DASH],
             Token::TripleDash => vec![RULE_HORIZONTAL_RULE],
+            Token::LeftDoubleAngle => vec![], // TODO
+            Token::RightDoubleAngle => vec![], // TODO
             Token::ClearFloatNeutral => vec![RULE_TODO], // TODO
             Token::ClearFloatCenter => vec![RULE_TODO], // TODO
             Token::ClearFloatLeft => vec![RULE_TODO], // TODO
