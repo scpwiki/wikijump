23408265163f4f00f04658f5d4ac4c8098ef7569
Ammon Smith <ammon.i.smith@gmail.com>
2021-03-20 04:58:26 -0400

    Mark maplit as test dependency only.

--- ftml/src/lib.rs
+++ ftml/src/lib.rs
@@ -37,9 +37,6 @@ extern crate enum_map;
 
 #[macro_use]
 extern crate lazy_static;
-
-#[macro_use]
-extern crate maplit;
 extern crate pest;
 
 #[macro_use]
@@ -64,6 +61,8 @@ extern crate wikidot_normalize;
 
 cfg_if! {
     if #[cfg(test)] {
+        #[macro_use]
+        extern crate maplit;
         extern crate sloggers;
     }
 }
