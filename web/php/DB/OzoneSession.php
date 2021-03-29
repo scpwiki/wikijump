31ac57b96d437149648d9fb9bc6f3b4c2072ac6f
bluesoul <atlassian@nags.me>
2021-03-29 08:44:41 -0600

    Fix OzoneSession, allow local dev connection to pgsql

--- web/php/DB/OzoneSession.php
+++ web/php/DB/OzoneSession.php
@@ -7,7 +7,11 @@ namespace Wikidot\DB;
  * Object Model Class.
  *
  */
-class OzoneSession extends OzoneSessionBase
+class OzoneSession extends \Ozone\Framework\DB\OzoneSession
 {
-
+    /**
+     *  Look man. I know how dumb this looks. You're not telling me anything I don't know.
+     *  But if you remove this class, or rename it, you'll break logon sessions.
+     *  Blame Ozone\Framework\Database\BaseDBPeer.
+     */
 }
