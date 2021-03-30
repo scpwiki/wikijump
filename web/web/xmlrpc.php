ed2b6366bb221193127b324f5a1f7b63818a99c1
bluesoul <atlassian@nags.me>
2021-03-30 16:46:10 -0600

    More deletions

--- web/web/xmlrpc.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once('../php/setup.php');
-require_once('Zend/XmlRpc/Server.php');
-
-$server = new Zend_XmlRpc_Server();
-$server->setClass('PingBackServer', 'pingback');
-
-echo $server->handle();
