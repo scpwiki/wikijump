7a61cc625cbdb1dce7b57abb05feb2d5e4dd8eac
Ammon Smith <ammon.i.smith@gmail.com>
2021-04-11 22:59:04 -0400

    Remove unused import.

--- ftml/src/wasm/parsing.rs
+++ ftml/src/wasm/parsing.rs
@@ -18,7 +18,6 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-use super::error::error_to_js;
 use super::prelude::*;
 use super::tokenizer::Tokenization;
 use crate::parsing::ParseOutcome as RustParseOutcome;
