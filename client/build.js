417ab50cf3ae65cde3edd67c98be02318f2c6004
Monkatraz <34875062+Monkatraz@users.noreply.github.com>
2021-04-13 08:17:08 -0600

    nicer logging messages

--- client/build.js
+++ client/build.js
@@ -1,10 +1,13 @@
-const { build, cliopts } = require('estrella')
+const { build, cliopts, basename, stdoutStyle: styl, fmtDuration } = require('estrella')
 const { nodeExternalsPlugin } = require('esbuild-node-externals')
 const { readdirSync, realpathSync } = require('fs')
+const { performance } = require('perf_hooks')
 
+const dev = !!cliopts.watch
 const [opts] = cliopts.parse(['self', 'Compiles the calling package.'])
 
 const SETTINGS_COMMON = {
+  // esbuild options
   outdir: 'dist',
   bundle: true,
   treeShaking: true,
@@ -13,8 +16,9 @@ const SETTINGS_COMMON = {
   format: 'esm',
   sourcemap: true,
   sourcesContent: true,
-  // estrella
-  tslint: false
+  // estrella options
+  tslint: false,
+  silent: true
 }
 
 function getDirectories(source) {
@@ -25,10 +29,16 @@ function getDirectories(source) {
 
 function buildPackage(name) {
   // figure out our paths based on if the package is self-compiling or not
-  const self = name === '.'
+  const self = !name // undefined or empty
   const dir = self ? process.cwd() : realpathSync('./packages/' + name)
   const index = (self ? '.' : dir) + '/src/index.ts'
   const package = (self ? '.' : dir) + '/package.json'
+
+  name = basename(dir)
+
+  if (!dev) console.log(styl.blue(`[${name}]`), 'Building!')
+  let start = performance.now()
+
   // build the darn thing
   build({
     ...SETTINGS_COMMON,
@@ -38,15 +48,26 @@ function buildPackage(name) {
     plugins: [nodeExternalsPlugin({ packagePath: package })],
     cwd: dir,
     // estrella uses Chokidar, so this property uses the Chokidar watch options interface
-    watch: cliopts.watch && {
+    watch: dev && {
       cwd: dir
+    },
+    // logging handlers
+    onStart(_, changed) {
+      if (dev && changed && changed.length) {
+        start = performance.now()
+        console.log(styl.blue(`[${name}]`), 'Rebuilding!', styl.orange(`[${changed.join(', ')}]`))
+      }
+    },
+    onEnd() {
+      const elapsed = fmtDuration(performance.now() - start)
+      console.log(styl.blue(`[${name}]`), 'Finished.', styl.green(`${elapsed}`))
     }
   })
 }
 
-// check if we're doing building all or just a single package
+// check if we're building all or just a single package
 if (opts.self) {
-  buildPackage('.')
+  buildPackage()
 } else {
   getDirectories('./packages').forEach(name => buildPackage(name))
 }
