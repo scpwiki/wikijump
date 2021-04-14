120cdb485fa84595687524617d349396ef9cb782
Ammon Smith <ammon.i.smith@gmail.com>
2021-04-14 14:13:26 -0400

    Place container class first in list.

--- ftml/src/render/html/builder.rs
+++ ftml/src/render/html/builder.rs
@@ -178,7 +178,7 @@ impl<'c, 'i, 'h, 't> HtmlBuilderTag<'c, 'i, 'h, 't> {
             // Otherwise, just pass in the value
             if key.eq_ignore_ascii_case(extra_key) {
                 merged = true;
-                self.attr(&key, &[value, " ", extra_value]);
+                self.attr(&key, &[extra_value, " ", value]); // so the container class is first
             } else {
                 self.attr(&key, &[value]);
             }
