5dfd524709bda64107af09d4dd336f72c09af38c
pxdnbluesoul <github@nags.me>
2021-04-15 22:47:53 -0600

    yeet

--- web/web/default_flow.php
+++ web/web/default_flow.php
@@ -9,9 +9,6 @@ require ('../php/setup.php');
 header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
 header("Expires: Mon, 26 Jul 1997 05:00:00 GMT"); // Date in the past
 
-
-setsecurecookie("wikijump_token7", md5(rand(0, 10000)), 0, '/', GlobalProperties::$SESSION_COOKIE_DOMAIN);
-
 try {
     $controller = new WDDefaultFlowController();
     $controller->process();
