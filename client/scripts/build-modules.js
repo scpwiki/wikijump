d380e60fea1b2f4d2d9cc403e331f605d5c05c9d
Monkatraz <34875062+Monkatraz@users.noreply.github.com>
2021-04-16 20:37:18 -0600

    simplify test building, running

--- client/scripts/build-modules.js
+++ client/scripts/build-modules.js
@@ -4,15 +4,55 @@ const { readdirSync, realpathSync } = require("fs")
 const { performance } = require("perf_hooks")
 const browserslist = require("browserslist")
 
-const [opts] = cliopts.parse(["self", "Compiles the calling module package only."])
+// -- CONSTANTS, COMMAND LINE ARGUMENTS
 
-const dev = Boolean(cliopts.watch)
-const self = opts.self ?? false
+const [opts] = cliopts.parse(
+  ["self", "Compiles the calling module package only."],
+  ["tests", "Compile the module tests instead."]
+)
 
-// demangle browserslist versions
+const DEV = Boolean(cliopts.watch)
+const SELF = opts.self ?? false
+const TESTS = opts.tests ?? false
+
+if (DEV && TESTS) throw new Error("Can't use -watch and -tests at the same time!")
+
+const fmt = TESTS ? styl.orange : styl.blue
+
+/** Common settings for all modules that are built. */
+const SETTINGS_COMMON = {
+  // esbuild settings
+  outdir: "dist",
+  // minify: true // estrella implicitly toggles minify depending on the `--debug` flag
+  bundle: true,
+  treeShaking: true,
+  splitting: true,
+  format: "esm",
+  platform: "browser",
+  sourcemap: true,
+  sourcesContent: true,
+
+  // estrella settings
+  tslint: false, // disables estrella's built-in tsc typechecking
+  quiet: true // silences estrella's logging, as we use our own logging
+}
+
 // prettier-ignore
 const DEMANGLE_TARGET_REGEX =
   /^(chrome|and_(?:ff|chr)|edge|firefox|ios_saf|safari)\s+([\d.]+)/
+
+// -- DEMANGLE BROWSERSLIST
+
+/*
+  esbuild expects the browsers list provided to it look like this:
+  chrome89, firefox78, etc.
+  Browserslist looks like this:
+  chrome 89, firefox 78, etc.
+  Additionally, esbuild only accepts certain browsers.
+  So, this chunk of code converts the browserslist list into an esbuild list.
+  It also renames some of the mobile browsers to match esbuild's version.
+*/
+
 const targets = new Set()
 for (const target of browserslist()) {
   const result = DEMANGLE_TARGET_REGEX.exec(target)
@@ -25,34 +65,20 @@ for (const target of browserslist()) {
   targets.add(browser + version)
 }
 
-const SETTINGS_COMMON = {
-  // esbuild settings
-  outdir: "dist",
-  // minify: true // estrella implicitly toggles minify depending on the `--debug` flag
-  bundle: true,
-  treeShaking: true,
-  splitting: true,
-  format: "esm",
-  platform: "browser",
-  sourcemap: true,
-  sourcesContent: true,
-  target: [...targets],
+// -- BUILD
 
-  // estrella settings
-  tslint: false, // disables estrella's built-in tsc typechecking
-  quiet: true // silences estrella's logging, as we use our own logging
-}
+// check if we're building all or just a single package
+if (SELF) buildModule()
+else directoriesOf("./modules").forEach(name => buildModule(name))
 
-function directoriesOf(source) {
-  return readdirSync(source, { withFileTypes: true })
-    .filter(dirent => dirent.isDirectory())
-    .map(dirent => dirent.name)
-}
+// -- FUNCTIONS
 
+/** Builds the module (a package in `modules/`).
+ *  If `-tests` was specified, it'll compile the module's tests, instead. */
 function buildModule(name) {
-  let dir, index, package
+  let dir, index, package, tests
 
-  if (self) {
+  if (SELF) {
     dir = process.cwd()
     index = "./src/index.ts"
     package = "./package.json"
@@ -62,42 +88,76 @@ function buildModule(name) {
     package = `${dir}/package.json`
   }
 
+  if (TESTS) {
+    try {
+      tests = filesOf(`${dir}/tests`).map(name => `./tests/${name}`)
+      if (tests.length === 0) {
+        console.log(fmt(`[${name}]`), "No tests, skipping.")
+        return
+      }
+    } catch {
+      // we'll be here if the `tests` folder doesn't exist
+      console.log(fmt(`[${name}]`), "No tests, skipping.")
+      return
+    }
+  }
+
   name = basename(dir)
 
-  if (!dev) console.log(styl.blue(`[${name}]`), "Building!")
+  const NODE = name.startsWith("node-")
+
+  if (!DEV) console.log(fmt(`[${name}]`), "Building!")
   let start = performance.now()
 
   build({
+    // -- SETTINGS
+
     ...SETTINGS_COMMON,
 
-    // esbuild settings
+    // esbuild
     absWorkingDir: dir,
     tsconfig: package,
     plugins: [nodeExternalsPlugin({ packagePath: package })],
+    target: [...targets],
 
-    // node compiling kludge
-    // prettier-ignore
-    ...(!name.startsWith('node-') ? {} : {
-      bundle: false,
-      splitting: false,
-      format: "cjs",
-      platform: "node"
-    }),
-
-    // estrella settings
+    // estrella
     entry: index,
     cwd: dir,
     // this property uses the Chokidar watch settings interface
     // we need to make sure that Chokidar is watching the module directory
-    watch: dev && { cwd: dir },
+    watch: DEV && { cwd: dir },
 
-    // logging handlers
+    // -- MODES
+
+    // node compiling
+    // prettier-ignore
+    ...(!NODE ? {} : {
+      bundle: false,
+      splitting: false,
+      format: "cjs",
+      platform: "node",
+      target: undefined
+    }),
+
+    // test compiling
+    //prettier-ignore
+    ...(!TESTS ? {} : {
+      entry: [...tests],
+      outdir: `${dir}/tests/dist`,
+      bundle: true,
+      splitting: false,
+      platform: "node",
+      sourcemap: false,
+      target: undefined
+    }),
+
+    // -- LOGGING HANDLERS
 
     onStart(_, changed) {
-      if (dev && changed && changed.length) {
+      if (DEV && changed && changed.length) {
         start = performance.now()
         console.log(
-          styl.blue(`[${name}]`),
+          fmt(`[${name}]`),
           "Rebuilding!",
           styl.orange(`[${changed.join(", ")}]`)
         )
@@ -106,11 +166,21 @@ function buildModule(name) {
 
     onEnd() {
       const elapsed = fmtDuration(performance.now() - start)
-      console.log(styl.blue(`[${name}]`), "Finished.", styl.green(`${elapsed}`))
+      console.log(fmt(`[${name}]`), "Finished.", styl.green(`${elapsed}`))
     }
   })
 }
 
-// check if we're building all or just a single package
-if (self) buildModule()
-else directoriesOf("./modules").forEach(name => buildModule(name))
+/** Gets the directories (shallow) of a folder. */
+function directoriesOf(source) {
+  return readdirSync(source, { withFileTypes: true })
+    .filter(dirent => dirent.isDirectory())
+    .map(dirent => dirent.name)
+}
+
+/** Gets the files (shallow) of a folder. */
+function filesOf(source) {
+  return readdirSync(source, { withFileTypes: true })
+    .filter(dirent => dirent.isFile())
+    .map(dirent => dirent.name)
+}
