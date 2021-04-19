bccd49a4b88c6a9caf7cc8d27437c6fe291d3c37
Ammon Smith <ammon.i.smith@gmail.com>
2021-04-19 03:38:31 -0400

    Allow unused logging macros.

--- ftml/src/log.rs
+++ ftml/src/log.rs
@@ -77,59 +77,6 @@ cfg_if! {
             pub use super::Logger;
         }
 
-        // Dummy filename/lineno macros
-        macro_rules! slog_filename {
-            () => ();
-        }
-
-        macro_rules! slog_lineno {
-            () => ();
-        }
-
-        // Dummy logging macros
-        macro_rules! crit {
-            ($l:expr, $($args:tt)+) => {
-                crate::log::unused($l)
-            };
-        }
-
-        macro_rules! error {
-            ($l:expr, $($args:tt)+) => {
-                crate::log::unused($l)
-            };
-        }
-
-        macro_rules! warn {
-            ($l:expr, $($args:tt)+) => {
-                crate::log::unused($l)
-            };
-        }
-
-        macro_rules! info {
-            ($l:expr, $($args:tt)+) => {
-                crate::log::unused($l)
-            };
-        }
-
-        macro_rules! debug {
-            ($l:expr, $($args:tt)+) => {
-                crate::log::unused($l)
-            };
-        }
-
-        macro_rules! trace {
-            ($l:expr, $($args:tt)+) => {
-                crate::log::unused($l)
-            };
-        }
-
-        // Dummy logging construction macros
-        macro_rules! slog_o {
-            ($($args:tt)+) => {
-                ()
-            };
-        }
-
         // Dummy logging structure
         #[derive(Debug, Copy, Clone)]
         pub struct Logger;
@@ -142,5 +89,62 @@ cfg_if! {
 
         // Helpers
         pub fn unused<T>(_x: T) {}
+
+        #[macro_use]
+        #[allow(unused_macros)]
+        mod macros {
+            // Dummy filename/lineno macros
+            macro_rules! slog_filename {
+                () => ();
+            }
+
+            macro_rules! slog_lineno {
+                () => ();
+            }
+
+            // Dummy logging macros
+            macro_rules! crit {
+                ($l:expr, $($args:tt)+) => {
+                    crate::log::unused($l)
+                };
+            }
+
+            macro_rules! error {
+                ($l:expr, $($args:tt)+) => {
+                    crate::log::unused($l)
+                };
+            }
+
+            macro_rules! warn {
+                ($l:expr, $($args:tt)+) => {
+                    crate::log::unused($l)
+                };
+            }
+
+            macro_rules! info {
+                ($l:expr, $($args:tt)+) => {
+                    crate::log::unused($l)
+                };
+            }
+
+            macro_rules! debug {
+                ($l:expr, $($args:tt)+) => {
+                    crate::log::unused($l)
+                };
+            }
+
+            macro_rules! trace {
+                ($l:expr, $($args:tt)+) => {
+                    crate::log::unused($l)
+                };
+            }
+
+            // Dummy logging construction macros
+            macro_rules! slog_o {
+                ($($args:tt)+) => {
+                    ()
+                };
+            }
+        }
     }
 }
