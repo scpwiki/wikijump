b62a4cff0f70db0fda43301f7a2dcbeb2b2c265d
Monkatraz <34875062+Monkatraz@users.noreply.github.com>
2021-04-19 02:14:47 -0600

    poke a quick typecheck fix in

--- client/modules/sheaf-core/src/print-tree.ts
+++ client/modules/sheaf-core/src/print-tree.ts
@@ -2,6 +2,8 @@
  * @file Exports the `printTree` (AST stringify) function for Sheaf.
  */
 
+import type { Input, Tree } from "lezer-tree"
+
 // TODO: STUB
 
 export function printTree(tree: Tree, input: Input | string, options = {}) {
