6254684c7a44c24781e950171954a24896a0827a
pxdnbluesoul <github@nags.me>
2021-04-20 17:11:10 -0600

    Move laravel to index.php, adjust routes and calls

--- web/app/Http/Controllers/OzoneController.php
+++ web/app/Http/Controllers/OzoneController.php
@@ -7,7 +7,7 @@ class OzoneController extends Controller
     public function handle($path = "")
     {
         ob_start();
-        require base_path('web/index.php');
+        require base_path('web/ozone.php');
         return response(ob_get_clean());
     }
 }
