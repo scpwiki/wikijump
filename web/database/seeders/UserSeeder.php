d893d55d58912fdc660753613f2bb03262afabc3
pxdnbluesoul <github@nags.me>
2021-04-21 21:30:11 -0600

    Create admin and user accounts via seeder

--- web/database/seeders/UserSeeder.php
+++ web/database/seeders/UserSeeder.php
@@ -2,7 +2,11 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Wikidot\Utils\WDStringUtils;
 
 class UserSeeder extends Seeder
 {
@@ -13,6 +17,31 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        /*
+         * TODO: It would be good if production credentials were either
+         * generated on the fly or if the admin were prompted for input.
+         */
+        if(env('APP_ENV') != 'production') {
+            DB::table('users')->insert(
+                [
+                    'username' => 'admin',
+                    'unix_name' => WDStringUtils::toUnixName('admin'),
+                    'password' => Hash::make('admin1'),
+                    'email' => 'admin@wikijump.dev',
+                    'email_verified_at' => Carbon::now(),
+                    'language' => env('DEFAULT_LANGUAGE'),
+                ]
+            );
+            DB::table('users')->insert(
+                [
+                    'username' => 'user',
+                    'unix_name' => WDStringUtils::toUnixName('user'),
+                    'password' => Hash::make('user1'),
+                    'email' => 'user@wikijump.dev',
+                    'email_verified_at' => Carbon::now(),
+                    'language' => env('DEFAULT_LANGUAGE'),
+                ]
+            );
+        }
     }
 }
