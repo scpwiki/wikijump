5d8d0cabc0616c15a11450012276e0f4ddb7c996
Monkatraz <34875062+Monkatraz@users.noreply.github.com>
2021-04-22 15:54:31 -0600

    initial build process for svelte components

---
+++ client/components/src/index.ts
@@ -0,0 +1,1 @@
+export { default as Tabview } from "./Tabview.svelte"
---
+++ client/scripts/build-components.js
@@ -0,0 +1,114 @@
+const { build, cliopts, stdoutStyle: styl, fmtDuration } = require("estrella")
+const { readdirSync } = require("fs")
+const { performance } = require("perf_hooks")
+const globby = require("globby")
+
+const { nodeExternalsPlugin } = require("esbuild-node-externals")
+const compileWorkersPlugin = require("./esbuild-compile-worker")
+
+const sveltePreprocess = require("svelte-preprocess")
+const sveltePlugin = require("esbuild-svelte")
+
+// -- CONSTANTS, COMMAND LINE ARGUMENTS
+
+const [opts] = cliopts.parse(["tests", "Compile the tests instead."])
+
+const DEV = Boolean(cliopts.watch)
+const TESTS = opts.tests ?? false
+
+if (DEV && TESTS) throw new Error("Can't use -watch and -tests at the same time!")
+
+const fmt = TESTS ? styl.orange : styl.blue
+
+buildComponents()
+
+async function buildComponents() {
+  const index = `./src/index.ts`
+  const sveltes = await globby("./src/**/*.svelte")
+
+  let tests
+  if (TESTS) {
+    try {
+      tests = filesOf(`./tests`).map(name => `./tests/${name}`)
+      if (tests.length === 0) {
+        console.log(fmt(`[components]`), "No tests, skipping.")
+        return
+      }
+    } catch {
+      // we'll be here if the `tests` folder doesn't exist
+      console.log(fmt(`[components]`), "No tests, skipping.")
+      return
+    }
+  }
+
+  if (!DEV) console.log(fmt(`[components]`), "Building!")
+  let start = performance.now()
+
+  // see `build-modules.js` for a lot more explanation on this
+  // most of this is taken from there
+
+  build({
+    // esbuild settings
+    outdir: "dist",
+    bundle: true,
+    treeShaking: true,
+    splitting: true,
+    format: "esm",
+    platform: "browser",
+    sourcemap: true,
+    sourcesContent: true,
+    plugins: [
+      nodeExternalsPlugin(),
+      compileWorkersPlugin,
+      sveltePlugin({ preprocess: sveltePreprocess() })
+    ],
+
+    // estrella settings
+    entry: [index, ...sveltes],
+    tslint: false,
+    quiet: true,
+
+    // test compiling
+    // prettier-ignore
+    ...(!TESTS ? {} : {
+      entry: [...tests],
+      outdir: `./tests/dist`,
+      bundle: true,
+      minify: false,
+      splitting: false,
+      format: "cjs",
+      platform: "node",
+      sourcemap: false,
+      target: undefined,
+      outExtension: { ".js": ".cjs" },
+      define: {
+        "window": "globalThis"
+      }
+    }),
+
+    // -- LOGGING HANDLERS
+
+    onStart(_, changed) {
+      if (DEV && changed && changed.length) {
+        start = performance.now()
+        console.log(
+          fmt(`[components]`),
+          "Rebuilding!",
+          styl.orange(`[${changed.join(", ")}]`)
+        )
+      }
+    },
+
+    onEnd() {
+      const elapsed = fmtDuration(performance.now() - start)
+      console.log(fmt(`[components]`), "Finished.", styl.green(`${elapsed}`))
+    }
+  })
+}
+
+/** Gets the files (shallow) of a folder. */
+function filesOf(source) {
+  return readdirSync(source, { withFileTypes: true })
+    .filter(dirent => dirent.isFile())
+    .map(dirent => dirent.name)
+}
