b45db3e13c5585b0ac85b3b2ae43f393a5e281ea
pxdnbluesoul <github@nags.me>
2021-04-22 22:28:05 -0600

    Remove unneeded require statement that PHPUnit barfs on

--- web/web/ozone.php
+++ web/web/ozone.php
@@ -4,8 +4,6 @@ use Ozone\Framework\Exceptions\OzoneDatabaseException;
 use Ozone\Framework\OzoneLogger;
 use Wikidot\Utils\WikiFlowController;
 
-require ('../php/setup.php');
-
 // to avoid caching
 header('Cache-Control: no-cache, must-revalidate'); // HTTP/1.1
 header('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
