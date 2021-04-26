52ef39f496096a1e784cfd482155fde8d7885fe2
Ammon Smith <ammon.i.smith@gmail.com>
2021-04-26 00:34:38 -0400

    Output ftml to "target/{level}/ftml.h"

--- ftml/build.rs
+++ ftml/build.rs
@@ -17,7 +17,13 @@ fn main() {
     // Generate C bindings for FFI
     let target_path = {
         let target_dir = env::var("OUT_DIR").unwrap();
+
+        // This takes the form '/path/to/repo/target/release/build/ftml-0000000000000000/out'
+        // We want '/path/to/repo/target/release/ftml.h'
         let mut path = PathBuf::from(target_dir);
+        path.pop();
+        path.pop();
+        path.pop();
         path.push("ftml.h");
         path
     };
