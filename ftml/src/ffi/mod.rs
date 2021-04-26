ab0c875b9533e9413eb9554337c08081e8405377
Ammon Smith <ammon.i.smith@gmail.com>
2021-04-26 01:12:15 -0400

    Add newline.

--- ftml/src/ffi/mod.rs
+++ ftml/src/ffi/mod.rs
@@ -24,6 +24,7 @@
 // This is only used for FFI, no weird memory tricks are used.
 // So this is the "safe" form of unsafe within Rust.
 #![allow(unsafe_code)]
+
 // This module uses C naming for its components, mostly snake_case.
 #![allow(non_camel_case_types)]
 
