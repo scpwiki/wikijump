148b2bba53ce1ea39931e232300c14a4f9c7eeb1
Monkatraz <34875062+Monkatraz@users.noreply.github.com>
2021-04-28 02:45:03 -0600

    fix shims

--- client/scripts/tests-shim.js
+++ client/scripts/tests-shim.js
@@ -2,6 +2,10 @@ const { JSDOM } = require("jsdom")
 const fetch = require("node-fetch")
 const { performance } = require("perf_hooks")
 
-globalThis.window = new JSDOM("")
+const DOM = new JSDOM("")
+
+globalThis.window = DOM.window
+globalThis.document = DOM.window.document
 globalThis.fetch = fetch
 globalThis.performance = performance
+globalThis.DOMParser = DOM.window.DOMParser
