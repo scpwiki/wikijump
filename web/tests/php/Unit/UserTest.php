a09f5c21667ba377d2029a07262da7206d920c59
pxdnbluesoul <github@nags.me>
2021-04-29 12:26:29 -0600

    Stub unit test of user class

--- web/tests/php/Unit/UserTest.php
+++ web/tests/php/Unit/UserTest.php
@@ -2,10 +2,19 @@
 
 namespace Tests\Unit;
 
+use Tests\TestCase;
 use Wikijump\Models\User;
-use PHPUnit\Framework\TestCase;
 
 class UserTest extends TestCase
 {
-
+    /**
+     * A basic test of the factory.
+     *
+     * @return void
+     */
+    public function test_models_can_be_instantiated()
+    {
+        $user = User::factory()->make();
+        $this->assertTrue($user instanceof User);
+    }
 }
