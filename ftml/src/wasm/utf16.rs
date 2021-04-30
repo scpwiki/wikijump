d92e7f0e0fe6c832a549aafd3f379b97da3a0e4b
Ammon Smith <ammon.i.smith@gmail.com>
2021-04-30 15:19:54 -0400

    Add Utf16IndexMap wrapper for WASM.

---
+++ ftml/src/wasm/utf16.rs
@@ -0,0 +1,67 @@
+/*
+ * wasm/utf16.rs
+ *
+ * ftml - Library to parse Wikidot text
+ * Copyright (C) 2019-2021 Wikijump Team
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+use super::prelude::*;
+use crate::Utf16IndexMap as RustUtf16IndexMap;
+use ouroboros::self_referencing;
+use std::sync::Arc;
+
+#[self_referencing]
+#[derive(Debug)]
+struct Utf16IndexMapInner {
+    text: String,
+
+    #[borrows(text)]
+    #[covariant]
+    inner: RustUtf16IndexMap<'this>,
+}
+
+#[wasm_bindgen]
+#[derive(Debug)]
+pub struct Utf16IndexMap {
+    inner: Arc<Utf16IndexMapInner>,
+}
+
+#[wasm_bindgen]
+impl Utf16IndexMap {
+    #[inline]
+    pub(crate) fn get(&self) -> &RustUtf16IndexMap {
+        self.inner.borrow_inner()
+    }
+
+    #[wasm_bindgen(constructor)]
+    pub fn new(text: String) -> Utf16IndexMap {
+        let inner = Utf16IndexMapInnerBuilder {
+            text,
+            inner_builder: |text: &str| RustUtf16IndexMap::new(text),
+        };
+
+        Utf16IndexMap {
+            inner: Arc::new(inner.build()),
+        }
+    }
+
+    #[wasm_bindgen]
+    pub fn copy(&self) -> Utf16IndexMap {
+        Utf16IndexMap {
+            inner: Arc::clone(&self.inner),
+        }
+    }
+}
