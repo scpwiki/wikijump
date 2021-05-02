7822bb794ed7658adf5306482e08663ac54ff999
Ross Williams <rossjrw@gmail.com>
2021-05-02 08:46:33 +0100

    Update Tabview component to use Svelte actions
    I have added comments noting where the structure and naming scheme of
    the tabview diverge from Wikidot's. Given that the old structure was so
    dependent on Yahoo UI, it did not make sense to retain these class
    names. Under normal circumstances I would have attempted to reproduce
    Wikidot's naming scheme where possible, however, in this case the actual
    structure remains intact.
    
    A simple 'Clickable' component has been added that is simply an anchor
    with a no-op href. I extracted this out to a component to quarantine its
    lint errors. This component will become decreasingly necessary as we
    phase out no-op anchors in favour of buttons.
    
    Throughout the Tabview components, 'tabNumber' has been used to refer to
    the index of each tab, as opposed to 'tabIndex' which could be ambiguous
    with the HTML feature.

---
+++ client/components/src/Clickable.svelte
@@ -0,0 +1,3 @@
+<a href="javascript:;">
+  <slot />
+</a>
---
+++ client/components/src/stubs/Tabview.svelte
@@ -0,0 +1,81 @@
+<script lang="ts">
+  import { contain } from "../lib/actions"
+  import Clickable from "../Clickable.svelte"
+
+  const DEFAULT_TAB_TITLE = "Unnamed tab"
+  const SELECTED_TAB_HOVER_TEXT = "active"
+
+  let thisElement: HTMLElement
+  let tabTitles: string[] = []
+  let tabContents: HTMLElement[] = []
+  let selectedTabNumber = 0
+
+  /**
+   * Sets the current tab selection.
+   *
+   * @param tabNumber - The tab index to select.
+   */
+  function selectTab(tabNumber: number) {
+    selectedTabNumber = tabNumber
+  }
+
+  /**
+   * Action that extracts the tab names and contents from the stub component.
+   *
+   * @param stub - The root element of the stubbed tabview.
+   */
+  const extractFromStub: SvelteAction = stub => {
+    const contents = stub.parentElement?.querySelectorAll<HTMLElement>(".tabview-content")
+    if (!contents) return
+    contents.forEach(contentElement => {
+      // Move them from the DOM into an array
+      contentElement.remove()
+      tabTitles = [...tabTitles, contentElement.dataset.title ?? DEFAULT_TAB_TITLE]
+      tabContents = [...tabContents, contentElement]
+    })
+  }
+</script>
+
+<!-- was #wiki-tabview-[hash].yui-navset.yui-navset-top -->
+<div class="tabview" use:extractFromStub>
+  <!-- was .yui-nav -->
+  <ul class="tabview-selectors">
+    {#each tabTitles as tabTitle, tabNumber}
+      <li
+        class="tabview-selector {tabNumber === selectedTabNumber ? 'selected' : ''}"
+        title={tabNumber === selectedTabNumber ? SELECTED_TAB_HOVER_TEXT : undefined}
+      >
+        <Clickable on:click={() => selectTab(tabNumber)}>
+          <!-- was em -->
+          {tabTitle}
+        </Clickable>
+      </li>
+    {/each}
+  </ul>
+  <!-- was .yui-content -->
+  <div class="tabview-contents">
+    {#each tabContents as tabContent, tabNumber}
+      <!-- was #wiki-tab-[id]-[id] -->
+      <div
+        use:contain={tabContent}
+        class="tabview-content {tabNumber === selectedTabNumber ? 'selected' : ''}"
+      />
+    {/each}
+  </div>
+</div>
+
+<style>
+  .tabview {
+    color: green;
+  }
+
+  .tabview-selector {
+    padding: 5px;
+    border: 1px solid black;
+    display: inline-block;
+  }
+
+  .tabview-content:not(.selected) {
+    display: none;
+  }
+</style>
