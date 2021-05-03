d2bd5706b5d8501f41933e3153a7438c8f0f0e8f
Monkatraz <34875062+Monkatraz@users.noreply.github.com>
2021-05-02 18:50:36 -0600

    use vite/client file references

--- client/misc/types/files.d.ts
+++ client/misc/types/files.d.ts
@@ -2,65 +2,8 @@
  *  @file Modules for correctly typing imported files.
  */
 
-// -- SPECIAL
-
-declare module "*.css"
-
-// -- VITE
-
-declare module "*?url" {
-  const text: string
-  export default text
-}
 
 declare module "*?bundled-worker" {
   const text: string
   export default text
-}
-
-// -- URL REFERENCES
-
-declare module "*.wasm" {
-  const url: string
-  export default url
-}
-
-declare module "*.svg" {
-  const url: string
-  export default url
-}
-
-declare module "*.bmp" {
-  const url: string
-  export default url
-}
-
-declare module "*.gif" {
-  const url: string
-  export default url
-}
-
-declare module "*.jpg" {
-  const url: string
-  export default url
-}
-
-declare module "*.jpeg" {
-  const url: string
-  export default url
-}
-
-declare module "*.png" {
-  const url: string
-  export default url
-}
-
-declare module "*.webp" {
-  const url: string
-  export default url
-}
-
-declare module "*.avif" {
-  const url: string
-  export default url
 }
