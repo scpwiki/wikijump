f72c8da348c1922247f970b360eb87680a6190ec
Monkatraz <34875062+Monkatraz@users.noreply.github.com>
2021-05-06 02:59:56 -0600

    move sheaf components to their own folder

--- client/modules/components/src/index.ts
+++ client/modules/components/src/index.ts
@@ -1,4 +1,4 @@
-export { default as Sheaf } from "./Sheaf.svelte"
-export { default as SheafPanel } from "./SheafPanel.svelte"
+export { default as Sheaf } from "./sheaf/Sheaf.svelte"
+export { default as SheafPanel } from "./sheaf/SheafPanel.svelte"
 export { default as Tabview } from "./Tabview.svelte"
 export { default as IntersectionPoint } from "./IntersectionPoint.svelte"
