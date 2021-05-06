e869dba62003c95d77434bc570171b9534f9d002
Monkatraz <34875062+Monkatraz@users.noreply.github.com>
2021-05-06 01:40:07 -0600

    use handy global-jsdom package for test shims

--- client/scripts/tests-shim.js
+++ client/scripts/tests-shim.js
@@ -1,11 +1,1 @@
-const { JSDOM } = require("jsdom")
-const fetch = require("node-fetch")
-const { performance } = require("perf_hooks")
-
-const DOM = new JSDOM("")
-
-globalThis.window = DOM.window
-globalThis.document = DOM.window.document
-globalThis.fetch = fetch
-globalThis.performance = performance
-globalThis.DOMParser = DOM.window.DOMParser
+require("global-jsdom/register")
