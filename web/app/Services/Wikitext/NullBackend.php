a128ac646e6b35fd191b950bcc931ad87b8cacb0
Ammon Smith <ammon.i.smith@gmail.com>
2021-05-06 21:16:19 -0400

    Add function version() to WikitextBackend.

--- web/app/Services/Wikitext/NullBackend.php
+++ web/app/Services/Wikitext/NullBackend.php
@@ -3,8 +3,9 @@ declare(strict_types = 1);
 
 namespace Wikijump\Services\Wikitext;
 
-
 class NullBackend implements WikitextBackend
 {
-
+    public function version(): string {
+        return 'null 0.0.0';
+    }
 }
--- web/app/Services/Wikitext/TextWikiBackend.php
+++ web/app/Services/Wikitext/TextWikiBackend.php
@@ -1,9 +1,13 @@
 <?php
+declare(strict_types = 1);
 
 namespace Wikijump\Services\Wikitext;
 
+use \Wikidot\Utils\WikiTransformation;
 
 class TextWikiBackend implements WikitextBackend
 {
-
+    public function version(): string {
+        return 'Text_Wiki 0.0.1';
+    }
 }
