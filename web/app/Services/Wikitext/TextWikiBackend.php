67a2ae27f10ba65d0411d234cae29e4743fea7a4
Ammon Smith <ammon.i.smith@gmail.com>
2021-05-06 21:20:49 -0400

    Construct WikiTransformation.

--- web/app/Services/Wikitext/TextWikiBackend.php
+++ web/app/Services/Wikitext/TextWikiBackend.php
@@ -7,6 +7,12 @@ use \Wikidot\Utils\WikiTransformation;
 
 class TextWikiBackend implements WikitextBackend
 {
+    private WikiTransformation $wt;
+
+    public function __construct() {
+        $this->wt = new WikiTransformation();
+    }
+
     public function version(): string {
         return 'Text_Wiki 0.0.1';
     }
