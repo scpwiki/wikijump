d6a940771b4cd6c893d40a0c1a9d018fe8667029
Monkatraz <34875062+Monkatraz@users.noreply.github.com>
2021-05-08 17:48:20 -0600

    compile tests with vite, enable coverage for components

--- client/scripts/build-tests.js
+++ client/scripts/build-tests.js
@@ -1,38 +1,35 @@
 const path = require("path")
-const fs = require("fs/promises")
+const fs = require("fs-extra")
 
 // make sure we're at root
 process.chdir(path.resolve(__dirname, "../"))
 
-const esbuild = require("esbuild")
+const vite = require("vite")
 const globby = require("globby")
 
+const svelte = require("@sveltejs/vite-plugin-svelte")
 const sveltePreprocess = require("svelte-preprocess")
-const { typescript } = require("svelte-preprocess-esbuild")
-const sveltePlugin = require("esbuild-svelte")
+const workerPlugin = require("./vite-plugin-bundled-worker.js")
+const tomlPlugin = require("./vite-plugin-toml.js")
 
 build()
 
-async function generateMegaBundleEntrypoint() {
-  const sourceFiles = await globby("modules/*/src/**/*.ts")
-  const testFiles = await globby("modules/*/tests/*.ts")
+const EXCLUDE = [/ftml-wasm-worker/]
 
-  // compile the entire monorepo into a dynamic import that won't get executed
-  // we do this so that the _entire monorepo_ gets bundled, but won't break anything
-  // this makes sure that code coverage and the like works
-  //
-  // the expression chosen here is designed to _always_ be false, but also
-  // not obviously false to an optimizer - this is so this section isn't
-  // tree-shaked or dead-coded out of the bundle (ruining the whole point of this)
-  let entrypoint = "if (!globalThis && 'obviously_not_undefined' === undefined) {\n"
+async function generateMegaBundleEntrypoint() {
+  const sourceFiles = await globby("modules/*/src/**/*.ts", { ignore: ["*.d.ts"] })
+  const testFiles = await globby("modules/*/tests/*.ts", { ignore: ["*.d.ts"] })
+
+  let entrypoint = "import '../scripts/tests-shim.js';\n"
+
   for (const file of sourceFiles) {
-    entrypoint += `  import(${JSON.stringify(`./${file}`)});\n`
+    if (EXCLUDE.some(regex => regex.test(file))) continue
+    entrypoint += `import ${JSON.stringify(`../${file}`)};\n`
   }
-  entrypoint += "}\n"
 
   // actually import the test files
   for (const file of testFiles) {
-    entrypoint += `import ${JSON.stringify(`./${file}`)};\n`
+    entrypoint += `import ${JSON.stringify(`../${file}`)};\n`
   }
 
   return entrypoint
@@ -42,53 +39,122 @@ async function build() {
   console.log(`[tests] Compiling test megabundle...`)
 
   const entrypoint = await generateMegaBundleEntrypoint()
+  const dir = path.resolve(process.cwd(), "tests-dist")
+  const file = path.resolve(dir, "test-megabundle.ts")
 
-  const result = await esbuild.build({
-    stdin: {
-      contents: entrypoint,
-      resolveDir: process.cwd(),
-      sourcefile: "test-megabundle.ts",
-      loader: "ts"
+  await fs.outputFile(file, entrypoint)
+
+  await vite.build({
+    root: dir,
+
+    esbuild: {
+      define: {
+        "import.meta.url": "__filename"
+      }
     },
-    // add other modules here if needed
-    external: [
-      "uvu",
-      // codemirror has CJS exports now, so this is ok
-      "@codemirror/*",
-      "lezer-tree",
-      // breaks things if included
-      "threads",
-      // large CJS-compatible dependencies (so we'll exclude them)
-      "@happy-dom/*",
-      "globby",
-      "fs-extra",
-      "@ltd/j-toml",
-      "svelte"
-    ],
-    inject: ["./scripts/tests-shim.js"],
-    outfile: "tests-dist/test-megabundle.cjs",
-    bundle: true,
-    treeShaking: true,
-    minify: false,
-    format: "cjs",
-    platform: "node",
-    sourcemap: true,
-    sourcesContent: false,
-    metafile: true,
-    loader: { ".wasm": "file", ".toml": "text", ".worker.ts": "text" },
-    logLevel: "error",
-    plugins: [
-      sveltePlugin({
-        compileOptions: { css: true, cssHash: () => "svelte" },
-        // render typescript using esbuild rather than tsc
-        preprocess: [typescript(), sveltePreprocess({ typescript: false })]
-      })
-    ]
-  })
 
-  // write metafile for examination purposes
-  // e.g. use with https://www.bundle-buddy.com
-  await fs.writeFile("tests-dist/meta.json", JSON.stringify(result.metafile))
+    css: {
+      preprocessorOptions: {
+        scss: { sourceMapEmbed: true, sourceMapContents: true, sourceMap: true }
+      }
+    },
+
+    plugins: [
+      workerPlugin(),
+      tomlPlugin(),
+      svelte({
+        compilerOptions: { css: true, cssHash: () => "svelte" },
+        preprocess: [
+          sveltePreprocess({
+            sass: { sourceMapEmbed: true, sourceMapContents: true, sourceMap: true }
+          })
+        ]
+      })
+    ],
+
+    build: {
+      lib: {
+        entry: file,
+        name: "test-megabundle",
+        fileName: "test-megabundle",
+        formats: ["cjs"]
+      },
+
+      sourcemap: true,
+      target: "node16",
+      minify: false,
+      brotliSize: false,
+      cssCodeSplit: false,
+
+      rollupOptions: {
+        treeshake: false,
+        output: {
+          interop: "esModule",
+          compact: true,
+          preferConst: true,
+          sourcemapExcludeSources: true
+        },
+        external: [
+          /uvu/,
+          // /vendor\/prism\.js/,
+          // codemirror has CJS exports now, so this is ok
+          /^@codemirror/,
+          "lezer-tree",
+          // breaks things if included
+          "threads",
+          // large CJS-compatible dependencies (so we'll exclude them)
+          /^@happy-dom/,
+          "globby",
+          "fs-extra",
+          "fs/promises",
+          "@ltd/j-toml",
+          "diff",
+          // from esbuild source:
+          // node builtins
+          "assert",
+          "async_hooks",
+          "buffer",
+          "child_process",
+          "cluster",
+          "console",
+          "constants",
+          "crypto",
+          "dgram",
+          "dns",
+          "domain",
+          "events",
+          "fs",
+          "http",
+          "http2",
+          "https",
+          "inspector",
+          "module",
+          "net",
+          "os",
+          "path",
+          "perf_hooks",
+          "process",
+          "punycode",
+          "querystring",
+          "readline",
+          "repl",
+          "stream",
+          "string_decoder",
+          "sys",
+          "timers",
+          "tls",
+          "trace_events",
+          "tty",
+          "url",
+          "util",
+          "v8",
+          "vm",
+          "worker_threads",
+          "zlib"
+        ]
+      }
+    }
+  })
 
   console.log("[tests] Megabundle compile complete.")
 }
