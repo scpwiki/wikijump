42595256b03e6c6c07100a64408f36b093609150
Monkatraz <34875062+Monkatraz@users.noreply.github.com>
2021-05-11 05:00:00 -0600

    cleanup test building

--- client/scripts/build-tests.js
+++ client/scripts/build-tests.js
@@ -1,86 +1,87 @@
 const path = require("path")
 const fs = require("fs-extra")
-
-// make sure we're at root
-process.chdir(path.resolve(__dirname, "../"))
-
 const vite = require("vite")
 const globby = require("globby")
-
 const svelte = require("@sveltejs/vite-plugin-svelte")
 const sveltePreprocess = require("svelte-preprocess")
 const workerPlugin = require("./vite-plugin-bundled-worker.js")
 const tomlPlugin = require("./vite-plugin-toml.js")
-
 const istanbul = require("./rollup-plugin-istanbul")
+
+// make sure we're at root
+process.chdir(path.resolve(__dirname, "../"))
+const DIR = path.resolve(process.cwd(), "tests-dist")
+const EXCLUDE = [/ftml-wasm-worker/]
 
 build()
 
-const EXCLUDE = [/ftml-wasm-worker/]
+// ----
 
-async function generateMegaBundleEntrypoint() {
-  const sourceFiles = await globby("modules/*/src/**/*.ts", { ignore: ["*.d.ts"] })
-  const testFiles = await globby("modules/*/tests/*.ts", { ignore: ["*.d.ts"] })
-
-  let entrypoint = "import '../scripts/tests-shim.js';\n"
-
-  for (const file of sourceFiles) {
+async function collectModules() {
+  const files = await globby("modules/*/src/**/*.ts", { ignore: ["*.d.ts"] })
+  let entrypoint = "import '../../scripts/tests-shim.js';\n"
+  for (const file of files) {
     if (EXCLUDE.some(regex => regex.test(file))) continue
     entrypoint += `import ${JSON.stringify(`../${file}`)};\n`
   }
+  return entrypoint
+}
 
-  // actually import the test files
-  for (const file of testFiles) {
+async function collectTests() {
+  const files = await globby("modules/*/tests/*.ts", { ignore: ["*.d.ts"] })
+  let entrypoint = "import '../../scripts/tests-shim.js';\n"
+  for (const file of files) {
     entrypoint += `import ${JSON.stringify(`../${file}`)};\n`
   }
-
   return entrypoint
+}
+
+async function generateMegaBundleEntrypoint() {
+  const moduleImport = await collectModules()
+  const testImport = await collectTests()
+
+  const modules = path.resolve(DIR, "megabundle-modules.ts")
+  const tests = path.resolve(DIR, "megabundle-tests.ts")
+  const entrypoint = path.resolve(DIR, "test-megabundle.ts")
+
+  // prettier-ignore
+  const src =
+    `import "./megabundle-tests";\n` +
+    `import "./megabundle-modules";`
+
+  await Promise.all([
+    fs.outputFile(modules, moduleImport),
+    fs.outputFile(tests, testImport),
+    fs.outputFile(entrypoint, src)
+  ])
+
+  return { entrypoint, modules, tests }
 }
 
 async function build() {
   console.log(`[tests] Compiling test megabundle...`)
 
-  const entrypoint = await generateMegaBundleEntrypoint()
-  const dir = path.resolve(process.cwd(), "tests-dist")
-  const file = path.resolve(dir, "test-megabundle.ts")
-
-  await fs.outputFile(file, entrypoint)
+  const { entrypoint, modules, tests } = await generateMegaBundleEntrypoint()
 
   await vite.build({
-    root: dir,
+    root: DIR,
 
     define: {
       "import.meta.url": '"file://test-megabundle"'
-    },
-
-    css: {
-      preprocessorOptions: {
-        scss: { sourceMapEmbed: true, sourceMapContents: true, sourceMap: true }
-      }
     },
 
     plugins: [
       workerPlugin(),
       tomlPlugin(),
       svelte({
+        emitCss: false,
         compilerOptions: { cssHash: () => "svelte" },
-        preprocess: [
-          sveltePreprocess({
-            sourceMap: true,
-            scss: { sourceMapEmbed: true, sourceMapContents: true, sourceMap: true }
-          })
-        ]
+        preprocess: [sveltePreprocess({ sourceMap: true })]
       })
     ],
 
     build: {
-      lib: {
-        entry: file,
-        name: "test-megabundle",
-        fileName: "test-megabundle",
-        formats: ["cjs"]
-      },
-
+      assetsDir: "./",
       sourcemap: true,
       target: "esnext",
       minify: false,
@@ -88,6 +89,7 @@ async function build() {
       cssCodeSplit: false,
 
       rollupOptions: {
+        input: [entrypoint],
         plugins: [
           // because esbuild rips out comments, esbuild will not preserve
           // the comments we need for ignoring lines
@@ -110,12 +112,12 @@ async function build() {
             extensions: [".ts", ".tsx", ".svelte"],
             // speeds things up or lowers file size
             skipPreflightCheck: true,
-            compact: true,
-            retainLines: true
+            compact: true
           })
         ],
 
         output: {
+          format: "cjs",
           interop: "esModule",
           sourcemapExcludeSources: true
         },
@@ -130,15 +132,17 @@ async function build() {
 }
 
 const EXTERNAL = [
+  // externalize this so that it gets required first
+  // this has required polyfills, so it needs to run first
+  "../../scripts/tests-shim.js",
   /uvu/,
-  // /vendor\/prism\.js/,
   // codemirror has CJS exports now, so this is ok
   /^@codemirror/,
   "lezer-tree",
   // breaks things if included
   "threads",
   // large CJS-compatible dependencies (so we'll exclude them)
-  /^@happy-dom/,
+  /happy-dom/,
   "globby",
   "fs-extra",
   "fs/promises",
