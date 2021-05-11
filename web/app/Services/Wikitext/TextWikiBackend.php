4875e95a56b7099fda279edc37fe4fbbf912d1c5
Ammon Smith <ammon.i.smith@gmail.com>
2021-05-11 16:31:27 -0400

    Set WikiTransformation page if it has PageInfo.

--- web/app/Services/Wikitext/TextWikiBackend.php
+++ web/app/Services/Wikitext/TextWikiBackend.php
@@ -3,6 +3,8 @@ declare(strict_types = 1);
 
 namespace Wikijump\Services\Wikitext;
 
+use \Wikidot\DB\Page;
+use \Wikidot\DB\Site;
 use \Wikidot\Utils\WikiTransformation;
 
 class TextWikiBackend implements WikitextBackend
@@ -11,10 +13,25 @@ class TextWikiBackend implements WikitextBackend
 
     public function __construct(string $mode, ?PageInfo $pageInfo) {
         $this->wt = new WikiTransformation();
-        // TODO
+
+        if (!is_null($pageInfo)) {
+            $site = self::getSite($pageInfo->site);
+            $page = self::getPage($site->getSiteId(), $pageInfo->page);
+            $this->wt->setPage($page);
+        }
     }
 
     public function version(): string {
         return 'Text_Wiki 0.0.1';
+    }
+
+    private static function getSite(string $siteSlug): Site {
+        $c = new Criteria();
+        $c->add('unix_name', $siteSlug);
+        return SitePeer::instance()->selectOne($c);
+    }
+
+    private static function getPage(int $siteId, string $pageSlug): Page {
+        return PagePeer::instance()->selectByName($siteId, $pageSlug);
     }
 }
