7bc051be7de760e1b74562dba726e5d8798a5c3a
pxdnbluesoul <github@nags.me>
2021-05-11 10:31:56 -0600

    Remove foreign key constraints on artisan migrate down

--- web/database/migrations/2021_05_05_185149_add_foreign_key_constraints.php
+++ web/database/migrations/2021_05_05_185149_add_foreign_key_constraints.php
@@ -287,6 +287,150 @@ class AddForeignKeyConstraints extends Migration
      */
     public function down()
     {
-        //
+
+        Schema::table('admin', function (Blueprint $table) {
+            $table->dropForeign('admin_site_id_foreign');
+        });
+        Schema::table('admin_notification', function (Blueprint $table) {
+            $table->dropForeign('admin_notification_site_id_foreign');
+        });
+        Schema::table('category', function (Blueprint $table) {
+            $table->dropForeign('category_site_id_foreign');
+        });
+        Schema::table('domain_redirect', function (Blueprint $table) {
+            $table->dropForeign('domain_redirect_site_id_foreign');
+        });
+        Schema::table('email_invitation', function (Blueprint $table) {
+            $table->dropForeign('email_invitation_site_id_foreign');
+        });
+        Schema::table('file', function (Blueprint $table) {
+            $table->dropForeign('file_page_id_foreign');
+        });
+        Schema::table('file', function (Blueprint $table) {
+            $table->dropForeign('file_site_id_foreign');
+        });
+        Schema::table('forum_category', function (Blueprint $table) {
+            $table->dropForeign('forum_category_group_id_foreign');
+        });
+        Schema::table('forum_category', function (Blueprint $table) {
+            $table->dropForeign('forum_category_last_post_id_foreign');
+        });
+        Schema::table('forum_category', function (Blueprint $table) {
+            $table->dropForeign('forum_category_site_id_foreign');
+        });
+        Schema::table('forum_group', function (Blueprint $table) {
+            $table->dropForeign('forum_group_site_id_foreign');
+        });
+        Schema::table('forum_post', function (Blueprint $table) {
+            $table->dropForeign('forum_post_site_id_foreign');
+        });
+        Schema::table('forum_settings', function (Blueprint $table) {
+            $table->dropForeign('forum_settings_site_id_foreign');
+        });
+        Schema::table('forum_thread', function (Blueprint $table) {
+            $table->dropForeign('forum_thread_site_id_foreign');
+        });
+        Schema::table('forum_thread', function (Blueprint $table) {
+            $table->dropForeign('forum_thread_category_id_foreign');
+        });
+        Schema::table('forum_thread', function (Blueprint $table) {
+            $table->dropForeign('forum_thread_page_id_foreign');
+        });
+        Schema::table('forum_thread', function (Blueprint $table) {
+            $table->dropForeign('forum_thread_last_post_id_foreign');
+        });
+        Schema::table('front_forum_feed', function (Blueprint $table) {
+            $table->dropForeign('front_forum_feed_page_id_foreign');
+        });
+        Schema::table('front_forum_feed', function (Blueprint $table) {
+            $table->dropForeign('front_forum_feed_site_id_foreign');
+        });
+        Schema::table('ip_block', function (Blueprint $table) {
+            $table->dropForeign('ip_block_site_id_foreign');
+        });
+        Schema::table('log_event', function (Blueprint $table) {
+            $table->dropForeign('log_event_site_id_foreign');
+        });
+        Schema::table('member', function (Blueprint $table) {
+            $table->dropForeign('member_site_id_foreign');
+        });
+        Schema::table('member_application', function (Blueprint $table) {
+            $table->dropForeign('member_application_site_id_foreign');
+        });
+        Schema::table('member_invitation', function (Blueprint $table) {
+            $table->dropForeign('member_invitation_site_id_foreign');
+        });
+        Schema::table('moderator', function (Blueprint $table) {
+            $table->dropForeign('moderator_site_id_foreign');
+        });
+        Schema::table('page', function (Blueprint $table) {
+            $table->dropForeign('page_parent_page_id_foreign');
+        });
+        Schema::table('page', function (Blueprint $table) {
+            $table->dropForeign('page_site_id_foreign');
+        });
+        Schema::table('page_abuse_flag', function (Blueprint $table) {
+            $table->dropForeign('page_abuse_flag_site_id_foreign');
+        });
+        Schema::table('page_compiled', function (Blueprint $table) {
+            $table->dropForeign('page_compiled_page_id_foreign');
+        });
+        Schema::table('page_edit_lock', function (Blueprint $table) {
+            $table->dropForeign('page_edit_lock_page_id_foreign');
+        });
+        Schema::table('page_inclusion', function (Blueprint $table) {
+            $table->dropForeign('page_inclusion_included_page_id_foreign');
+        });
+        Schema::table('page_inclusion', function (Blueprint $table) {
+            $table->dropForeign('page_inclusion_including_page_id_foreign');
+        });
+        Schema::table('page_link', function (Blueprint $table) {
+            $table->dropForeign('page_link_from_page_id_foreign');
+        });
+        Schema::table('page_link', function (Blueprint $table) {
+            $table->dropForeign('page_link_to_page_id_foreign');
+        });
+        Schema::table('page_rate_vote', function (Blueprint $table) {
+            $table->dropForeign('page_rate_vote_page_id_foreign');
+        });
+        Schema::table('page_tag', function (Blueprint $table) {
+            $table->dropForeign('page_tag_page_id_foreign');
+        });
+        Schema::table('site_backup', function (Blueprint $table) {
+            $table->dropForeign('site_backup_site_id_foreign');
+        });
+        Schema::table('site_settings', function (Blueprint $table) {
+            $table->dropForeign('site_settings_site_id_foreign');
+        });
+        Schema::table('site_super_settings', function (Blueprint $table) {
+            $table->dropForeign('site_super_settings_site_id_foreign');
+        });
+        Schema::table('site_tag', function (Blueprint $table) {
+            $table->dropForeign('site_tag_site_id_foreign');
+        });
+        Schema::table('site_viewer', function (Blueprint $table) {
+            $table->dropForeign('site_viewer_site_id_foreign');
+        });
+        Schema::table('theme', function (Blueprint $table) {
+            $table->dropForeign('theme_site_id_foreign');
+        });
+        Schema::table('ucookie', function (Blueprint $table) {
+            $table->dropForeign('ucookie_session_id_foreign');
+        });
+        Schema::table('ucookie', function (Blueprint $table) {
+            $table->dropForeign('ucookie_site_id_foreign');
+        });
+        Schema::table('user_abuse_flag', function (Blueprint $table) {
+            $table->dropForeign('user_abuse_flag_site_id_foreign');
+        });
+        Schema::table('user_block', function (Blueprint $table) {
+            $table->dropForeign('user_block_site_id_foreign');
+        });
+        Schema::table('watched_forum_thread', function (Blueprint $table) {
+            $table->dropForeign('watched_forum_thread_thread_id_foreign');
+        });
+        Schema::table('watched_page', function (Blueprint $table) {
+            $table->dropForeign('watched_page_page_id_foreign');
+        });
     }
 }
