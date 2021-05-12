8b23a2df1966c94f7abac0d3e78f8d961453984c
Ammon Smith <ammon.i.smith@gmail.com>
2021-05-11 22:44:01 -0400

    Add ParseRenderMode::TABLE_OF_CONTENTS.

--- web/app/Services/Wikitext/ParseRenderMode.php
+++ web/app/Services/Wikitext/ParseRenderMode.php
@@ -8,19 +8,21 @@ use \Wikijump\Common\Enum;
 /**
  * Enum ParseRenderMode, for representing the context in which parsing and rendering is being carried out.
  *
- * FEED           -- RSS feeds
- * LIST           -- ListPages module and other dynamic lists
- * PAGE           -- Compiling a regular page
- * FORUM_POST     -- Compiling a forum post
- * DIRECT_MESSAGE -- Compiling a direct message
+ * PAGE              -- Compiling a regular page
+ * FORUM_POST        -- Compiling a forum post
+ * DIRECT_MESSAGE    -- Compiling a direct message
+ * FEED              -- RSS feeds
+ * LIST              -- ListPages module and other dynamic lists
+ * TABLE_OF_CONTENTS -- Producing a table of contents source mapping
  *
  * @package Wikijump\Services\Wikitext
  */
 final class ParseRenderMode extends Enum
 {
-    const FEED = 0;
-    const LIST = 1;
-    const PAGE = 2;
-    const FORUM_POST = 3;
-    const DIRECT_MESSAGE = 4;
+    const PAGE = 0;
+    const FORUM_POST = 1;
+    const DIRECT_MESSAGE = 2;
+    const FEED = 3;
+    const LIST = 4;
+    CONST TABLE_OF_CONTENTS = 5;
 }
