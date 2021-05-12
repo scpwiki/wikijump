91cf2cc82a48b8f9c3bb24af392ea84d9b492da8
Ammon Smith <ammon.i.smith@gmail.com>
2021-05-11 23:09:50 -0400

    Rename DummyBackend and give it some basic output.

--- web/app/Services/Wikitext/DummyBackend.php
+++ web/app/Services/Wikitext/DummyBackend.php
@@ -3,20 +3,20 @@ declare(strict_types = 1);
 
 namespace Wikijump\Services\Wikitext;
 
-class NullBackend implements WikitextBackend
+class DummyBackend implements WikitextBackend
 {
     public function renderHtml(string $wikitext): HtmlOutput
     {
-        return new HtmlOutput('', '', [], []);
+        return new HtmlOutput('<span class="output">(Rendered HTML here)</span>', '.output { color: green; }', [], []);
     }
 
     public function renderText(string $wikitext): TextOutput
     {
-        return new TextOutput('', []);
+        return new TextOutput('(Rendered text here)', []);
     }
 
     public function version(): string
     {
-        return 'null 0.0.0';
+        return 'dummy 0.0.0';
     }
 }
