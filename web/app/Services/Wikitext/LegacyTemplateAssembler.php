ce1ca8ae5ff4838a4c5a0d0e1fe5c579516c48b7
Ammon Smith <ammon.i.smith@gmail.com>
2021-05-11 23:50:22 -0400

    Split out template assembly into its own file.

---
+++ web/app/Services/Wikitext/LegacyTemplateAssembler.php
@@ -0,0 +1,219 @@
+<?php
+
+namespace Wikijump\Services\Wikitext;
+
+use Ozone\Framework\Database\Criteria;
+use Wikidot\DB\ForumThreadPeer;
+use Wikidot\DB\PageTagPeer;
+use Wikidot\Utils\GlobalProperties;
+use Wikijump\Helpers\LegacyTools;
+use Wikijump\Models\User;
+
+/**
+ * Legacy class to assemble _template files.
+ * This just copies the logic originally held in WikiTransformation.
+ * It's a big mess. TODO clean this nonsense up.
+ * @package Wikijump\Services\Wikitext
+ */
+final class LegacyTemplateAssembler
+{
+    private function __construct() {}
+
+    public static function assembleTemplate($source, $template, $page = null)
+    {
+        /* First check if it is a real "live" template. If not, return the original $source.
+         * To be recognized as a live template it mast contain either %%content%% or
+         * %%content{X}%% tags. */
+
+        /* Handle ListPages module inside a template -- %%content%% need to be escaped. */
+        $template = preg_replace_callback(
+            ";^\\[\\[module\\s+ListPages(.*?)\n\\[\\[/module\\]\\];ms",
+            array(self, 'assemblyTemplateHandleListPages'),
+            $template
+        );
+        $template = preg_replace_callback(
+            ";^\\[\\[module\\s+NextPage(.*?)\n\\[\\[/module\\]\\];ms",
+            array(self, 'assemblyTemplateHandleListPages'),
+            $template
+        );
+        $template = preg_replace_callback(
+            ";^\\[\\[module\\s+PreviousPage(.*?)\n\\[\\[/module\\]\\];ms",
+            array(self, 'assemblyTemplateHandleListPages'),
+            $template
+        );
+        $template = preg_replace_callback(
+            ";^\\[\\[module\\s+Feed(.*?)\n\\[\\[/module\\]\\];ms",
+            array(self, 'assemblyTemplateHandleListPages'),
+            $template
+        );
+        $template = preg_replace_callback(
+            ";^\\[\\[module\\s+FrontForum(.*?)\n\\[\\[/module\\]\\];ms",
+            array(self, 'assemblyTemplateHandleListPages'),
+            $template
+        );
+
+        if (!preg_match(';%%content({[0-9]+})?%%;', $template)) {
+            return $source;
+        }
+        $out = $source;
+
+        $template = preg_replace(';%%content({[0-9]+})?%%;', '%%%\\0%%%', $template);
+        $template = preg_replace(';(?<!%)%%[a-z0-9\(\)_]+%%(?!%);i', '%%%\\0%%%', $template);
+        $template = preg_replace(';(?<!%)%%date(\|.*?)?%%(?!%);i', '%%%\\0%%%', $template);
+
+        $template = preg_replace(";%\xFA%(content({[0-9]+}))?%\xFA%;", "%%\\1%%", $template);
+        $template = preg_replace(";%\xFA%([a-z0-9\(\)_]+)%\xFA%;i", '%%\\1%%', $template);
+        $template = preg_replace(";%\xFA%(date(\|.*?)?)%\xFA%;i", '%%\\1%%', $template);
+
+        /* Check if has a ===== delimiter. */
+        $split = preg_split(';^={4,}$;sm', $template);
+        if (count($split) > 1) {
+            $template = trim($split[0]);
+        }
+
+        /* If there is $page, try substituting more tags. */
+        if ($page) {
+            $b = $template;
+            $title = $page->getTitle();
+            $title = str_replace(array('[', ']'), '', $title);
+            $b = str_replace('%%%%%title%%%%%', $title, $b);
+            $b = preg_replace(
+                ';%%%%%((linked_title)|(title_linked))%%%%%;i',
+                preg_quote_replacement('[[[' . $page->getUnixName() . ' | ' . $title . ']]]'),
+                $b
+            );
+
+            if ($page->getOwnerUserId()) {
+                $user = User::find($page->getOwnerUserId());
+                if (LegacyTools::isSystemAccount($user->id) === false) {
+                    $userString = '[[*user ' . $user->username . ']]';
+                } else {
+                    $userString = _('Anonymous user');
+                }
+            } else {
+                $userString = _('Anonymous user');
+            }
+            $b = str_ireplace('%%%%%author%%%%%', $userString, $b);
+            $b = str_ireplace('%%%%%user%%%%%', $userString, $b);
+
+            $b = str_ireplace('%%%%%user_edited%%%%%', $userString, $b);
+
+            $b = preg_replace(
+                ';%%%%%date(\|.*?)?%%%%%;',
+                '%%%%%date|' . $page->getDateCreated()->getTimestamp() . '\\1%%%%%',
+                $b
+            );
+            $b = preg_replace(
+                ';%%%%%date_edited(\|.*?)?%%%%%;',
+                '%%%%%date|' . $page->getDateLastEdited()->getTimestamp() . '\\1%%%%%',
+                $b
+            );
+
+            /* %%rating%% */
+            $b = str_ireplace('%%%%%rating%%%%%', $page->getRate(), $b);
+
+            /* %%comments%% */
+            $b = preg_replace_callback(
+                '/%%%%%comments%%%%%/i',
+                array(self, '_handleCommentsCount'),
+                $b
+            );
+
+            /* %%page_unix_name%% */
+            $b = str_ireplace('%%%%%page_unix_name%%%%%', $page->getUnixName(), $b);
+
+            if (strpos($page->getUnixName(), ':') != false) {
+                $tmp0 = explode(':', $page->getUnixName());
+                $categoryName00 = $tmp0[0];
+            } else {
+                $categoryName00 = '_default';
+            }
+
+            $b = str_ireplace('%%%%%category%%%%%', $categoryName00, $b);
+
+            /* %%link%% */
+            $site = $page->getSite();
+            $b = str_ireplace(
+                '%%%%%link%%%%%',
+                GlobalProperties::$HTTP_SCHEMA . '://' . $site->getDomain() . '/' . $page->getUnixName(),
+                $b
+            );
+
+            /* %%tags%% */
+            $b = preg_replace_callback(
+                '/%%%%%tags%%%%%/i',
+                array(self, 'handleTags'),
+                $b
+            );
+
+            $b = preg_replace_callback(
+                ';%%%%%date\|([0-9]+)(\|.*?)?%%%%%;',
+                array(self, 'formatData'),
+                $b
+            );
+
+            $template = $b;
+        }
+
+        $out = str_replace('%%%%%content%%%%%', trim($out), $template);
+
+        /* Handle split sources. */
+        $splitSource = preg_split('/^([=]{4,})$/m', $source);
+        for ($i = 0; $i < count($splitSource); $i++) {
+            $out = str_replace('%%%%%content{' . ($i + 1) . '}%%%%%', trim($splitSource[$i]), $out);
+        }
+        return preg_replace(';%%%%%content({[0-9]+})?%%%%%;', '', $out);
+    }
+
+    private static function _assemblyTemplateHandleListPages($m)
+    {
+        if (preg_match(';^\[\[module;sm', $m[1])) {
+            return $m[0];
+        } else {
+            $b = preg_replace(';%%(content({[0-9]+}))?%%;', "%\xFA%\\1%\xFA%", $m[0]);
+            $b = preg_replace(';(?<!%)%%([a-z0-9\(\)_]+)%%(?!%);i', "%\xFA%\\1%\xFA%", $b);
+            $b = preg_replace(';(?<!%)%%(date(\|.*?)?)%%(?!%);i', "%\xFA%\\1%\xFA%", $b);
+            return $b;
+        }
+    }
+
+    private static function handleCommentsCount($m, $page)
+    {
+        $threadId = $page->getThreadId();
+        if ($threadId) {
+            $thread = ForumThreadPeer::instance()->selectByPrimaryKey($threadId);
+            if ($thread) {
+                return $thread->getNumberPosts();
+            }
+        }
+        return 0;
+    }
+
+    private static function formatDate($m)
+    {
+        if (isset($m[2])) {
+            $format = preg_replace(';^\|;', '', $m[2]);
+        } else {
+            $format = '%e %b %Y, %H:%M %Z|agohover';
+        }
+        return '[[date ' . $m[1] . ' format="' . $format . '"' . ']]';
+    }
+
+    private static function handleTags($m, $page)
+    {
+        /* Select tags. */
+        // get the tags
+        $c = new Criteria();
+        $c->add('page_id', $page->getPageId());
+        $c->addOrderAscending('tag');
+        $tags = PageTagPeer::instance()->select($c);
+        $t2 = [];
+        foreach ($tags as $t) {
+            $t2[] = $t->getTag();
+        }
+        if (count($t2) == 0) {
+            return _('//no tags found for this page//');
+        }
+        return implode(' ', $t2);
+    }
+}
--- web/php/Utils/WikiTransformation.php
+++ web/php/Utils/WikiTransformation.php
@@ -6,12 +6,7 @@ use Ozone\Framework\Database\Criteria;
 use Ozone\Framework\Database\Database;
 use Ozone\Framework\Ozone;
 use Text_Wiki;
-
-use Wikidot\DB\ForumThreadPeer;
 use Wikidot\DB\PageTagPeer;
-use Wikijump\Helpers\LegacyTools;
-use Wikijump\Models\User;
-use Wikijump\Services\Wikitext\HtmlUtilities;
 
 class WikiTransformation
 {
@@ -28,8 +23,6 @@ class WikiTransformation
     private $transformationFormat = 'xhtml';
 
     public $wiki;
-
-    private $_tmpPage;
 
     public function __construct($initialize = true)
     {
@@ -66,162 +59,6 @@ class WikiTransformation
 
             $wiki->disableRule('wikilink');
             $this->wiki = $wiki;
-        }
-    }
-
-    public function assemblyTemplate($source, $template, $page = null)
-    {
-        /* First check if it is a real "live" template. If not, return the original $source.
-         * To be recognized as a live template it mast contain either %%content%% or
-         * %%content{X}%% tags. */
-
-        /* Handle ListPages module inside a template -- %%content%% need to be escaped. */
-        $template = preg_replace_callback(";^\\[\\[module\\s+ListPages(.*?)\n\\[\\[/module\\]\\];ms", array($this, '_assemblyTemplateHandleListPages'), $template);
-        $template = preg_replace_callback(";^\\[\\[module\\s+NextPage(.*?)\n\\[\\[/module\\]\\];ms", array($this, '_assemblyTemplateHandleListPages'), $template);
-        $template = preg_replace_callback(";^\\[\\[module\\s+PreviousPage(.*?)\n\\[\\[/module\\]\\];ms", array($this, '_assemblyTemplateHandleListPages'), $template);
-        $template = preg_replace_callback(";^\\[\\[module\\s+Feed(.*?)\n\\[\\[/module\\]\\];ms", array($this, '_assemblyTemplateHandleListPages'), $template);
-        $template = preg_replace_callback(";^\\[\\[module\\s+FrontForum(.*?)\n\\[\\[/module\\]\\];ms", array($this, '_assemblyTemplateHandleListPages'), $template);
-
-        if (!preg_match(';%%content({[0-9]+})?%%;', $template)) {
-            return $source;
-        }
-        $out = $source;
-
-        $template = preg_replace(';%%content({[0-9]+})?%%;', '%%%\\0%%%', $template);
-        $template = preg_replace(';(?<!%)%%[a-z0-9\(\)_]+%%(?!%);i', '%%%\\0%%%', $template);
-        $template = preg_replace(';(?<!%)%%date(\|.*?)?%%(?!%);i', '%%%\\0%%%', $template);
-
-        $template = preg_replace(";%\xFA%(content({[0-9]+}))?%\xFA%;", "%%\\1%%", $template);
-        $template = preg_replace(";%\xFA%([a-z0-9\(\)_]+)%\xFA%;i", '%%\\1%%', $template);
-        $template = preg_replace(";%\xFA%(date(\|.*?)?)%\xFA%;i", '%%\\1%%', $template);
-
-        /* Check if has a ===== delimiter. */
-        $split = preg_split(';^={4,}$;sm', $template);
-        if (count($split) > 1) {
-            $template = trim($split[0]);
-        }
-
-        /* If there is $page, try substituting more tags. */
-        if ($page) {
-            $this->_tmpPage = $page;
-            $b = $template;
-            $title = $page->getTitle();
-            $title = str_replace(array('[',']'), '', $title);
-            $b = str_replace('%%%%%title%%%%%', $title, $b);
-            $b = preg_replace(";%%%%%((linked_title)|(title_linked))%%%%%;i", preg_quote_replacement('[[[' . $page->getUnixName() . ' | ' . $title . ']]]'), $b);
-
-
-            if ($page->getOwnerUserId()) {
-                $user = User::find($page->getOwnerUserId());
-                if (LegacyTools::isSystemAccount($user->id) === false) {
-                    $userString = '[[*user ' . $user->username . ']]';
-                } else {
-                    $userString = _('Anonymous user');
-                }
-            } else {
-                $userString = _('Anonymous user');
-            }
-            $b = str_ireplace("%%%%%author%%%%%", $userString, $b);
-            $b = str_ireplace("%%%%%user%%%%%", $userString, $b);
-
-            $b = str_ireplace("%%%%%user_edited%%%%%", $userString, $b);
-
-            $b = preg_replace(';%%%%%date(\|.*?)?%%%%%;', '%%%%%date|' . $page->getDateCreated()->getTimestamp() . '\\1%%%%%', $b);
-            $b = preg_replace(';%%%%%date_edited(\|.*?)?%%%%%;', '%%%%%date|' . $page->getDateLastEdited()->getTimestamp() . '\\1%%%%%', $b);
-
-            /* %%rating%% */
-            $b = str_ireplace('%%%%%rating%%%%%', $page->getRate(), $b);
-
-             /* %%comments%% */
-            $b = preg_replace_callback("/%%%%%comments%%%%%/i", array(
-                $this, '_handleComementsCount'), $b);
-
-            /* %%page_unix_name%% */
-            $b = str_ireplace('%%%%%page_unix_name%%%%%', $page->getUnixName(), $b);
-
-            if (strpos($page->getUnixName(), ":") != false) {
-                $tmp0 = explode(':', $page->getUnixName());
-                $categoryName00 = $tmp0[0];
-            } else {
-                $categoryName00 = "_default";
-            }
-
-            $b = str_ireplace('%%%%%category%%%%%', $categoryName00, $b);
-
-            /* %%link%% */
-            $site = $page->getSite();
-            $b = str_ireplace('%%%%%link%%%%%', GlobalProperties::$HTTP_SCHEMA . "://" . $site->getDomain().'/'.$page->getUnixName(), $b);
-
-            /* %%tags%% */
-            $b = preg_replace_callback("/%%%%%tags%%%%%/i", array(
-                $this, '_handleTags'), $b);
-
-            $b = preg_replace_callback(';%%%%%date\|([0-9]+)(\|.*?)?%%%%%;', array(
-            $this, '_formatDate'), $b);
-
-            $template = $b;
-        }
-
-        $out = str_replace('%%%%%content%%%%%', trim($out), $template);
-        /* Handle split sources. */
-        $splitSource = preg_split('/^([=]{4,})$/m', $source);
-        for ($i = 0; $i < count($splitSource); $i++) {
-            $out = str_replace('%%%%%content{'.($i+1).'}%%%%%', trim($splitSource[$i]), $out);
-        }
-        return preg_replace(';%%%%%content({[0-9]+})?%%%%%;', '', $out);
-    }
-
-    private function _formatDate($m)
-    {
-        if (isset($m[2])) {
-            $format = preg_replace(';^\|;', '', $m[2]);
-        } else {
-            $format = '%e %b %Y, %H:%M %Z|agohover';
-        }
-        return '[[date ' . $m[1] . ' format="'.$format.'"' . ']]';
-    }
-
-    private function _handleComementsCount($m)
-    {
-        $page = $this->_tmpPage;
-        $threadId = $page->getThreadId();
-        if ($threadId) {
-            $thread = ForumThreadPeer::instance()->selectByPrimaryKey($threadId);
-            if ($thread) {
-                return $thread->getNumberPosts();
-            }
-        }
-        return 0;
-    }
-
-    private function _handleTags($m)
-    {
-        $page = $this->_tmpPage;
-        /* Select tags. */
-        // get the tags
-        $c = new Criteria();
-        $c->add("page_id", $page->getPageId());
-        $c->addOrderAscending("tag");
-        $tags = PageTagPeer::instance()->select($c);
-        $t2 = array();
-        foreach ($tags as $t) {
-            $t2[] = $t->getTag();
-        }
-        if (count($t2) == 0) {
-            return _('//no tags found for this page//');
-        }
-        return implode(' ', $t2);
-    }
-
-    private function _assemblyTemplateHandleListPages($m)
-    {
-        if (preg_match(';^\[\[module;sm', $m[1])) {
-            return $m[0];
-        } else {
-            $b = preg_replace(';%%(content({[0-9]+}))?%%;', "%\xFA%\\1%\xFA%", $m[0]);
-            $b = preg_replace(';(?<!%)%%([a-z0-9\(\)_]+)%%(?!%);i', "%\xFA%\\1%\xFA%", $b);
-            $b = preg_replace(';(?<!%)%%(date(\|.*?)?)%%(?!%);i', "%\xFA%\\1%\xFA%", $b);
-            return $b;
         }
     }
 
