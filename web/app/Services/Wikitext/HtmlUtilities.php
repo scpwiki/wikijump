a2149b53d516526c8bfdd7caa420a1cc89590690
Ammon Smith <ammon.i.smith@gmail.com>
2021-05-11 22:09:00 -0400

    Refactor HtmlUtilities::purify().
    Horribly overwrought piece of Wikidot jank.

--- web/app/Services/Wikitext/HtmlUtilities.php
+++ web/app/Services/Wikitext/HtmlUtilities.php
@@ -5,26 +5,16 @@ namespace Wikijump\Services\Wikitext;
 
 class HtmlUtilities
 {
-    // copied from Wikidot, TODO refactor
     public static function purify(string $html): string
     {
-        //$html is not a complete page so we need to wrap it!
-        $head = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"><html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">  <head>    <title>Just A Wrapper</title><meta http-equiv="content-type" content="text/html;charset=UTF-8"/>  </head> <!--wrapdelimiter--><body>';
-        $tail = ' </body><!--wrapdelimiter--></html>';
+        $tidyConfig = [
+            // We don't want the <html> or <head>, just the cleaned-up HTML body
+            'show-body-only' => true,
 
-        $c = $head . $html . $tail;
-        $config = array(
-            'indent' => false,
-            'output-xhtml' => true,
-            'wrap' => 0
-        );
+            // Disable long-line wrapping
+            'wrap' => 0,
+        ];
 
-        $c2 = tidy_parse_string($c, $config, 'UTF8');
-
-        $arr = explode("<!--wrapdelimiter-->", $c2);
-        $out = $arr[1];
-        $out = str_replace("<body>", "", $out);
-        $out = str_replace("</body>", "", $out);
-        return $out;
+        return tidy_repair_string($html, $tidyConfig);
     }
 }
