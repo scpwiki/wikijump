44675400d5ecb7760a568a6a4b1cd9011a0f01b7
Monkatraz <34875062+Monkatraz@users.noreply.github.com>
2021-05-16 11:53:33 -0600

    add ported tabview system

--- client/modules/components/src/index.ts
+++ client/modules/components/src/index.ts
@@ -1,8 +1,11 @@
 export { default as Button } from "./Button.svelte"
 export { default as Card } from "./Card.svelte"
 export { default as Icon } from "./Icon.svelte"
-export { default as Tabview } from "./Tabview.svelte"
+export { default as Tab } from "./tabs/Tab.svelte"
+export { default as Tabview } from "./tabs/Tabview.svelte"
 export { default as IntersectionPoint } from "./IntersectionPoint.svelte"
+
+export { default as TabviewStub } from "./Tabview.svelte"
 
 export * from "./lib/focus"
 export * from "./lib/tippy"
