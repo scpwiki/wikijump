0a5bbc0d0b3d447916327e96fb28a1c256e93e97
Monkatraz <34875062+Monkatraz@users.noreply.github.com>
2021-05-17 16:22:10 -0600

    add matchmedia shim

--- client/scripts/tests-shim.js
+++ client/scripts/tests-shim.js
@@ -1,3 +1,12 @@
 const { GlobalRegistrator } = require("@happy-dom/global-registrator")
 
 GlobalRegistrator.register()
+
+// this is a filthy hack
+// but this _should_ satisfy enough of the interface not to break things
+// Element implements a bunch of stuff matchMedia does, so that's our hack
+globalThis.matchMedia = str => {
+  const element = new Element()
+  element.matches = true
+  return element
+}
