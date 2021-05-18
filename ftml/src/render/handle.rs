607957d022ec44769980a9acf3f24e6f4f811a36
Ammon Smith <ammon.i.smith@gmail.com>
2021-05-17 22:19:05 -0400

    Output more useful dummy UserInfo data.

--- ftml/src/render/handle.rs
+++ ftml/src/render/handle.rs
@@ -71,10 +71,13 @@ impl Handle {
         format!("TODO: actual title ({})", page_slug)
     }
 
-    pub fn get_user_info(&self, log: &Logger, name: &str) -> Option<UserInfo> {
+    pub fn get_user_info<'a>(&self, log: &Logger, name: &'a str) -> Option<UserInfo<'a>> {
         debug!(log, "Fetching user info"; "name" => name);
 
-        Some(UserInfo::dummy())
+        let mut info = UserInfo::dummy();
+        info.user_name = cow!(name);
+        info.user_profile_url = Cow::Owned(format!("https://www.wikijump.com/user:info/{}", name));
+        Some(info)
     }
 
     pub fn get_image_link<'a>(
