362e76f4c59b24c53195d876f032ec1df0b06fe7
Ammon Smith <ammon.i.smith@gmail.com>
2021-05-17 22:43:37 -0400

    Add BLOCK_PARAGRAPH.

---
+++ ftml/src/parsing/rule/impls/block/blocks/paragraph.rs
@@ -0,0 +1,57 @@
+/*
+ * parsing/rule/impls/block/blocks/paragraph.rs
+ *
+ * ftml - Library to parse Wikidot text
+ * Copyright (C) 2019-2021 Wikijump Team
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+use super::prelude::*;
+
+pub const BLOCK_PARAGRAPH: BlockRule = BlockRule {
+    name: "block-paragraph",
+    accepts_names: &["p", "paragraph"],
+    accepts_star: false,
+    accepts_score: false,
+    accepts_newlines: true,
+    parse_fn,
+};
+
+fn parse_fn<'r, 't>(
+    log: &Logger,
+    parser: &mut Parser<'r, 't>,
+    name: &'t str,
+    flag_star: bool,
+    flag_score: bool,
+    in_head: bool,
+) -> ParseResult<'r, 't, Elements<'t>> {
+    debug!(
+        log,
+        "Parsing paragraph block";
+        "in-head" => in_head,
+        "name" => name,
+    );
+
+    assert!(!flag_star, "Paragraph doesn't allow star flag");
+    assert!(!flag_score, "Paragraph doesn't allow score flag");
+    assert_block_name(&BLOCK_PARAGRAPH, name);
+
+    let arguments = parser.get_head_map(&BLOCK_PARAGRAPH, in_head)?;
+    let (elements, exceptions, _) =
+        parser.get_body_elements(&BLOCK_PARAGRAPH, true)?.into();
+
+    let element = Elements::Multiple(elements);
+    ok!(element, exceptions)
+}
