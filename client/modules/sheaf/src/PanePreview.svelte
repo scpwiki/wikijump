5572e5ae454a5209daf64f47b31151795075c599
Monkatraz <34875062+Monkatraz@users.noreply.github.com>
2021-05-19 22:22:54 -0600

    i18n for editor preview

--- client/modules/sheaf/src/PanePreview.svelte
+++ client/modules/sheaf/src/PanePreview.svelte
@@ -7,6 +7,7 @@
   import CodeDisplay from "./CodeDisplay.svelte"
   import type { SheafContext } from "./context"
   import { RenderHandler } from "./render-handler"
+  import { t } from "wj-state"
 
   const { editor, bindings, settings, small } = getContext<SheafContext>("sheaf")
 
@@ -23,25 +24,25 @@
 <div class="sheaf-preview-container {theme} codetheme-{theme}">
   <Tabview noborder contained compact conditional>
     <Tab>
-      <span slot="button">Result</span>
+      <span slot="button">{$t("sheaf.preview_tabs.RESULT")}</span>
       <div class="sheaf-preview">
         <Wikitext morph {debug} wikitext={() => render.result()} />
       </div>
     </Tab>
 
     <Tab>
-      <span slot="button">HTML Output</span>
+      <span slot="button">{$t("sheaf.preview_tabs.HTML")}</span>
       <CodeDisplay content={render.html(true)} lang="html" />
     </Tab>
 
     <Tab>
-      <span slot="button">CSS Output</span>
+      <span slot="button">{$t("sheaf.preview_tabs.CSS")}</span>
       <CodeDisplay content={render.style()} lang="css" />
     </Tab>
 
     {#if debug}
       <Tab>
-        <span slot="button">AST</span>
+        <span slot="button">{$t("sheaf.preview_tabs.AST")}</span>
         <CodeDisplay content={render.stringifiedAST()} lang="json" />
       </Tab>
     {/if}
