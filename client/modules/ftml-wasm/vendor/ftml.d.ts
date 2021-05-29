5adcaa78ee9aa30ec2af4cc4e89100b8974bbdd8
Ammon Smith <ammon.i.smith@gmail.com>
2021-05-29 03:24:38 -0400

    Update vendored ftml properly.

--- client/modules/ftml-wasm/vendor/ftml.d.ts
+++ client/modules/ftml-wasm/vendor/ftml.d.ts
@@ -22,15 +22,15 @@ export function tokenize(text: string): Tokenization;
 */
 export function version(): string;
 /**
-* @param {Tokenization} tokens
-* @returns {ParseOutcome}
-*/
-export function parse(tokens: Tokenization): ParseOutcome;
-/**
 * @param {string} text
 * @returns {string}
 */
 export function preprocess(text: string): string;
+/**
+* @param {Tokenization} tokens
+* @returns {ParseOutcome}
+*/
+export function parse(tokens: Tokenization): ParseOutcome;
 
 
 export interface IHtmlOutput {
@@ -226,3 +226,60 @@ export class Utf16IndexMap {
 */
   get_index(index: number): number;
 }
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly __wbg_pageinfo_free: (a: number) => void;
+  readonly pageinfo_new: (a: number) => number;
+  readonly pageinfo_page: (a: number, b: number) => void;
+  readonly pageinfo_category: (a: number, b: number) => void;
+  readonly pageinfo_site: (a: number, b: number) => void;
+  readonly pageinfo_title: (a: number, b: number) => void;
+  readonly pageinfo_alt_title: (a: number, b: number) => void;
+  readonly pageinfo_rating: (a: number) => number;
+  readonly pageinfo_tags: (a: number) => number;
+  readonly pageinfo_language: (a: number, b: number) => void;
+  readonly __wbg_htmloutput_free: (a: number) => void;
+  readonly htmloutput_copy: (a: number) => number;
+  readonly htmloutput_body: (a: number, b: number) => void;
+  readonly htmloutput_styles: (a: number) => number;
+  readonly htmloutput_html_meta: (a: number) => number;
+  readonly render_html: (a: number, b: number) => number;
+  readonly render_text: (a: number, b: number, c: number) => void;
+  readonly pageinfo_copy: (a: number) => number;
+  readonly __wbg_utf16indexmap_free: (a: number) => void;
+  readonly utf16indexmap_new: (a: number, b: number) => number;
+  readonly utf16indexmap_copy: (a: number) => number;
+  readonly utf16indexmap_get_index: (a: number, b: number) => number;
+  readonly __wbg_tokenization_free: (a: number) => void;
+  readonly tokenization_copy: (a: number) => number;
+  readonly tokenization_text: (a: number, b: number) => void;
+  readonly tokenization_tokens: (a: number) => number;
+  readonly tokenize: (a: number, b: number) => number;
+  readonly version: (a: number) => void;
+  readonly preprocess: (a: number, b: number, c: number) => void;
+  readonly __wbg_parseoutcome_free: (a: number) => void;
+  readonly parseoutcome_copy: (a: number) => number;
+  readonly parseoutcome_syntax_tree: (a: number) => number;
+  readonly parseoutcome_warnings: (a: number) => number;
+  readonly __wbg_syntaxtree_free: (a: number) => void;
+  readonly syntaxtree_data: (a: number) => number;
+  readonly parse: (a: number) => number;
+  readonly syntaxtree_copy: (a: number) => number;
+  readonly __wbindgen_malloc: (a: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number) => number;
+  readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
+  readonly __wbindgen_free: (a: number, b: number) => void;
+}
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
