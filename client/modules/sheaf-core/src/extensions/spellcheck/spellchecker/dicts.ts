c7fbfcc5ba57ec13dc8fbc7005185e772b3fb234
Monkatraz <34875062+Monkatraz@users.noreply.github.com>
2021-06-06 17:00:57 -0600

    fix dicts resolving

--- client/modules/sheaf-core/src/extensions/spellcheck/spellchecker/dicts.ts
+++ client/modules/sheaf-core/src/extensions/spellcheck/spellchecker/dicts.ts
@@ -12,29 +12,32 @@ export interface Dictionary {
  * locale's language code, without any region attached.
  */
 export const DICTIONARIES: Record<string, () => Promise<Dictionary>> = {
+  // TODO: use @root when vite-tsconfig-paths gets support for suffixes
+  // see: https://github.com/aleclarson/vite-tsconfig-paths/pull/29
+  // looks like: await url(import("@root/locales/spellcheck/en.txt?url"))
   "en": async () => ({
-    dict: await url(import("@root/locales/spellcheck/en.txt?url"))
+    dict: await url(import("../../../../../../../locales/spellcheck/en.txt?url"))
   }),
   "de": async () => ({
-    dict: await url(import("@root/locales/spellcheck/de.txt?url"))
+    dict: await url(import("../../../../../../../locales/spellcheck/de.txt?url"))
   }),
   "es": async () => ({
-    dict: await url(import("@root/locales/spellcheck/es.txt?url"))
+    dict: await url(import("../../../../../../../locales/spellcheck/es.txt?url"))
   }),
   "fr": async () => ({
-    dict: await url(import("@root/locales/spellcheck/fr.txt?url"))
+    dict: await url(import("../../../../../../../locales/spellcheck/fr.txt?url"))
   }),
   "he": async () => ({
-    dict: await url(import("@root/locales/spellcheck/he.txt?url"))
+    dict: await url(import("../../../../../../../locales/spellcheck/he.txt?url"))
   }),
   "it": async () => ({
-    dict: await url(import("@root/locales/spellcheck/it.txt?url"))
+    dict: await url(import("../../../../../../../locales/spellcheck/it.txt?url"))
   }),
   "ru": async () => ({
-    dict: await url(import("@root/locales/spellcheck/ru.txt?url"))
+    dict: await url(import("../../../../../../../locales/spellcheck/ru.txt?url"))
   }),
   "zh": async () => ({
-    dict: await url(import("@root/locales/spellcheck/zh.txt?url"))
+    dict: await url(import("../../../../../../../locales/spellcheck/zh.txt?url"))
   })
 }
 
