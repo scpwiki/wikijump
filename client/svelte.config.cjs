a94574e107d4e4e7f30d6e37461ebed2e805ee1a
Monkatraz <34875062+Monkatraz@users.noreply.github.com>
2021-06-07 15:17:29 -0600

    remove old svelte config msg

--- client/svelte.config.cjs
+++ client/svelte.config.cjs
@@ -1,10 +1,3 @@
-// I'm fairly sure this file doesn't actually do anything.
-// I think everything is handled by esbuild or some other builder with its own config,
-// so basically just ignore this file.
-//
-// It has to be kept because if it wasn't, editors wouldn't know to
-// check for Svelte components.
-
 const sveltePreprocess = require("svelte-preprocess")
 
 module.exports = {
