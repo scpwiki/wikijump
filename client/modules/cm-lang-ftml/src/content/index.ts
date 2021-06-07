cf4ce04b8e15258ab34d40f00712c1a4f342f3f9
Monkatraz <34875062+Monkatraz@users.noreply.github.com>
2021-06-06 23:50:21 -0600

    document content worker

--- client/modules/cm-lang-ftml/src/content/index.ts
+++ client/modules/cm-lang-ftml/src/content/index.ts
@@ -1,8 +1,8 @@
 import { decode, transfer, WorkerModule } from "worker-module"
-import type { ContentModuleInterface } from "./worker/content.worker"
+import type { ContentModuleInterface } from "./content.worker"
 
 async function importWorker() {
-  return (await import("./worker/content.worker?bundled-worker")).default
+  return (await import("./content.worker?bundled-worker")).default
 }
 
 export class ContentWorker extends WorkerModule<ContentModuleInterface> {
@@ -10,6 +10,15 @@ export class ContentWorker extends WorkerModule<ContentModuleInterface> {
     super("ftml-lang-content-worker", importWorker, { persist: true })
   }
 
+  /**
+   * Extracts the "content" of a string of wikitext, as in a string with
+   * all markup removed and replaced with an equivalent number of whitespaces.
+   *
+   * @param str - The string of wikitext to extract the content of.
+   * @param raw - If true, an encoded ArrayBuffer will be returned. This is
+   *   useful if the results of this function are to be immediately passed
+   *   into another worker.
+   */
   async extract(str: string | ArrayBuffer): Promise<string>
   async extract(str: string | ArrayBuffer, raw: true): Promise<ArrayBuffer>
   async extract(str: string | ArrayBuffer, raw = false): Promise<string | ArrayBuffer> {
@@ -17,6 +26,12 @@ export class ContentWorker extends WorkerModule<ContentModuleInterface> {
     return raw ? result : decode(result)
   }
 
+  /**
+   * Returns the "stats" for a string of wikitext. This includes its byte
+   * count and word count.
+   *
+   * @param str - The string of wikitext to get the stats of.
+   */
   async stats(str: string | ArrayBuffer) {
     return await this.invoke("stats", transfer(str))
   }
