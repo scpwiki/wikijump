af526e51461fcc7c78c984207e217d86a70d565c
Monkatraz <34875062+Monkatraz@users.noreply.github.com>
2021-06-12 20:22:48 -0600

    update locale filters for each language

--- client/modules/wj-codemirror/src/spellcheck/spellchecker/locales.ts
+++ client/modules/wj-codemirror/src/spellcheck/spellchecker/locales.ts
@@ -7,55 +7,66 @@ export const LOCALES: Record<string, SpellcheckerLocale> = {
       ["’", "'"] // normalize right single quotation marks into apostrophes
     ],
     filters: [
-      /[^\p{L}]\p{L}(?![\p{L}'])/gu, // single characters
-      /\p{Nd}+\p{L}+/gu,             // e.g. 5GW, 20mm, etc.
-      /\b's\b/gu,                    // remove `'s` plurals
-      /\s'|'(?!\p{L})/gu,            // remove ' marks on the start/end of words
-      /\b[\p{Lu}\p{Nd}]{2,4}\b/gu,   // SCP, MTF, etc. capitalized initialisms
-      /[^\p{L}'\s]+/gu               // filter punctuation out
+      /[^\p{L}']\p{L}(?![\p{L}'])/gu,           // single characters
+      /\p{Nd}+\p{L}+/gu,                        // e.g. 5GW, 20mm, etc.
+      /\b's\b/gu,                               // remove `'s` plurals
+      /[^\p{L}]'|'(?!\p{L})/gu,                 // remove ' marks on start/end of words
+      /[^\p{L}][\p{Lu}\p{Nd}]{2,5}(?!\p{L})/gu, // SCP, MTF, etc. capitalized initialisms
+      /[^\p{L}'\s]+/gu                          // filter punctuation out
     ],
     unknown: true
   },
 
   "de": {
     filters: [
-      /[^\p{L}]\p{L}(?!\p{L})/gu,  // single characters
-      /\p{Nd}+\p{L}+/gu,           // e.g. 5GW, 20mm, etc.
-      /\b[\p{Lu}\p{Nd}]{2,4}\b/gu, // SCP, MTF, etc. capitalized initialisms
-      /[^\p{L}\s]+/gu              // filter punctuation out
+      /[^\p{L}]\p{L}(?!\p{L})/gu,               // single characters
+      /\p{Nd}+\p{L}+/gu,                        // e.g. 5GW, 20mm, etc.
+      /[^\p{L}][\p{Lu}\p{Nd}]{2,5}(?!\p{L})/gu, // SCP, MTF, etc. capitalized initialisms
+      /[^\p{L}\s]+/gu                           // filter punctuation out
     ],
     compound: true
   },
 
   "es":{
     filters: [
-      /[^\p{L}]\p{L}(?!\p{L})/gu,  // single characters
-      /\p{Nd}+\p{L}+/gu,           // e.g. 5GW, 20mm, etc.
-      /\b[\p{Lu}\p{Nd}]{2,4}\b/gu, // SCP, MTF, etc. capitalized initialisms
-      /[^\p{L}\s]+/gu              // filter punctuation out
-    ]
+      /[^\p{L}]\p{L}(?!\p{L})/gu,               // single characters
+      /\p{Nd}+\p{L}+/gu,                        // e.g. 5GW, 20mm, etc.
+      /[^\p{L}][\p{Lu}\p{Nd}]{2,5}(?!\p{L})/gu, // SCP, MTF, etc. capitalized initialisms
+      /[^\p{L}\s]+/gu                           // filter punctuation out
+    ],
+    unknown: true
   },
 
   "fr": {
     filters: [
+      /[^\p{L}]\p{L}(?!\p{L})/gu,               // single characters
+      /\p{Nd}+\p{L}+/gu,                        // e.g. 5GW, 20mm, etc.
+      /[^\p{L}][\p{Lu}\p{Nd}]{2,5}(?!\p{L})/gu, // SCP, MTF, etc. capitalized initialisms
       /[^\p{L}\s]+/gu                           // filter punctuation out
     ]
   },
 
   "it": {
     filters: [
-      /[^\p{L}]\p{L}(?!\p{L})/gu,  // single characters
-      /\p{Nd}+\p{L}+/gu,           // e.g. 5GW, 20mm, etc.
-      /\b[\p{Lu}\p{Nd}]{2,4}\b/gu, // SCP, MTF, etc. capitalized initialisms
-      /[^\p{L}\s]+/gu              // filter punctuation out
-    ]
+      /[^\p{L}']\p{L}(?![\p{L}'])/gu,           // single characters
+      /[^\p{L}]\p{L}+'(?=\p{L})/gu,             // elision
+      /[^\p{L}]'|'(?!\p{L})/gu,                 // remove ' marks on start/end of words
+      /\p{Nd}+\p{L}+/gu,                        // e.g. 5GW, 20mm, etc.
+      /[^\p{L}][\p{Lu}\p{Nd}]{2,5}(?!\p{L})/gu, // SCP, MTF, etc. capitalized initialisms
+      /[^\p{L}'\s]+/gu                          // filter punctuation out
+    ],
+    unknown: true
   },
 
+  // TODO: handle stress accents? e.g. ё vs е
   "ru": {
     filters: [
+      /[^\p{L}-]\p{L}(?![\p{L}-])/gu,            // single characters
+      /\p{Nd}+\p{L}+/gu,                         // e.g. 5GW, 20mm, etc.
+      /[^\p{L}][\p{Lu}\p{Nd}-]{2,5}(?!\p{L})/gu, // capitalized initialisms
       /[^\p{L}\s-]+/gu                           // filter punctuation out
     ],
-    compound: true
+    unknown: true
   }
 }
 
