3318de1f19fdb7e38466dcd119de24602b142615
Monkatraz <34875062+Monkatraz@users.noreply.github.com>
2021-06-14 02:33:02 -0600

    match contracted titles

--- client/modules/cm-nspell/src/locales.ts
+++ client/modules/cm-nspell/src/locales.ts
@@ -2,12 +2,18 @@ import type { Locale } from "./types"
 
 // prettier-ignore
 const DEFAULT_LOCALE: Locale = {
-  pattern: /\p{L}(?![\p{L}\p{Nd}'’])|[\p{L}\p{Nd}][\p{L}\p{Nd}'’]*[\p{L}\p{Nd}]/gu,
+  // Three alternatives:
+  // 1. Catches words like "Dr.", "Agt.", etc.
+  // 2. Single isolated characters
+  // 3. Everything else, i.e. words of length >= 2
+  // Includes numbers when matching, e.g. 50mm is matched.
+  pattern: /\p{Lu}\p{L}{1,3}\.|\p{L}(?![\p{L}\p{Nd}'’])|[\p{L}\p{Nd}][\p{L}\p{Nd}'’]*[\p{L}\p{Nd}]/gu,
   filters: [
-      /^\p{L}$/u,               // single characters
-      /^\p{Nd}+$/u,             // pure numbers
-      /^\p{Nd}+\p{L}+$/u,       // e.g. 5GW, 20mm, etc.
-      /^[\p{Lu}\p{Nd}]{2,5}$/u, // SCP, MTF, etc. capitalized initialisms
+      /^\p{L}\.?$/u,               // single characters
+      /^\p{Nd}+\.?$/u,             // pure numbers
+      /^\p{Lu}\p{L}+\./u,          // contracted titles
+      /^\p{Nd}+\p{L}+\.?$/u,       // e.g. 5GW, 20mm, etc.
+      /^[\p{Lu}\p{Nd}]{2,5}\.?$/u, // SCP, MTF, etc. capitalized initialisms
   ]
 }
 
