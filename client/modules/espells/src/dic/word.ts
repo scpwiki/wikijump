d2e08d04a4057bb398a9350f317c0b3c4a7d9e1e
Monkatraz <34875062+Monkatraz@users.noreply.github.com>
2021-06-19 00:44:05 -0600

    document word

--- client/modules/espells/src/dic/word.ts
+++ client/modules/espells/src/dic/word.ts
@@ -3,15 +3,34 @@ import { RepPattern } from "../aff/rep-pattern"
 import { CapType, CONSTANTS as C } from "../constants"
 import { split } from "../util"
 
+/** A word as found in a {@link Dic} instance's index. */
 export class Word {
+  /** The raw stem of the word, as parsed from the `.dic` file. */
   declare stem: string
+
+  /** The capitalization type of this word. */
   declare capType: CapType
 
   // using optionals here to save on memory
+
+  /** The {@link Flags} that this word is associated with. */
   declare flags?: Flags
+
+  /**
+   * Misc. data that the word was associated with in the `.dic` file. e.g.
+   * determining if `is:gendered` is associated with a word would be
+   * `word.data.get("is").has("gendered")`.
+   */
   declare data?: Map<string, Set<string>>
+
+  /** Common misspellings for this word. */
   declare altSpellings?: Set<string>
 
+  /**
+   * @param line - The line from a `.dic` file to parse. Can also just be
+   *   treated as a "word" argument.
+   * @param aff - {@link Aff} data to use.
+   */
   constructor(line: string, aff: Aff) {
     const match = C.SPLIT_WORD_REGEX.exec(line)
     if (!match) throw new SyntaxError(`Invalid line in dictionary '${line}'`)
