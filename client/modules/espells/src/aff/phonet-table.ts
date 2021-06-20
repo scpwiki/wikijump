4027784e9658691485eaf42b10003b76ffb2a141
Monkatraz <34875062+Monkatraz@users.noreply.github.com>
2021-06-20 15:39:41 -0600

    document phonet-table

--- client/modules/espells/src/aff/phonet-table.ts
+++ client/modules/espells/src/aff/phonet-table.ts
@@ -1,6 +1,29 @@
 import { CONSTANTS as C } from "../constants"
 import { re } from "../util"
 
+// TODO: use homegrown metaphone implementation (maybe)
+
+/**
+ * A table for metaphone transformations. These transformations may provide
+ * superior suggestions because they describe similar sounding (as in
+ * spoken) syllables, which will be used by the suggestion engine to find
+ * words that may not be spelled similarly but *sound* similar.
+ *
+ * It roughly uses the following syntax:
+ *
+ * ```text
+ * PHONE <number of entries>
+ * PHONE <pattern> <replacement>
+ * ```
+ *
+ * `replacement` is a simple string, with the special value `_`
+ * (underscore) meaning an empty string. `pattern` is complex, and
+ * currently isn't very well documented. Additionally, both Spylls and
+ * Espells do not fully implement Hunspell's more intricate details, such
+ * as rule prioritizing and concepts like "follow-up rules".
+ *
+ * Dictionaries that use this feature are unfortunately quite rare.
+ */
 export class PhonetTable {
   rules: Record<string, PhonetTableRule[]> = {}
 
@@ -37,16 +60,34 @@ export class PhonetTable {
   }
 }
 
+/**
+ * An individual phonetic table rule.
+ *
+ * @see {@link PhonetTable}
+ */
 class PhonetTableRule {
   constructor(
+    /** The `RegExp` used for checking if this rule applies. */
     public search: RegExp,
+    /** The string to represent a matched phoneme with. */
     public replacement: string,
+    /** If true, this rule only applies at the start of a word. */
     public start = false,
+    /** If true, this rule only applies at the end of a word. */
     public end = false,
+    /** Currently unusued in both Spylls and Espells. */
     public followup = true,
+    /** Currently unusued in both Spylls and Espells. */
     public priority = 5
   ) {}
 
+  /**
+   * Checks if a rule is matched by this rule, and if it is, returns the
+   * `RegExpExecArray` match, otherwise returning false.
+   *
+   * @param word - The word to check.
+   * @param pos - The position in the word to check.
+   */
   match(word: string, pos: number) {
     if (this.start && pos > 0) return false
     this.search.lastIndex = pos
