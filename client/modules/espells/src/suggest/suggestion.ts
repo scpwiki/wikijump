659b9bb44d0c898f15ef55c4adca68674555f897
Monkatraz <34875062+Monkatraz@users.noreply.github.com>
2021-06-20 19:03:02 -0600

    document suggestion

--- client/modules/espells/src/suggest/suggestion.ts
+++ client/modules/espells/src/suggest/suggestion.ts
@@ -1,14 +1,44 @@
+/** The object that the {@link Suggest} class uses internally for tracking suggestions. */
 export class Suggestion {
-  constructor(public text: string, public kind: string) {}
+  constructor(
+    /** The actual suggested text. */
+    public text: string,
+    /** Describes what actually generated this suggestion, using a string label. */
+    public kind: string
+  ) {}
 
+  /**
+   * Returns a new {@link Suggestion}, cloned from this current instance,
+   * but with any properties given replaced.
+   */
   replace(text = this.text, kind = this.kind) {
     return new Suggestion(text, kind)
   }
 }
 
+/**
+ * Like the usual {@link Suggestion}, but instead stores a list of words
+ * that represents the entire suggestion.
+ */
 export class MultiWordSuggestion {
-  constructor(public words: string[], public kind: string, public allowDash = true) {}
+  constructor(
+    /** The list of words that represents this suggestion. */
+    public words: string[],
+    /** Describes what actually generated this suggestion, using a string label. */
+    public kind: string,
+    /**
+     * If true, this suggestion is allowed to be given to the user with
+     * dashes joining the words together.
+     */
+    public allowDash = true
+  ) {}
 
+  /**
+   * Converts this multi-word suggestion into a normal {@link Suggestion} by
+   * joining the words list together.
+   *
+   * @param seperator - The separator to use. Defaults to a space.
+   */
   stringify(seperator = " ") {
     return new Suggestion(this.words.join(seperator), this.kind)
   }
