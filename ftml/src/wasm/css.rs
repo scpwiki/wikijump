6f431c02051fdace905c7484db21e306f7ade1b5
Ammon Smith <ammon.i.smith@gmail.com>
2021-07-27 10:29:05 -0400

    Add base_css() method to wasm.

--- ftml/src/wasm/css.rs
+++ ftml/src/wasm/css.rs
@@ -1,5 +1,5 @@
 /*
- * wasm/mod.rs
+ * wasm/css.rs
  *
  * ftml - Library to parse Wikidot text
  * Copyright (C) 2019-2021 Wikijump Team
@@ -18,32 +18,9 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#[macro_use]
-mod macros;
+use super::prelude::*;
 
-#[cfg(feature = "css")]
-mod css;
-
-mod error;
-mod log;
-mod misc;
-mod parsing;
-mod preproc;
-mod render;
-mod tokenizer;
-mod utf16;
-
-mod prelude {
-    pub use super::log::LOGGER;
-    pub use wasm_bindgen::prelude::*;
-    pub use wasm_bindgen::JsCast;
+#[wasm_bindgen]
+pub fn base_css() -> String {
+    str!(crate::FTML_BASE_CSS)
 }
-
-#[cfg(feature = "wasm-log")]
-pub use self::log::ConsoleLogger;
-
-pub use self::misc::version;
-pub use self::parsing::{parse, ParseOutcome, SyntaxTree};
-pub use self::preproc::preprocess;
-pub use self::render::{render_html, render_text};
-pub use self::tokenizer::{tokenize, Tokenization};
