00a3477c71af45fa81fbf1984e5e8a9d68b81f6a
pxdnbluesoul <github@nags.me>
2021-08-14 12:40:11 -0600

    Specify stack driver as fallback for mail

--- web/config/mail.php
+++ web/config/mail.php
@@ -64,7 +64,7 @@ return [
 
         'log' => [
             'transport' => 'log',
-            'channel' => env('MAIL_LOG_CHANNEL'),
+            'channel' => env('MAIL_LOG_CHANNEL', 'stack'),
         ],
 
         'array' => [
