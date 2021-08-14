9b27e6388c16713bb2181387737c6a16c94f9990
pxdnbluesoul <github@nags.me>
2021-08-14 15:53:32 -0600

    Add setTemplate and findModuleClass methods

--- web/app/Helpers/PageBuilder.php
+++ web/app/Helpers/PageBuilder.php
@@ -1,9 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Wikijump\Helpers;
 
 use Ozone\Framework\PageProperties;
 use Ozone\Framework\ParameterList;
+use Ozone\Framework\PathManager;
 
 /**
  * PageBuilder: It's like RunData, but good.
@@ -74,14 +77,27 @@ class PageBuilder
          * First, the parameter list is populated.
          * This is the business of inspecting the request and making some decisions.
          */
-        $this->parameterList = $this->initParameterList();
+        $this->initParameterList();
+
+        /**
+         * Then we take our extracted parameters and choose the template and screen to use.
+         */
+        $this->setTemplate();
     }
 
-    private function initParameterList()
+    /**
+     * Retrieve contents of various PHP vars related to paths and queries.
+     *
+     * @return void
+     */
+    public function initParameterList(): void
     {
         /** The ajaxMode property can be set true by the AjaxModuleWebFlowController */
-        if($this->ajaxMode === true){
+        if ($this->ajaxMode === true) {
+            /** We populate an empty key for the parameter bucket that calling modules may use. */
             $this->allParameters['AMODULE'] = [];
+
+            /** We break the POST down into key-value pairs and put them in our parameter store. */
             foreach ($_POST as $key => $value) {
                 /** Convert any CRLF values to LF */
                 $value = str_replace(["\r\n", "\r"], "\n", $value);
@@ -90,14 +106,12 @@ class PageBuilder
                 $this->parameterArray[$key] = $value;
 
                 /** And note that it's an AJAX Module. */
-                $this->parameterTypes[$key] = "AMODULE";
+                $this->parameterTypes[$key] = 'AMODULE';
 
                 /** Add the POST key-value pair as a subarray of an AMODULE key. */
                 $this->allParameters['AMODULE'][$key] = $value;
             }
-        }
-        else
-        {
+        } else {
             /** We'll instantiate some empty keys that some calling modules expect. */
             $this->allParameters['GET'] = [];
             $this->allParameters['POST'] = [];
@@ -153,15 +167,139 @@ class PageBuilder
 
             /** Lastly, POST values don't need as much manipulation. */
             foreach ($_POST as $key => $value) {
-
                 /** Convert any CRLF values to LF */
-                $value = str_replace(["\r\n", "\r"], "\n", $value);
+                $value = str_replace(["\r\n", "\r", "\n"], "\n", $value);
 
                 /** And add them to the bucket. */
                 $this->parameterArray[$key] = $value;
                 $this->parameterTypes[$key] = 'POST';
                 $this->allParameters['POST'][$key] = urldecode($value);
             }
+        }
+    }
+
+    public function setTemplate()
+    {
+        /** The ajaxMode property can be set true by the AjaxModuleWebFlowController */
+        if ($this->ajaxMode === true) {
+            /** We can reliably assume that an AJAX module provided a key called moduleName */
+            $template = $this->parameterArray['moduleName'];
+
+            /**
+             * If we didn't get a valid value back (not empty, contains only
+             *  letters, numbers, underscores, and slashes), use the Empty template. (nothing)
+             */
+            if ($template == null || preg_match('/^[a-z0-9_\/]+$/i', $template) != 1) {
+                $template = 'Empty';
+            }
+
+            /**
+             * Otherwise we're going to assume there's a matching template in
+             *  templates/modules to go with the PHP module in php/modules.
+             *
+             * These AJAX-centric modules generate the HTML to be injected back
+             *  into the DOM wholesale.
+             */
+            $this->moduleTemplate = $template;
+
+            $this->findModuleClass($this->moduleTemplate);
+        } else {
+            /**
+             * If we're not making an AJAX call, we're looking for a Screen instead of a template.
+             * Screens are in php/Screens and are the classes that use the
+             * templates in templates/screens.
+             */
+            $template = $this->parameterArray['template'];
+
+            /**
+             * If we didn't get a valid value back (not empty, contains only
+             *  letters, numbers, underscores, and slashes), use the Index screen. (nothing)
+             */
+            if ($template == null || preg_match('/^[a-z0-9_\/]+$/i', $template) != 1) {
+                $template = 'Index';
+            }
+
+            /**
+             * We receive the call on the php file as a path using two
+             * underscores instead of a slash.  We need to pass the actual path to
+             * our next set of tasks.
+             */
+            $template = str_replace('__', '/', $template);
+            $this->screenTemplate = $template;
+
+            $this->findModuleClass($this->screenTemplate);
+        }
+    }
+
+    /**
+     * Parse and return the corresponding path for a template or screen.
+     * @param $template
+     * @return void
+     */
+    public function findModuleClass($template)
+    {
+        /**  Retrieve the path to the module. */
+        if ($this->moduleTemplate != null) {
+            $classFilename = PathManager::moduleClass($template);
+        }
+        if ($this->screenTemplate != null) {
+            $classFilename = PathManager::screenClass($this->screenTemplate);
+        }
+
+        /** If the file is there, return the path and name. */
+        if (file_exists($classFilename)) {
+            $moduleClassPath = $classFilename;
+            $moduleClassName = array_slice(explode($classFilename, '/'), -1);
+        } else {
+            /** If it's not there, I guess we're going on an adventure. */
+            if ($this->moduleTemplate != null) {
+                $dir = PathManager::moduleClassDir();
+            }
+            if ($this->screenTemplate != null) {
+                $dir = PathManager::screenClassDir();
+            }
+
+            /** We've got an arbitrary number of slashes that may lead to a file. */
+            $module_path = explode('/', $template);
+
+            /** We're going to work our way backwards from the deepest subfolder to the parent to find it. */
+            for ($i = count($module_path) - 1; $i >= 0; $i--) {
+                $path = '';
+                /** For an incrementing value that's the same size as the max path depth... */
+                for ($k = 0; $k < $i; $k++) {
+                    /**
+                     * Build a string with the most specific path to the
+                     * fallback (DefaultModule.php) file.
+                     */
+                    $path .= $module_path[$k] . '/';
+                    if ($this->moduleTemplate != null) {
+                        $path .= 'DefaultModule.php';
+                    }
+                    if ($this->screenTemplate != null) {
+                        $path .= 'DefaultScreen.php';
+                    }
+                }
+                $classFiles[] = $path;
+            }
+            /** Add this constructed path to an array. */
+
+            foreach ($classFiles as $classFile) {
+                /** The first one that matches is the one we'll provide to the caller. */
+                if (file_exists($dir . $classFile)) {
+                    $moduleClassPath = $dir . $classFile;
+                    if($this->moduleTemplate != null)
+                    {
+                        $moduleClassName = 'DefaultModule';
+                    }
+                    if($this->screenTemplate != null)
+                    {
+                        $moduleClassName = 'DefaultScreen';
+                    }
+                    break;
+                }
+            }
+            $this->moduleClassName = $moduleClassName;
+            $this->moduleClassPath = $moduleClassPath;
         }
     }
 }
