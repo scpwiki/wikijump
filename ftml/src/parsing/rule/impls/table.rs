69a20cd635287b4e0a65dfe203ffba28fc71d796
Ammon Smith <ammon.i.smith@gmail.com>
2021-08-22 02:18:01 -0400

    Initial implementation of simple table parse rule.

--- ftml/src/parsing/rule/impls/table.rs
+++ ftml/src/parsing/rule/impls/table.rs
@@ -19,6 +19,15 @@
  */
 
 use super::prelude::*;
+use crate::tree::{Alignment, Table, TableCell, TableRow};
+use std::num::NonZeroU32;
+
+#[derive(Debug)]
+struct TableCellStart {
+    align: Option<Alignment>,
+    header: bool,
+    column_span: NonZeroU32,
+}
 
 pub const RULE_TABLE: Rule = Rule {
     name: "table",
@@ -28,9 +37,155 @@ pub const RULE_TABLE: Rule = Rule {
 
 fn try_consume_fn<'p, 'r, 't>(
     log: &Logger,
-    _parser: &'p mut Parser<'r, 't>,
+    parser: &'p mut Parser<'r, 't>,
 ) -> ParseResult<'r, 't, Elements<'t>> {
     debug!(log, "Trying to parse simple table");
 
-    todo!()
+    let mut rows = Vec::new();
+    let mut exceptions = Vec::new();
+    let mut _paragraph_break = false;
+
+    loop {
+        let row = parse_row(log, parser)? //
+            .chain(&mut exceptions, &mut _paragraph_break);
+
+        match row {
+            Some(row) => rows.push(row),
+            None => break,
+        }
+    }
+
+    let table = Table {
+        rows,
+        attributes: AttributeMap::new(),
+    };
+
+    ok!(false; Element::Table(table), exceptions)
+}
+
+fn parse_row<'p, 'r, 't>(
+    log: &Logger,
+    parser: &'p mut Parser<'r, 't>,
+) -> ParseResult<'r, 't, Option<TableRow<'t>>> {
+    debug!(log, "Trying to parse simple table row");
+
+    let mut cells = Vec::new();
+    let mut exceptions = Vec::new();
+    let mut _paragraph_break = false;
+
+    // Loop for each cell in the row
+    'cell: loop {
+        let mut elements = Vec::new();
+        let TableCellStart {
+            align,
+            header,
+            column_span,
+        } = parse_cell_start(parser)?;
+
+        // Loop for each element in the cell
+        'row: loop {
+            match next_two_tokens(parser) {
+                // Special case:
+                //
+                // If there is "_\n" next, then treat this as a newline insertion.
+                // Since normally a newline will end the row, but we want a <br>
+                // in the cell contents.
+                (Token::Underscore, Some(Token::LineBreak | Token::ParagraphBreak)) => {
+                    elements.push(Element::LineBreak);
+                }
+
+                // End the cell or row.
+                (
+                    Token::TableColumn
+                    | Token::TableColumnTitle
+                    | Token::TableColumnLeft
+                    | Token::TableColumnCenter
+                    | Token::TableColumnRight,
+                    Some(next),
+                ) => {
+                    match next {
+                        // We cannot accept a ParagraphBreak since
+                        // that would form a separate table.
+                        Token::LineBreak | Token::InputEnd => break 'row,
+
+                        // Otherwise, the cell is finished, and we proceed to the next one.
+                        _ => break 'cell,
+                    }
+                }
+
+                // Invalid tokens
+                (Token::LineBreak | Token::ParagraphBreak | Token::InputEnd, _) => {
+                    return Err(parser.make_warn(ParseWarningKind::RuleFailed))
+                }
+
+                // Consume tokens like normal.
+                _ => {
+                    let new_elements = consume(log, parser)?
+                        .chain(&mut exceptions, &mut _paragraph_break);
+
+                    elements.extend(new_elements);
+                }
+            }
+        }
+
+        // Build table cell
+        cells.push(TableCell {
+            elements,
+            header,
+            column_span,
+            align,
+            attributes: AttributeMap::new(),
+        });
+    }
+
+    let row = TableRow {
+        cells,
+        attributes: AttributeMap::new(),
+    };
+
+    ok!(false; Some(row), exceptions)
+}
+
+/// Parse out the cell settings from the start.
+///
+/// Cells have a few settings, such as alignment, and most importantly
+/// here, their span, which is specified by having multiple
+/// `Token::TableColumn` (`||`) adjacent together.
+fn parse_cell_start(parser: &mut Parser) -> Result<TableCellStart, ParseWarning> {
+    let mut span = 1;
+
+    let (align, header) = loop {
+        match parser.current().token {
+            // Style cases, terminal
+            Token::TableColumnTitle => break (None, true),
+            Token::TableColumnLeft => break (Some(Alignment::Left), false),
+            Token::TableColumnCenter => break (Some(Alignment::Center), false),
+            Token::TableColumnRight => break (Some(Alignment::Right), false),
+
+            // Regular column, iterate to see if it has a span
+            Token::TableColumn => {
+                span += 1;
+                parser.step()?;
+            }
+
+            // Regular column, terminal
+            _ if span > 1 => break (None, false),
+
+            // No span depth, just an invalid token
+            _ => return Err(parser.make_warn(ParseWarningKind::RuleFailed)),
+        }
+    };
+
+    Ok(TableCellStart {
+        align,
+        header,
+        column_span: NonZeroU32::new(span).unwrap(),
+    })
+}
+
+fn next_two_tokens(parser: &Parser) -> (Token, Option<Token>) {
+    let first = parser.current().token;
+    let second = parser.look_ahead(1).map(|next| next.token);
+
+    (first, second)
 }
