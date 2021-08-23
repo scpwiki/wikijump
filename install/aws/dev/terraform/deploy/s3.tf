0fc7ea1d205427e2634e61d2b838dace3cf1c1dd
pxdnbluesoul <github@nags.me>
2021-08-23 07:18:55 -0600

    Missing var directives

--- install/aws/dev/terraform/deploy/s3.tf
+++ install/aws/dev/terraform/deploy/s3.tf
@@ -1,10 +1,10 @@
 resource "aws_s3_bucket" "elb_logs" {
-  bucket = "${s3_elb_log_bucket_prefix}-${var.environment}"
+  bucket = "${var.s3_elb_log_bucket_prefix}-${var.environment}"
   acl    = "log-delivery-write"
 }
 
 resource "aws_s3_bucket" "wikijump_assets" {
-  bucket = "${s3_asset_bucket_prefix}-${var.environment}"
+  bucket = "${var.s3_asset_bucket_prefix}-${var.environment}"
   acl    = "public-read"
 
   cors_rule {
