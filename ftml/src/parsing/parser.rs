409947478a10712745975ab176968b30ba487200
Ammon Smith <ammon.i.smith@gmail.com>
2021-08-22 20:47:35 -0400

    Log when failing to step().

--- ftml/src/parsing/parser.rs
+++ ftml/src/parsing/parser.rs
@@ -337,7 +337,10 @@ impl<'r, 't> Parser<'r, 't> {
                 self.remaining = remaining;
                 Ok(current)
             }
-            None => Err(self.make_warn(ParseWarningKind::EndOfInput)),
+            None => {
+                trace!(self.log, "Exhausted all tokens, yielding end of input warning");
+                Err(self.make_warn(ParseWarningKind::EndOfInput))
+            }
         }
     }
 
