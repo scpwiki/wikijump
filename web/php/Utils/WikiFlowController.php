71e763be4fd9e77ee9c1d14b7248de4696f71fb0
Ammon Smith <ammon.i.smith@gmail.com>
2021-09-07 14:20:13 -0400

    Remove unreachable break.

--- web/php/Utils/WikiFlowController.php
+++ web/php/Utils/WikiFlowController.php
@@ -162,7 +162,6 @@ class WikiFlowController extends WebFlowController
                     header("HTTP/1.1 301 Moved Permanently");
                     header("Location: ".'https://'.$_SERVER["HTTP_HOST"].$_SERVER['REQUEST_URI']);
                     exit();
-                    break;
             }
         }
 
