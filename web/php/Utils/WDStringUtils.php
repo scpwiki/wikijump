987ee4c7f0860173d759372714e1f1401134d6a0
Ammon Smith <ammon.i.smith@gmail.com>
2021-09-06 23:59:08 -0400

    Add unicode flag to all normalization regular expressions.

--- web/php/Utils/WDStringUtils.php
+++ web/php/Utils/WDStringUtils.php
@@ -23,17 +23,17 @@ class WDStringUtils
         $text = mb_convert_case($text, MB_CASE_FOLD, 'UTF-8');
 
         // Normalize string
-        $text = preg_replace('/[^\p{L}\p{N}\-:_]/', '-', $text); // Replace non-alphanumeric or non-slug characters
-        $text = preg_replace('/^_/', ':_', $text); // Allow leading underscores (e.g. _default, _template)
-        $text = preg_replace('/(?<!:)_/', '-', $text); // Clobber all other underscores
-        $text = preg_replace('/(^\-*|\-*$)/', '', $text); // Strip leading or trailing dashes
-        $text = preg_replace('/[\-]{2,}/', '-', $text); // Combine multiple dashes
-        $text = preg_replace('/[:]{2,}/', ':', $text); // Combine multiple colons
+        $text = preg_replace('/[^\p{L}\p{N}\-:_]/u', '-', $text); // Replace non-alphanumeric or non-slug characters
+        $text = preg_replace('/^_/u', ':_', $text); // Allow leading underscores (e.g. _default, _template)
+        $text = preg_replace('/(?<!:)_/u', '-', $text); // Clobber all other underscores
+        $text = preg_replace('/(^\-*|\-*$)/u', '', $text); // Strip leading or trailing dashes
+        $text = preg_replace('/[\-]{2,}/u', '-', $text); // Combine multiple dashes
+        $text = preg_replace('/[:]{2,}/u', ':', $text); // Combine multiple colons
 
         // Clean up boundaries
         $text = str_replace('(:-|-:)', ':', $text); // Dashes before or after categories
         $text = str_replace('(_-|-_)', '_', $text); // Dashes before or after leading underscores
-        $text = preg_replace('/(^:|:$)/', '', $text); // Strip leading or trailing colons
+        $text = preg_replace('/(^:|:$)/u', '', $text); // Strip leading or trailing colons
 
         return $text;
     }
