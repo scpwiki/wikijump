98905dc128b0639aed8429051b271bb2ca99bec4
Ammon Smith <ammon.i.smith@gmail.com>
2021-09-07 23:55:22 -0400

    Convert tabs to spaces.

--- web/lib/ozoneframework/php/core/ParameterList.php
+++ web/lib/ozoneframework/php/core/ParameterList.php
@@ -8,34 +8,34 @@ namespace Ozone\Framework;
  */
 class ParameterList {
 
-	private array $parameterArray = [];
-	private array $parameterTypes = [];
-	private array $parameterFrom = [];
+    private array $parameterArray = [];
+    private array $parameterTypes = [];
+    private array $parameterFrom = [];
 
-	private array $allParameters = [];
+    private array $allParameters = [];
 
-	public function initParameterList($runData): void
+    public function initParameterList($runData): void
     {
-		if($runData->isAjaxMode()){
-			$this->allParameters['AMODULE'] = [];
-			foreach ($_POST as $key => $value) {
-				$value = $this->fixNewLines($value);
+        if($runData->isAjaxMode()){
+            $this->allParameters['AMODULE'] = [];
+            foreach ($_POST as $key => $value) {
+                $value = $this->fixNewLines($value);
 
-				$this->parameterArray[$key] = $value;
-				$this->parameterTypes[$key] = "AMODULE";
-				$this->parameterFrom[$key] = 0; // 0 means "direct", + values means 'inherited'
-				$this->allParameters['AMODULE'][$key] = $value;
-			}
-		} else{
-			//initialize GET parameters from the url... because of mod_rewrite
-			$qs =  $_SERVER['QUERY_STRING'];
-			/* Check if there is a "?" char - if so, remove it. */
-			$qs = preg_replace('/\?.*$/', '', $qs);
-			$split = explode('/', $qs);
-			if (count($split) >= 1) {
-				$this->parameterArray['template'] = $split[0];
-				$this->parameterTypes['template'] = 'GET';
-			}
+                $this->parameterArray[$key] = $value;
+                $this->parameterTypes[$key] = "AMODULE";
+                $this->parameterFrom[$key] = 0; // 0 means "direct", + values means 'inherited'
+                $this->allParameters['AMODULE'][$key] = $value;
+            }
+        } else{
+            //initialize GET parameters from the url... because of mod_rewrite
+            $qs =  $_SERVER['QUERY_STRING'];
+            /* Check if there is a "?" char - if so, remove it. */
+            $qs = preg_replace('/\?.*$/', '', $qs);
+            $split = explode('/', $qs);
+            if (count($split) >= 1) {
+                $this->parameterArray['template'] = $split[0];
+                $this->parameterTypes['template'] = 'GET';
+            }
 
             /**
              * If an & is present in the URI, split that into key-value pairs.
@@ -53,28 +53,30 @@ class ParameterList {
                 $this->allParameters['GET'][$key] = urldecode($value);
             }
 
-			// now populate other parameters...
-			$this->allParameters['GET'] = [];
-			for($i=1; $i<count($split); $i+=2){
-				$key = $split[$i];
-				$value=$split[$i+1];
-				$this->parameterArray[$key] = urldecode($value);
-				$this->parameterTypes[$key] = 'GET';
-				$this->parameterFrom[$key] = 0;
-				$this->allParameters['GET'][$key] = urldecode($value);
-			}
+            // Parse path parameters.
+            // This was edited to be more flexible in how it handles parameters, see
+            // https://github.com/scpwiki/wikidot-path
+            $this->allParameters['GET'] = [];
+            for($i = 1; $i < count($split); $i += 2){
+                $key = $split[$i];
+                $value=$split[$i+1];
+                $this->parameterArray[$key] = urldecode($value);
+                $this->parameterTypes[$key] = 'GET';
+                $this->parameterFrom[$key] = 0;
+                $this->allParameters['GET'][$key] = urldecode($value);
+            }
 
-			// POST parameters are not affected by mod_rewrite
-			$this->allParameters['POST'] = [];
-			foreach ($_POST as $key => $value) {
-				$value = $this->fixNewLines($value);
-				$this->parameterArray[$key] = $value;
-				$this->parameterTypes[$key] = 'POST';
-				$this->parameterFrom[$key] = 0;
-				$this->allParameters['POST'][$key] = urldecode($value);
-			}
-		}
-	}
+            // POST parameters are not affected by mod_rewrite
+            $this->allParameters['POST'] = [];
+            foreach ($_POST as $key => $value) {
+                $value = $this->fixNewLines($value);
+                $this->parameterArray[$key] = $value;
+                $this->parameterTypes[$key] = 'POST';
+                $this->parameterFrom[$key] = 0;
+                $this->allParameters['POST'][$key] = urldecode($value);
+            }
+        }
+    }
 
     /**
      * Converts an arbitrary parameter value to its appropriate PHP type.
@@ -107,21 +109,21 @@ class ParameterList {
         return $value;
     }
 
-	public function containsParameter(string $name): bool
+    public function containsParameter(string $name): bool
     {
-		return !array_search($name, $this->parameterArray) === false;
-	}
+        return !array_search($name, $this->parameterArray) === false;
+    }
 
     /**
      * @return mixed|null
      */
-	public function getParameterValue(string $name, $type = null, $type2 = null)
-	{
-		if($type == null || $this->parameterTypes[$name] == $type || $this->parameterTypes[$name] == $type2){
-			return $this->parameterArray[$name];
-		}
-		return null;
-	}
+    public function getParameterValue(string $name, $type = null, $type2 = null)
+    {
+        if($type == null || $this->parameterTypes[$name] == $type || $this->parameterTypes[$name] == $type2){
+            return $this->parameterArray[$name];
+        }
+        return null;
+    }
 
     /**
      * Like getParameterValue(), but 'casts' the string to boolean.
@@ -147,66 +149,66 @@ class ParameterList {
         }
     }
 
-	public function delParameter(string $key): void
+    public function delParameter(string $key): void
     {
-		unset($this->parameterArray[$key]);
-		unset($this->parameterTypes[$key]);
-	}
+        unset($this->parameterArray[$key]);
+        unset($this->parameterTypes[$key]);
+    }
 
     /**
      * Returns type of the passed parameter: POST or GET.
      * @param $name
      * @return string
      */
-	public function getParameterType(string $name): string
-	{
-		return $this->parameterTypes[$name];
-	}
-
-	public function asArray(): array
+    public function getParameterType(string $name): string
     {
-		return $this->parameterArray;
-	}
+        return $this->parameterTypes[$name];
+    }
 
-	public function asArrayAll(): array
+    public function asArray(): array
     {
-		return $this->allParameters;
-	}
+        return $this->parameterArray;
+    }
 
-	public function addParameter(string $key, string $value, ?string $type=null): void
+    public function asArrayAll(): array
+    {
+        return $this->allParameters;
+    }
+
+    public function addParameter(string $key, string $value, ?string $type=null): void
     {
         $this->parameterArray["$key"] = $value;
         $this->parameterTypes["$key"] = $type;
         $this->allParameters[$type][$key] = $value;
-	}
+    }
 
-	public function numberOfParameters(): int
+    public function numberOfParameters(): int
     {
-		return count($this->parameterArray);
-	}
+        return count($this->parameterArray);
+    }
 
-	private function fixNewLines(string $text): string
+    private function fixNewLines(string $text): string
     {
-		$text = str_replace("\r\n", "\n", $text);
-		$text = str_replace("\r", "\n", $text);
-		return $text;
-	}
+        $text = str_replace("\r\n", "\n", $text);
+        $text = str_replace("\r", "\n", $text);
+        return $text;
+    }
 
-	public function getParametersByType(?string $type) {
-		$out = [];
-		foreach($this->parameterArray as $key => $value){
-			if($this->parameterTypes[$key] === $type){
-				$out[$key] = $value;
-			}
-		}
-		return $out;
-	}
+    public function getParametersByType(?string $type) {
+        $out = [];
+        foreach($this->parameterArray as $key => $value){
+            if($this->parameterTypes[$key] === $type){
+                $out[$key] = $value;
+            }
+        }
+        return $out;
+    }
 
-	public function resolveParameter(string $key, string $from) {
-		if(isset($this->allParameters[$from][$key])) {
-			return $this->allParameters[$from][$key];
-		} else {
- 			return null;
-		}
-	}
+    public function resolveParameter(string $key, string $from) {
+        if(isset($this->allParameters[$from][$key])) {
+            return $this->allParameters[$from][$key];
+        } else {
+             return null;
+        }
+    }
 }
