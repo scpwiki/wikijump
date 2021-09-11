c7deea2e024d9e3d5fa6ed7ef6cddd767e685e99
Ammon Smith <ammon.i.smith@gmail.com>
2021-09-10 21:25:16 -0400

    Fix block name in regex search.

--- ftml/src/includes/mod.rs
+++ ftml/src/includes/mod.rs
@@ -35,7 +35,7 @@ use regex::{Regex, RegexBuilder};
 
 lazy_static! {
     static ref INCLUDE_REGEX: Regex = {
-        RegexBuilder::new(r"^\[\[\s*include\s+")
+        RegexBuilder::new(r"^\[\[\s*include-messy\s+")
             .case_insensitive(true)
             .multi_line(true)
             .dot_matches_new_line(true)
