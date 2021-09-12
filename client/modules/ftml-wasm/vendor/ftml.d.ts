d9c7ca59b784e0a6720967e0636bb6e64179c381
Monkatraz <34875062+Monkatraz@users.noreply.github.com>
2021-09-11 20:29:35 -0600

    repack ftml, but with SPEEEED

--- client/modules/ftml-wasm/vendor/ftml.d.ts
+++ client/modules/ftml-wasm/vendor/ftml.d.ts
@@ -1,21 +1,6 @@
 /* tslint:disable */
 /* eslint-disable */
 /**
-* @returns {string}
-*/
-export function version(): string;
-/**
-* @param {PageInfo} page_info
-* @param {Tokenization} tokens
-* @returns {ParseOutcome}
-*/
-export function parse(page_info: PageInfo, tokens: Tokenization): ParseOutcome;
-/**
-* @param {string} text
-* @returns {string}
-*/
-export function preprocess(text: string): string;
-/**
 * @param {PageInfo} page_info
 * @param {SyntaxTree} syntax_tree
 * @returns {HtmlOutput}
@@ -28,10 +13,25 @@ export function render_html(page_info: PageInfo, syntax_tree: SyntaxTree): HtmlO
 */
 export function render_text(page_info: PageInfo, syntax_tree: SyntaxTree): string;
 /**
+* @returns {string}
+*/
+export function version(): string;
+/**
 * @param {string} text
 * @returns {Tokenization}
 */
 export function tokenize(text: string): Tokenization;
+/**
+* @param {PageInfo} page_info
+* @param {Tokenization} tokens
+* @returns {ParseOutcome}
+*/
+export function parse(page_info: PageInfo, tokens: Tokenization): ParseOutcome;
+/**
+* @param {string} text
+* @returns {string}
+*/
+export function preprocess(text: string): string;
 
 
 export interface IPageInfo {
@@ -43,30 +43,6 @@ export interface IPageInfo {
     rating: number;
     tags: string[];
     language: string;
-}
-
-
-
-
-
-export interface IElement {
-    element: string;
-    data?: any;
-}
-
-export interface ISyntaxTree {
-    elements: IElement[];
-    styles: string[];
-}
-
-export interface IParseWarning {
-    token: string;
-    rule: string;
-    span: {
-        start: number;
-        end: number;
-    };
-    kind: string;
 }
 
 
@@ -102,6 +78,30 @@ export interface IToken {
         start: number;
         end: number;
     };
+}
+
+
+
+
+
+export interface IElement {
+    element: string;
+    data?: any;
+}
+
+export interface ISyntaxTree {
+    elements: IElement[];
+    styles: string[];
+}
+
+export interface IParseWarning {
+    token: string;
+    rule: string;
+    span: {
+        start: number;
+        end: number;
+    };
+    kind: string;
 }
 
 
@@ -246,7 +246,6 @@ export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembl
 
 export interface InitOutput {
   readonly memory: WebAssembly.Memory;
-  readonly version: (a: number) => void;
   readonly __wbg_pageinfo_free: (a: number) => void;
   readonly pageinfo_new: (a: number) => number;
   readonly pageinfo_page: (a: number, b: number) => void;
@@ -257,13 +256,6 @@ export interface InitOutput {
   readonly pageinfo_rating: (a: number) => number;
   readonly pageinfo_tags: (a: number) => number;
   readonly pageinfo_language: (a: number, b: number) => void;
-  readonly __wbg_parseoutcome_free: (a: number) => void;
-  readonly parseoutcome_syntax_tree: (a: number) => number;
-  readonly parseoutcome_warnings: (a: number) => number;
-  readonly __wbg_syntaxtree_free: (a: number) => void;
-  readonly syntaxtree_data: (a: number) => number;
-  readonly parse: (a: number, b: number) => number;
-  readonly preprocess: (a: number, b: number, c: number) => void;
   readonly __wbg_htmloutput_free: (a: number) => void;
   readonly htmloutput_copy: (a: number) => number;
   readonly htmloutput_body: (a: number, b: number) => void;
@@ -272,18 +264,26 @@ export interface InitOutput {
   readonly htmloutput_backlinks: (a: number) => number;
   readonly render_html: (a: number, b: number) => number;
   readonly render_text: (a: number, b: number, c: number) => void;
+  readonly pageinfo_copy: (a: number) => number;
+  readonly __wbg_utf16indexmap_free: (a: number) => void;
+  readonly utf16indexmap_new: (a: number, b: number) => number;
+  readonly utf16indexmap_copy: (a: number) => number;
+  readonly utf16indexmap_get_index: (a: number, b: number) => number;
+  readonly version: (a: number) => void;
   readonly __wbg_tokenization_free: (a: number) => void;
+  readonly tokenization_copy: (a: number) => number;
   readonly tokenization_text: (a: number, b: number) => void;
   readonly tokenization_tokens: (a: number) => number;
   readonly tokenize: (a: number, b: number) => number;
-  readonly __wbg_utf16indexmap_free: (a: number) => void;
-  readonly utf16indexmap_new: (a: number, b: number) => number;
-  readonly utf16indexmap_get_index: (a: number, b: number) => number;
+  readonly __wbg_parseoutcome_free: (a: number) => void;
   readonly parseoutcome_copy: (a: number) => number;
+  readonly parseoutcome_syntax_tree: (a: number) => number;
+  readonly parseoutcome_warnings: (a: number) => number;
+  readonly __wbg_syntaxtree_free: (a: number) => void;
+  readonly syntaxtree_data: (a: number) => number;
+  readonly parse: (a: number, b: number) => number;
+  readonly preprocess: (a: number, b: number, c: number) => void;
   readonly syntaxtree_copy: (a: number) => number;
-  readonly pageinfo_copy: (a: number) => number;
-  readonly tokenization_copy: (a: number) => number;
-  readonly utf16indexmap_copy: (a: number) => number;
   readonly __wbindgen_malloc: (a: number) => number;
   readonly __wbindgen_realloc: (a: number, b: number, c: number) => number;
   readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
