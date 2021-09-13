0d8242e9c55aac454fdd456cafaa59d34abee069
Monkatraz <34875062+Monkatraz@users.noreply.github.com>
2021-09-13 06:55:13 -0600

    beautify logs, make publish ask if you're sure

---
+++ client/scripts/pretty-logs.js
@@ -0,0 +1,70 @@
+const readline = require("readline")
+const { execSync } = require("child_process")
+const chalk = require("chalk")
+
+function linebreak() {
+  console.log("")
+}
+
+function separator() {
+  console.log(chalk.gray("-------------------------"))
+}
+
+function header(title) {
+  const dashes = Math.round((24 - (title.length + 2)) / 2)
+  const chrs = "─".repeat(dashes)
+  console.log(chalk.bgBlue(`${chrs} ${title} ${chrs}`))
+}
+
+function section(title) {
+  const dashes = Math.round((24 - (title.length + 2)) / 2)
+  const chrs = "─".repeat(dashes)
+  console.log(chalk.blueBright(`${chrs} ${title} ${chrs}`))
+}
+
+function info(...msgs) {
+  const msg = msgs.join("\n")
+  console.log(chalk.green(msg))
+}
+
+function warn(...msgs) {
+  const msg = msgs.join("\n")
+  console.warn(chalk.yellow("-------- WARNING --------"))
+  console.warn(chalk.yellow(msg))
+}
+
+function error(...msgs) {
+  const msg = msgs.join("\n")
+  console.warn(chalk.redBright("--------- ERROR ---------"))
+  console.warn(chalk.redBright(msg))
+}
+
+function cmd(command) {
+  execSync(command, { stdio: "inherit" })
+}
+
+function question(question) {
+  return new Promise((resolve, reject) => {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    })
+
+    rl.question(chalk.magentaBright(question), answer => {
+      rl.close()
+      resolve(answer)
+    })
+  })
+}
+
+module.exports = {
+  linebreak,
+  separator,
+  header,
+  section,
+  info,
+  warn,
+  error,
+  cmd,
+  question
+}
