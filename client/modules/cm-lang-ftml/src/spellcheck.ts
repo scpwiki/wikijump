183f49ee60f14075f68823733a59f1fa4d48eb02
Monkatraz <34875062+Monkatraz@users.noreply.github.com>
2021-09-18 15:02:11 -0600

    update ftml spellcheck blacklist

--- client/modules/cm-lang-ftml/src/spellcheck.ts
+++ client/modules/cm-lang-ftml/src/spellcheck.ts
@@ -2,27 +2,31 @@ import type { Tree } from "@lezer/common"
 import type { Word } from "@wikijump/cm-espells"
 import type { EditorState } from "@wikijump/codemirror/cm"
 
+const BLACKLIST_REGEX = [
+  /^BlockName/,
+  /^BlockNode/,
+  /^Include/,
+  /^Module/,
+  /^Link/,
+  /^PageVariable/,
+  /^TripleLink/,
+  /^SingleLink/
+]
+
 const BLACKLIST = [
-  "BlockName",
-  "BlockNameUnknown",
-  "BlockNameModule",
-  "BlockNameInclude",
-  "BlockLabel",
   "BlockComment",
-  "BlockNodeArgumentValue",
-  "BlockNodeArgumentName",
-  "IncludeParameterProperty",
+  "BlockLabel",
   "BlockValue",
-  "IncludeValue",
-  "ValueName",
-  "ModuleName",
-  "ModuleNameUnknown",
-  "link",
-  "color"
+  "String",
+  "EmbeddedParser",
+  "RawEscapeBlock",
+  "Escaped",
+  "ColoredTextColor"
 ]
 
 export function spellcheckFTML(state: EditorState, tree: Tree, word: Word) {
   const node = tree.resolve(word.from, 1)
   if (BLACKLIST.includes(node.name)) return false
+  if (BLACKLIST_REGEX.some(re => re.test(node.name))) return false
   return true
 }
