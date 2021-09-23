b443fdf73acfd46f5cbb60bdee538f5e8df95e75
Monkatraz <34875062+Monkatraz@users.noreply.github.com>
2021-09-23 16:23:21 -0600

    make ftml components use their own scss

--- client/modules/ftml-components/src/index.ts
+++ client/modules/ftml-components/src/index.ts
@@ -1,7 +1,1 @@
-// TODO: this css import is temporary
-// it's just for the sake of making sure this css always gets loaded
-// but if we were to make this, say, a NPM module, it's probably better to just
-// leave a convinent way to import the css file rather than force it to be loaded
-import "@root/ftml/misc/ftml-base.css"
-
-export * from "./components/code"
+export * from "./components/code/code"
