73a03bcffdbd52deb01b8e9c3e71019146b4b106
Ammon Smith <ammon.i.smith@gmail.com>
2021-09-25 18:16:05 -0400

    Add stub for BLOCK_MATH.

---
+++ ftml/src/parsing/rule/impls/block/blocks/math.rs
@@ -0,0 +1,48 @@
+/*
+ * parsing/rule/impls/block/blocks/math.rs
+ *
+ * ftml - Library to parse Wikidot text
+ * Copyright (C) 2019-2021 Wikijump Team
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+use super::prelude::*;
+
+pub const BLOCK_MATH: BlockRule = BlockRule {
+    name: "block-math",
+    accepts_names: &["math"],
+    accepts_star: false,
+    accepts_score: false,
+    accepts_newlines: true,
+    parse_fn,
+};
+
+fn parse_fn<'r, 't>(
+    log: &Logger,
+    parser: &mut Parser<'r, 't>,
+    name: &'t str,
+    flag_star: bool,
+    flag_score: bool,
+    in_head: bool,
+) -> ParseResult<'r, 't, Elements<'t>> {
+    info!(
+        log,
+        "Parsing math block";
+        "in-head" => in_head,
+        "name" => name,
+    );
+
+    todo!()
+}
