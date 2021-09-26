8cea3cbadd8b731d94849f7c706ca0419047dc31
Ammon Smith <ammon.i.smith@gmail.com>
2021-09-26 06:25:11 -0400

    Add time zone to default datetime format string.

--- ftml/src/render/mod.rs
+++ ftml/src/render/mod.rs
@@ -34,7 +34,7 @@ pub mod text;
 mod handle;
 
 /// The default format string for `[[date]]`, if none is specified.
-pub const DEFAULT_DATETIME_FORMAT: &str = "%B %d, %Y %I:%M:%S %p";
+pub const DEFAULT_DATETIME_FORMAT: &str = "%B %d, %Y %I:%M:%S %p %Z";
 
 use self::handle::{Handle, ModuleRenderMode};
 use crate::log::prelude::*;
