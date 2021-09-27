baa9d7cf54b3969f893e169dbdb4385b407da48d
Bri <34875062+Monkatraz@users.noreply.github.com>
2021-09-27 03:25:17 -0600

    Accept removing pointless comma
    Co-authored-by: Ammon Smith <ammon.i.smith@gmail.com>

--- ftml/src/data/user_info.rs
+++ ftml/src/data/user_info.rs
@@ -58,7 +58,7 @@ impl UserInfo<'_> {
             user_name: cow!("michal-frackowiak"),
             user_karma: 5,
             user_avatar_data: cow!(AVATAR_BASE64_DATA),
-            user_profile_url: cow!("/user:info/michal-frackowiak",),
+            user_profile_url: cow!("/user:info/michal-frackowiak"),
         }
     }
 }
