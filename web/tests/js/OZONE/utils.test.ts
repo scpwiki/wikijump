48d17176a631eaa80c493e47a85fc07c24cf8aba
Monkatraz <34875062+Monkatraz@users.noreply.github.com>
2021-09-29 17:36:31 -0600

    update old esbuild commits

--- web/tests/js/OZONE/utils.test.ts
+++
@@ -1,8 +0,0 @@
-import OZONE from "@/javascript/OZONE";
-
-describe("arrayToPostData", () => {
-  it("converts an object to a query string", () => {
-    const obj = { one: "two", three: "four" };
-    expect(OZONE.utils.arrayToPostData(obj)).toEqual("one=two&three=four");
-  });
-});
