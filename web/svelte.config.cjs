ee6586862434efbe7ee083b380c28d6aab7b5701
Monkatraz <34875062+Monkatraz@users.noreply.github.com>
2021-09-30 00:24:18 -0600

    remove unneeded file

--- web/svelte.config.cjs
+++
@@ -1,9 +0,0 @@
-const sveltePreprocess = require("svelte-preprocess")
-
-module.exports = {
-  preprocess: [
-    sveltePreprocess({
-      sass: { sourceMapEmbed: true, sourceMapContents: true, sourceMap: true }
-    })
-  ]
-}
