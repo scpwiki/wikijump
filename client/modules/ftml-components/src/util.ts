614979b8cccff22cecafc3bed3f3b24b6b728846
Monkatraz <34875062+Monkatraz@users.noreply.github.com>
2021-09-30 18:36:24 -0600

    remove old function

--- client/modules/ftml-components/src/util.ts
+++ client/modules/ftml-components/src/util.ts
@@ -1,20 +1,4 @@
-/**
- * Safely defines a `customElement`.
- *
- * @param tagName - The tag name to define.
- * @param element - The class of the custom element.
- * @param options - Options for the custom element definition.
- */
-export function defineElement(
-  tagName: string,
-  element: CustomElementConstructor,
-  options?: ElementDefinitionOptions
-) {
-  if (!customElements.get(tagName)) {
-    customElements.define(tagName, element, options)
-  }
-}
-
+/** Autonomous custom element class type. */
 export type CustomElement = typeof HTMLElement & { tag: string }
 
 /**
