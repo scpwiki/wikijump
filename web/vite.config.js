a045a937a89276dccc98644aadfd0f7626b6772d
Monkatraz <34875062+Monkatraz@users.noreply.github.com>
2021-10-03 23:02:30 -0600

    adjust config to not need artisan

--- web/vite.config.js
+++ web/vite.config.js
@@ -4,7 +4,26 @@ import { SASS_OPTIONS, SVELTE_OPTIONS } from "./scripts/vite-config"
 import vitePluginToml from "./scripts/vite-plugin-toml"
 import vitePluginYaml from "./scripts/vite-plugin-yaml"
 
-export default defineConfig()
+// HACK
+// This is a _hard-coded return value_ from the following Artisan command:
+// php artisan vite:config
+// We need this JSON or else `laravel-vite` will be forced to run the command itself,
+// if it does this, it will fail on build, because it will be unable to use PHP in
+// the container it runs in.
+// TODO: remove this if possible
+const PHP_CONFIG = {
+  "entrypoints": ["resources/scripts/index.ts"],
+  "ignore_patterns": ["/\\.d\\.ts$/"],
+  "aliases": { "@": "resources" },
+  "public_directory": "resources/static",
+  "ping_timeout": 10,
+  "ping_url": null,
+  "build_path": "build",
+  "dev_url": "http://localhost:3000",
+  "commands": []
+}
+
+export default defineConfig({}, PHP_CONFIG)
   .withPlugins(vitePluginToml, vitePluginYaml)
   .withPlugin(svelte(SVELTE_OPTIONS))
   .merge({
