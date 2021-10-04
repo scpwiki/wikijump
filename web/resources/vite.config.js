dc2d98a14cff9d00a724cf5a1c8a79b8932929f1
Monkatraz <34875062+Monkatraz@users.noreply.github.com>
2021-10-04 15:35:15 -0600

    make resources itself a package

--- web/resources/vite.config.js
+++ web/resources/vite.config.js
@@ -1,21 +1,19 @@
 import { svelte } from "@sveltejs/vite-plugin-svelte"
 import { defineConfig } from "laravel-vite"
-import { SASS_OPTIONS, SVELTE_OPTIONS } from "./scripts/vite-config"
-import vitePluginToml from "./scripts/vite-plugin-toml"
-import vitePluginYaml from "./scripts/vite-plugin-yaml"
+import { SASS_OPTIONS, SVELTE_OPTIONS } from "../scripts/vite-config.js"
+import vitePluginToml from "../scripts/vite-plugin-toml.js"
+import vitePluginYaml from "../scripts/vite-plugin-yaml.js"
 
-// HACK
 // This is a _hard-coded return value_ from the following Artisan command:
 // php artisan vite:config
 // We need this JSON or else `laravel-vite` will be forced to run the command itself,
 // if it does this, it will fail on build, because it will be unable to use PHP in
 // the container it runs in.
-// TODO: remove this if possible
 const PHP_CONFIG = {
-  "entrypoints": ["resources/scripts/index.ts"],
+  "entrypoints": ["scripts/index.ts"],
   "ignore_patterns": ["/\\.d\\.ts$/"],
-  "aliases": { "@": "resources" },
-  "public_directory": "resources/static",
+  "aliases": { "@": "../resources" },
+  "public_directory": "static",
   "ping_timeout": 10,
   "ping_url": null,
   "build_path": "build",
@@ -27,6 +25,8 @@ export default defineConfig({}, PHP_CONFIG)
   .withPlugins(vitePluginToml, vitePluginYaml)
   .withPlugin(svelte(SVELTE_OPTIONS))
   .merge({
+    root: "../",
+
     css: {
       preprocessorOptions: {
         scss: SASS_OPTIONS
