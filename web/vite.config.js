adfc320fbbab9b4b6dd135af95b0a40e40814036
Monkatraz <34875062+Monkatraz@users.noreply.github.com>
2021-10-05 00:17:22 -0600

    manually point to vite's config

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,8 +1,14 @@
+const fs = require("fs")
 import { svelte } from "@sveltejs/vite-plugin-svelte"
 import { defineConfig } from "laravel-vite"
 import { SASS_OPTIONS, SVELTE_OPTIONS } from "./scripts/vite-config.js"
 import vitePluginToml from "./scripts/vite-plugin-toml.js"
 import vitePluginYaml from "./scripts/vite-plugin-yaml.js"
+
+const entrypoints = fs
+  .readdirSync("resources/scripts")
+  .filter(ent => fs.statSync(`resources/scripts/${ent}`).isFile())
+  .map(file => `resources/scripts/${file}`)
 
 // This is a modified return value from the following Artisan command:
 // php artisan vite:config
@@ -10,7 +16,7 @@ import vitePluginYaml from "./scripts/vite-plugin-yaml.js"
 // if it does this, it will fail on build, because it will be unable to use PHP in
 // the container it runs in.
 const PHP_CONFIG = {
-  "entrypoints": ["resources/scripts/index.ts"],
+  "entrypoints": entrypoints,
   "ignore_patterns": ["/\\.d\\.ts$/"],
   "aliases": { "@": "resources" },
   "public_directory": "resources/static",
@@ -21,7 +27,7 @@ const PHP_CONFIG = {
   "commands": []
 }
 
-export default defineConfig({}, PHP_CONFIG)
+const config = defineConfig({}, PHP_CONFIG)
   .withPlugins(vitePluginToml, vitePluginYaml, svelte(SVELTE_OPTIONS))
   .merge({
     server: {
@@ -41,3 +47,5 @@ export default defineConfig({}, PHP_CONFIG)
       cssCodeSplit: false
     }
   })
+
+export default config
