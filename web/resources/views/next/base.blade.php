0fc7681f5607c2f648a569ab3e9387cff941bd45
Monkatraz <34875062+Monkatraz@users.noreply.github.com>
2021-10-06 00:24:07 -0600

    add CSRF token

--- web/resources/views/next/base.blade.php
+++ web/resources/views/next/base.blade.php
@@ -39,6 +39,7 @@
     <meta charset="utf-8">
     <base href="/">
     <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
 
     {{-- Security --}}
     <meta name="referrer" content="no-referrer">
