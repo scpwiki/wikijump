dbf18132ca14385cee0418d694055a3a00755688
Ammon Smith <ammon.i.smith@gmail.com>
2021-10-09 01:14:53 -0400

    Minor PHP refatoring.

--- web/php/Modules/ManageSite/ManageSiteLicenseModule.php
+++ web/php/Modules/ManageSite/ManageSiteLicenseModule.php
@@ -12,23 +12,23 @@ class ManageSiteLicenseModule extends ManageSiteBaseModule
     public function build($runData)
     {
 
-        $site = $runData->getTemp("site");
-        $runData->contextAdd("site", $site);
+        $site = $runData->getTemp('site');
+        $runData->contextAdd('site', $site);
 
         // get all categories for the site
         $c = new Criteria();
-        $c->add("site_id", $site->getSiteId());
+        $c->add('site_id', $site->getSiteId());
         $c->addOrderAscending("replace(name, '_', '00000000')");
         $categories = CategoryPeer::instance()->select($c);
 
-        $runData->contextAdd("categories", $categories);
+        $runData->contextAdd('categories', $categories);
 
         // also prepare categories to put into javascript...
-        $cats2 = array();
+        $cats2 = [];
         foreach ($categories as $category) {
-            $cats2[] = $category->getFieldValuesArray();
+            array_push($cats2, $category->getFieldValuesArray());
         }
-        $runData->ajaxResponseAdd("categories", $cats2);
+        $runData->ajaxResponseAdd('categories', $cats2);
 
         // Add license data
         $runData->contextAdd('licenses', config('licenses'));
