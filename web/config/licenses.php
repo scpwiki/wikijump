7bbb51b7068e1207cc99625ec7e296d0863c233a
Ammon Smith <ammon.i.smith@gmail.com>
2021-10-09 23:14:05 -0400

    Emit simple arrays in config/licenses.php

--- web/config/licenses.php
+++ web/config/licenses.php
@@ -1,100 +1,97 @@
 <?php
 declare(strict_types=1);
 
-use \Wikijump\Common\License;
+return [
+    // Creative Commons 4.0
+    [
+        'id' => 'cc_by_sa_4',
+        'name' => 'Creative Commons Attribution-ShareAlike 4.0 License (recommended)',
+        'url' => 'https://creativecommons.org/licenses/by-sa/4.0/',
+    ],
+    [
+        'id' => 'cc_by_4',
+        'name' => 'Creative Commons Attribution 4.0 License',
+        'url' => 'https://creativecommons.org/licenses/by/4.0/',
+    ],
+    [
+        'id' => 'cc_by_nd_4',
+        'name' => 'Creative Commons Attribution-NoDerivs 4.0 License',
+        'url' => 'https://creativecommons.org/licenses/by-nd/4.0/',
+    ],
+    [
+        'id' => 'cc_by_nc_4',
+        'name' => 'Creative Commons Attribution-NonCommercial 4.0 License',
+        'url' => 'https://creativecommons.org/licenses/by-nc/4.0/',
+    ],
+    [
+        'id' => 'cc_by_nc_sa_4',
+        'name' => 'Creative Commons Attribution-NonCommercial-ShareAlike 4.0 License',
+        'url' => 'https://creativecommons.org/licenses/by-nc-sa/4.0/',
+    ],
+    [
+        'id' => 'cc_by_nc_nd_4',
+        'name' => 'Creative Commons Attribution-NonCommercial-NoDerivs 4.0 License',
+        'url' => 'https://creativecommons.org/licenses/by-nc-nd/4.0/',
+    ],
+
+    // Creative Commons 3.0
+    [
+        'id' => 'cc_by_sa_3',
+        'name' => 'Creative Commons Attribution-ShareAlike 3.0 License',
+        'url' => 'https://creativecommons.org/licenses/by-sa/3.0/',
+    ],
+    [
+        'id' => 'cc_by_3',
+        'name' => 'Creative Commons Attribution 3.0 License',
+        'url' => 'https://creativecommons.org/licenses/by/3.0/',
+    ],
+    [
+        'id' => 'cc_by_nd_3',
+        'name' => 'Creative Commons Attribution-NoDerivs 3.0 License',
+        'url' => 'https://creativecommons.org/licenses/by-nd/3.0/',
+    ],
+    [
+        'id' => 'cc_by_nc_3',
+        'name' => 'Creative Commons Attribution-NonCommercial 3.0 License',
+        'url' => 'https://creativecommons.org/licenses/by-nc/3.0/',
+    ],
+    [
+        'id' => 'cc_by_nc_sa_3',
+        'name' => 'Creative Commons Attribution-NonCommercial-ShareAlike 3.0 License',
+        'url' => 'https://creativecommons.org/licenses/by-nc-sa/3.0/',
+    ],
+    [
+        'id' => 'cc_by_nc_nd_3',
+        'name' => 'Creative Commons Attribution-NonCommercial-NoDerivs 3.0 License',
+        'url' => 'https://creativecommons.org/licenses/by-nc-nd/3.0/',
+    ],
+
+    // Other Licenses
+    [
+        'id' => 'cc0',
+        'name' => 'CC0 (Public Domain)',
+        'url' => 'https://creativecommons.org/publicdomain/zero/1.0/',
+    ],
+    [
+        'id' => 'gnu_fdl_13',
+        'name' => 'GNU Free Documentation License 1.3',
+        'url' => 'https://www.gnu.org/copyleft/fdl.html',
+    ],
+
+    // Fallback
+    [
+        'id' => 'standard',
+        'name' => 'Standard copyright (not recommended)',
+        'url' => null,
+        'unless' => false,
+    ],
+];
 
 // Licenses in display order
 $list = [
-    // Creative Commons 4.0
-    new License(
-        'cc_by_sa_4',
-        'Creative Commons Attribution-ShareAlike 4.0 License (recommended)',
-        'https://creativecommons.org/licenses/by-sa/4.0/',
-    ),
-    new License(
-        'cc_by_4',
-        'Creative Commons Attribution 4.0 License',
-        'https://creativecommons.org/licenses/by/4.0/',
-    ),
-    new License(
-        'cc_by_nd_4',
-        'Creative Commons Attribution-NoDerivs 4.0 License',
-        'https://creativecommons.org/licenses/by-nd/4.0/',
-    ),
-    new License(
-        'cc_by_nc_4',
-        'Creative Commons Attribution-NonCommercial 4.0 License',
-        'https://creativecommons.org/licenses/by-nc/4.0/',
-    ),
-    new License(
-        'cc_by_nc_sa_4',
-        'Creative Commons Attribution-NonCommercial-ShareAlike 4.0 License',
-        'https://creativecommons.org/licenses/by-nc-sa/4.0/',
-    ),
-    new License(
-        'cc_by_nc_nd_4',
-        'Creative Commons Attribution-NonCommercial-NoDerivs 4.0 License',
-        'https://creativecommons.org/licenses/by-nc-nd/4.0/',
-    ),
-
-    // Creative Commons 3.0
-    new License(
-        'cc_by_sa_3',
-        'Creative Commons Attribution-ShareAlike 3.0 License',
-        'https://creativecommons.org/licenses/by-sa/3.0/',
-    ),
-    new License(
-        'cc_by_3',
-        'Creative Commons Attribution 3.0 License',
-        'https://creativecommons.org/licenses/by/3.0/',
-    ),
-    new License(
-        'cc_by_nd_3',
-        'Creative Commons Attribution-NoDerivs 3.0 License',
-        'https://creativecommons.org/licenses/by-nd/3.0/',
-    ),
-    new License(
-        'cc_by_nc_3',
-        'Creative Commons Attribution-NonCommercial 3.0 License',
-        'https://creativecommons.org/licenses/by-nc/3.0/',
-    ),
-    new License(
-        'cc_by_nc_sa_3',
-        'Creative Commons Attribution-NonCommercial-ShareAlike 3.0 License',
-        'https://creativecommons.org/licenses/by-nc-sa/3.0/',
-    ),
-    new License(
-        'cc_by_nc_nd_3',
-        'Creative Commons Attribution-NonCommercial-NoDerivs 3.0 License',
-        'https://creativecommons.org/licenses/by-nc-nd/3.0/',
-    ),
-
-    // Other Licenses
-    new License(
-        'cc0',
-        'CC0 (Public Domain)',
-        'https://creativecommons.org/publicdomain/zero/1.0/',
-    ),
-    new License(
-        'gnu_fdl_13',
-        'GNU Free Documentation License 1.3',
-        'https://www.gnu.org/copyleft/fdl.html',
-    ),
-
-    // Fallback
-    new License(
-        'standard',
-        'Standard copyright (not recommended)',
-        null,
-    )
 ];
 
-// Licenses by ID
-// (generated from the license list)
-$ids = [];
-foreach ($list as &$license) {
-    $ids[$license->id] = $license;
-}
+$ids = License::generateMapping($list);
 
 // TODO: add localization
 return [
