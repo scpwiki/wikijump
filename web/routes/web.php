7dc08fcc8ca1a01e40bd0d0a48ca39fab3e8eb93
Monkatraz <34875062+Monkatraz@users.noreply.github.com>
2021-10-13 00:16:03 -0600

    move logic to a page controller

--- web/routes/web.php
+++ web/routes/web.php
@@ -11,6 +11,7 @@ use Wikidot\Utils\AjaxModuleWikiFlowController;
 use Wikidot\Utils\GlobalProperties;
 use Wikijump\Helpers\LegacyTools;
 use Wikijump\Http\Controllers\OzoneController;
+use Wikijump\Http\Controllers\PageController;
 use Wikijump\Models\User;
 
 use const Wikijump\Helpers\LegacyTools;
@@ -150,120 +151,8 @@ if (GlobalProperties::$FEATURE_FRONTEND === 'next') {
         ->where("special", "system|admin")
         ->where("path", ".+");
 
-    Route::get('/{path?}', function () {
-        $values = LegacyTools::generateScreenVars();
-
-        // TODO: move this to a controller
-
-        $title = 'Wikijump';
-        $canonical = URL::current();
-        $license = null;
-
-        $social_title = null;
-
-        $slug = null;
-        $category = null;
-        $breadcrumbs = null;
-        $page_title = null;
-        $revision = null;
-        $last_edit_date = null;
-        $last_edit_days_since = null;
-        $tags = null;
-
-        if ($values['wikiPage']) {
-            $page = $values['wikiPage'];
-            $timestamp = $page->getDateLastEdited()->getTimestamp();
-
-            $slug = $page->getUnixName();
-            $page_title = $page->getTitleOrUnixName();
-            $revision = $page->getRevisionNumber();
-            $last_edit_date = strftime('%x %r', $timestamp);
-            $last_edit_days_since = floor((time() - $timestamp) / (60 * 60 * 24));
-            $tags = $values['tags'] ?? null;
-
-            $title = $page_title . ' | ' . $title;
-
-            $social_title = $page_title;
-
-            // this should always be there, but just in case...
-            if ($values['category']) {
-                $category = $values['category']->getName();
-
-                // we only want to provide license info if the page actually has one
-                $lic = $values['category']->getLicense();
-                if ($lic->url()) {
-                    $license = $lic;
-                }
-            }
-
-            if ($values['breadcrumbs']) {
-                $breadcrumbs = [];
-                foreach ($values['breadcrumbs'] as $breadcrumb) {
-                    array_push($breadcrumbs, [
-                        'title' => $breadcrumb->getTitleOrUnixName(),
-                        'slug' => $breadcrumb->getUnixName(),
-                    ]);
-                }
-            }
-        }
-
-        return view('next.wiki.page', [
-            // TODO: description, image, twitter, etc.
-            // TODO: site theming
-            // TODO: favicons
-            // TODO: header image/text + subtitle management
-
-            'title' => $title,
-            'canonical' => $canonical,
-            'license' => $license,
-
-            'social_title' => $social_title,
-            'social_type' => 'article',
-            'social_url' => $canonical,
-
-            'header_img_url' => '/files--static/media/logo-outline.min.svg',
-
-            'navbar_items' => [
-                'SCP Series' => [
-                    'Series VII' => '/',
-                    'Series VI' => '/',
-                    'Series V' => '/',
-                    'Series IV' => '/',
-                    'Series III' => '/',
-                    'Series II' => '/',
-                    'Series I' => '/',
-                ],
-                'Tales' => [
-                    'Foundation Tales' => '/',
-                    'Series Archive' => '/',
-                    'Incident Reports' => '/',
-                    'CreepyPasta Archive' => '/',
-                ],
-                'Library' => [],
-                'Universe' => [],
-                'SCP Global' => [],
-                'Background' => "/",
-                'About' => "/",
-            ],
-
-            'sidebar_content' => $values['sideBar1Content'] ?? null,
-            'page_content' => $values['pageContent'] ?? null,
-
-            'page_slug' => $slug,
-            'page_category' => $category,
-            'page_title' => $page_title,
-            'page_breadcrumbs' => $breadcrumbs,
-            'page_revision' => $revision,
-            'page_last_edit_date' => $last_edit_date,
-            'page_last_edit_days_since' => $last_edit_days_since,
-            'page_tags' => $tags,
-
-            'HTTP_SCHEMA' => GlobalProperties::$HTTP_SCHEMA,
-            'URL_DOMAIN' => GlobalProperties::$URL_DOMAIN,
-            'URL_HOST' => GlobalProperties::$URL_HOST,
-            'SERVICE_NAME' => GlobalProperties::$SERVICE_NAME,
-        ]);
-    })->where( "path", ".*" );
+    Route::get('/{path?}', [PageController::class, 'show'])
+        ->where( "path", ".*" );
 } else {
     /**
      * This fallback route will defer to the OzoneController, which will boot an
