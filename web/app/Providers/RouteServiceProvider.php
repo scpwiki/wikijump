0ad353116ccc90ccda81e20a5dd4d1f3dd30da9e
Monkatraz <34875062+Monkatraz@users.noreply.github.com>
2021-10-14 07:00:51 -0600

    fix api path

--- web/app/Providers/RouteServiceProvider.php
+++ web/app/Providers/RouteServiceProvider.php
@@ -44,7 +44,7 @@ class RouteServiceProvider extends ServiceProvider
         $this->configureRateLimiting();
 
         $this->routes(function () {
-            Route::prefix('api')
+            Route::prefix('api--v0')
                 ->middleware('api')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/api.php'));
--- web/modules/api/src/api.ts
+++ web/modules/api/src/api.ts
@@ -2,7 +2,7 @@ import { Api } from "../vendor/api"
 
 export class WikijumpAPI extends Api<void> {
   // TODO: allow giving a specific site here
-  constructor(baseUrl = "/api--v1") {
+  constructor(baseUrl = "/api--v0") {
     super({ baseUrl, baseApiParams: { format: "json" } })
   }
 }
