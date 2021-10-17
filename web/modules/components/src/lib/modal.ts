3074d8f78bb5861d5b3660dc2e484c1c94263175
Monkatraz <34875062+Monkatraz@users.noreply.github.com>
2021-10-17 12:55:25 -0600

    document modal

--- web/modules/components/src/lib/modal.ts
+++ web/modules/components/src/lib/modal.ts
@@ -1,12 +1,42 @@
 import { SvelteComponent } from "svelte"
 import Dialog from "../Dialog.svelte"
 
+/** Wraps around the {@link Dialog} component to provide a simple interface for */
 export class Modal<T extends typeof SvelteComponent> {
+  /** The internal {@link Dialog} component. */
   private dialog: Dialog
+
+  /** Keeps track of the open state. */
   private _open: boolean
 
+  /** The component being slotted into the dialog. */
   readonly component: T
 
+  /** Callback fired when the open state of the modal changes. */
+  declare onChange?: (open: boolean) => void
+
+  /** Callback fired when the modal opens. */
+  declare onOpen?: () => void
+
+  /** Callback fired when the modal closes. */
+  declare onClose?: () => void
+
+  /**
+   * Callback fired when the modal is "cancelled" by pressing escape. This
+   * callback is provided with the actual `cancel` event, so the
+   * cancellation itself can be cancelled by calling the `preventDefault()`
+   * method on the event.
+   *
+   * @param evt - The cancel event.
+   */
+  declare onCancel?: (evt: Event) => void
+
+  /**
+   * @param component - The component to be slotted into the dialog.
+   * @param open - Whether the dialog should be open initially.
+   * @param lazy - If true, slotted content will only be inserted into the
+   *   DOM when the dialog is open.
+   */
   constructor(component: T, open = false, lazy = true) {
     this.component = component
     this._open = open
@@ -29,24 +59,19 @@ export class Modal<T extends typeof SvelteComponent> {
     })
   }
 
+  /** The open state of the modal. Can be set to open or close it. */
   get open() {
     return this._open
   }
 
+  /** The open state of the modal. Can be set to open or close it. */
   set open(state: boolean) {
     if (this._open === state) return
     this.dialog.$set({ open: state })
     this._open = state
   }
 
-  declare onChange?: (open: boolean) => void
-
-  declare onOpen?: () => void
-
-  declare onClose?: () => void
-
-  declare onCancel?: (evt: Event) => void
-
+  /** Destroys the modal. */
   destroy() {
     this.dialog.$destroy()
   }
