08a00ccd3bfbb791142e723c240860ec9510e5b4
Monkatraz <34875062+Monkatraz@users.noreply.github.com>
2021-10-17 16:50:11 -0600

    add a basic login modal

---
+++ web/resources/lib/login-modal.ts
@@ -0,0 +1,4 @@
+import { Modal } from "@wikijump/components"
+import LoginPanel from "./components/LoginPanel.svelte"
+
+export const LoginModal = new Modal(LoginPanel, true)
--- web/resources/scripts/index.ts
+++ web/resources/scripts/index.ts
@@ -1,4 +1,5 @@
 import "@wikijump/ftml-components"
 import "../lib/elements/nav-dropdown"
+import "../lib/login-modal"
 
 export {}
