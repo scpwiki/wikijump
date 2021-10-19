0454ec3e1e3c3892d53bad8ec2d27c39fabdfb7b
Monkatraz <34875062+Monkatraz@users.noreply.github.com>
2021-10-19 11:55:43 -0600

    wrap legacy "_" function in a check

--- web/app/Helpers/helpers.php
+++ web/app/Helpers/helpers.php
@@ -18,13 +18,15 @@ function pg_is_error(Throwable $e, string $code): bool
     return (string) $e->getCode() === $code;
 }
 
-/**
- * Legacy helper for Ozone's gettext() functions.
- *
- * @deprecated Use __() instead.
- */
-function _(string $key): string
-{
-    Log::warning('Use of deprecated _() function');
-    return __($key);
+if (!function_exists('_')) {
+    /**
+     * Legacy helper for Ozone's gettext() functions.
+     *
+     * @deprecated Use __() instead.
+     */
+    function _(string $key): string
+    {
+        Log::warning('Use of deprecated _() function');
+        return __($key);
+    }
 }
