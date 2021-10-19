22d29fa6d6070fbdea80d9c19af19eb6a0e45125
Yossipossi1 <yossi@davidpc.localdomain>
2021-10-18 20:29:34 -0700

    added the stuff

--- web/app/Services/TagEngine/TagEngine.php
+++ web/app/Services/TagEngine/TagEngine.php
@@ -17,19 +17,11 @@ use Ds\Set;
 
         $current_tags   = new Set($current_tags); // Tags being saved to the page.
         $previous_tags  = new Set($previous_tags); // Tags previously saved to the page.
-        $allowed_tags   = new Set($configuration->allowed_tags); // What tags are allowed on the site.
 
         $valid          = true; // Whether or not the tags being saved are valid according to the site's configuration.
         $forbidden_tags = null; // What tags present in the current tags are not allowed on the site.
 
-
-        /* Check the tags against the tag whitelist. */
-        if (!$allowed_tags->isEmpty()) {
-            $forbidden_tags = $current_tags->diff($allowed_tags);
-            if(!$forbidden_tags->isEmpty()) {
-                $valid = false;
-            }
-        }
+        
 
         /* Return validator's decision along with relevant information if the tags are invalid. */
         return new TagDecision($valid, $forbidden_tags);
