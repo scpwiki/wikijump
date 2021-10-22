4276c912c69daf041b9bc4942c1e7bf9cf3c60af
Monkatraz <34875062+Monkatraz@users.noreply.github.com>
2021-10-21 22:50:09 -0600

    make auth styling generic

--- web/resources/scripts/auth-login.ts
+++ web/resources/scripts/auth-login.ts
@@ -1,8 +1,8 @@
 import LoginForm from "../lib/components/LoginForm.svelte"
-import "./login.scss"
+import "./auth.scss"
 
 window.addEventListener("DOMContentLoaded", () => {
-  const container = document.getElementById("login_form_container")
+  const container = document.getElementById("auth_form_container")
   if (!container) throw new Error("No container found")
   const form = new LoginForm({ target: container })
 })
--- web/resources/views/next/auth/login.blade.php
+++ web/resources/views/next/auth/login.blade.php
@@ -8,24 +8,26 @@
 ])
 
 @push('scripts')
-    @vite('login.ts')
+    @vite('auth-login.ts')
 @endpush
 
 @section('app')
-    <div id="app_login">
-        <div id="login_panel" class="light">
+    <div id="app_auth">
+        <div id="auth_panel" class="light">
             <a href="/" title="{{ __('frame.GOTO_HOME_PAGE') }}">
                 <img src="/files--static/media/logo.min.svg">
             </a>
             <hr>
-            <div id="login_form_container">
+
+            <div id="auth_form_container">
             </div>
-            <a id="login_create_account" href="/user-services/register">
+
+            <a id="auth_create_account" href="/user-services/register">
                 {{ __("account_panel.CREATE_ACCOUNT") }}
             </a>
 
             {{-- gets placed _outside_ of the panel via styling --}}
-            <div id="login_links">
+            <div id="auth_links">
                 {{-- TODO: link to actual pages --}}
                 <a href="/terms">{{ __('frame.footer.TERMS') }}</a>
                 <a href="/privacy">{{ __('frame.footer.PRIVACY') }}</a>
