ae2cef19079967b67c8835dfb45346b62bedb722
Monkatraz <34875062+Monkatraz@users.noreply.github.com>
2021-10-21 23:08:59 -0600

    redirect to homepage on login

--- web/resources/scripts/auth-register.ts
+++ web/resources/scripts/auth-register.ts
@@ -5,4 +5,9 @@ window.addEventListener("DOMContentLoaded", () => {
   const container = document.getElementById("auth_form_container")
   if (!container) throw new Error("No container found")
   const form = new RegisterForm({ target: container })
+
+  form.$on("register", () => {
+    // redirect to home
+    location.href = "/"
+  })
 })
