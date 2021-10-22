4a2eb25ecc91106512ae9d442ad111073197aa55
Monkatraz <34875062+Monkatraz@users.noreply.github.com>
2021-10-22 11:33:01 -0600

    make a base view for auth screens

--- web/resources/views/next/auth/register.blade.php
+++ web/resources/views/next/auth/register.blade.php
@@ -1,9 +1,9 @@
 {{--
     Register screen (non-API).
-    Extends from `next.base`.
+    Extends from `next.auth.auth`.
 --}}
 
-@extends('next.base', [
+@extends('next.auth.auth', [
     'title' => __('account_panel.REGISTER')
 ])
 
@@ -11,29 +11,11 @@
     @vite('auth-register.ts')
 @endpush
 
-@section('app')
-    <div id="app_auth">
-        <div id="auth_panel" class="light">
-            <a href="/" title="{{ __('frame.GOTO_HOME_PAGE') }}">
-                <img src="/files--static/media/logo.min.svg">
-            </a>
-            <hr>
+@section('content')
+<div id="auth_form_container">
+</div>
 
-            <div id="auth_form_container">
-            </div>
-
-            <a id="auth_login" href="/user--services/login">
-                {{ __("account_panel.LOGIN") }}
-            </a>
-
-            {{-- gets placed _outside_ of the panel via styling --}}
-            <div id="auth_links">
-                {{-- TODO: link to actual pages --}}
-                <a href="/terms">{{ __('frame.footer.TERMS') }}</a>
-                <a href="/privacy">{{ __('frame.footer.PRIVACY') }}</a>
-                <a href="/docs">{{ __('frame.footer.DOCS') }}</a>
-                <a href="/security">{{ __('frame.footer.SECURITY') }}</a>
-            </div>
-        </div>
-    </div>
+<a id="auth_login" href="/user--services/login">
+    {{ __("account_panel.LOGIN") }}
+</a>
 @endsection
