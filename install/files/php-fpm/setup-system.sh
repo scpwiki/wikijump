144c6c823a4c97fe26aa77b013796b57f627c9ec
Ammon Smith <ammon.i.smith@gmail.com>
2021-10-26 20:26:10 -0400

    Reinstitute apt dependency sort.

--- install/files/php-fpm/setup-system.sh
+++ install/files/php-fpm/setup-system.sh
@@ -3,12 +3,12 @@ set -eux
 
 apt update
 apt install -y \
+	git \
+	html2text \
+	imagemagick \
+	libfreetype6-dev \
 	libgd-dev \
 	libjpeg62-turbo-dev \
-	libfreetype6-dev \
-	imagemagick \
-	git \
-	zip \
-	html2text \
+	libtidy-dev \
 	postgresql-common \
-	libtidy-dev
+	zip
