b04c6fab809c6c9a36cbb1aa75213a6b0dc09cc7
Ammon Smith <ammon.i.smith@gmail.com>
2021-10-30 23:24:46 -0400

    Indent 2021_07_27_174756_tag_engine migration.

--- web/database/migrations/2021_07_27_174756_tag_engine.php
+++ web/database/migrations/2021_07_27_174756_tag_engine.php
@@ -14,21 +14,21 @@ class TagEngine extends Migration
     public function up()
     {
 
-      // Drop 'site_tag' table (entirely deprecated).
-      Schema::drop('site_tag');
+        // Drop 'site_tag' table (entirely deprecated).
+        Schema::drop('site_tag');
 
-      // Create 'tag_settings' table.
-      Schema::create('tag_settings', function (Blueprint $table) {
-        $table->id('configuration_id')->autoIncrement();
-        $table->unsignedInteger('site_id')->nullable()->index();
-        $table->string('configuration_name')->default('Unnamed Configuration');
-        $table->jsonb('allowed_tags')->default('[]');
-      });
+        // Create 'tag_settings' table.
+        Schema::create('tag_settings', function (Blueprint $table) {
+            $table->id('configuration_id')->autoIncrement();
+            $table->unsignedInteger('site_id')->nullable()->index();
+            $table->string('configuration_name')->default('Unnamed Configuration');
+            $table->jsonb('allowed_tags')->default('[]');
+        });
 
-      // Add column to 'site' that enables/disables the tag engine.
-      Schema::table('site', function (Blueprint $table) {
-        $table->boolean('enable_tag_engine')->default(false);
-      });
+        // Add column to 'site' that enables/disables the tag engine.
+        Schema::table('site', function (Blueprint $table) {
+            $table->boolean('enable_tag_engine')->default(false);
+        });
     }
 
     /**
@@ -39,20 +39,20 @@ class TagEngine extends Migration
     public function down()
     {
 
-      // Recreate 'site_tag' from legacy tables.
-      Schema::create('site_tag', function (Blueprint $table) {
-        $table->id('tag_id')->startingValue(2);
-        $table->unsignedInteger('site_id')->nullable();
-        $table->string('tag', 20)->nullable();
-        $table->unique(['site_id', 'tag']);
-      });
+        // Recreate 'site_tag' from legacy tables.
+        Schema::create('site_tag', function (Blueprint $table) {
+            $table->id('tag_id')->startingValue(2);
+            $table->unsignedInteger('site_id')->nullable();
+            $table->string('tag', 20)->nullable();
+            $table->unique(['site_id', 'tag']);
+        });
 
-      // Drop new 'tag_settings' table.
-      Schema::drop('tag_settings');
+        // Drop new 'tag_settings' table.
+        Schema::drop('tag_settings');
 
-      // Drop new 'enable_tag_engine' column in 'site' table.
-      Schema::table('site', function (Blueprint $table) {
-        $table->dropColumn('enable_tag_engine');
-      });
+        // Drop new 'enable_tag_engine' column in 'site' table.
+        Schema::table('site', function (Blueprint $table) {
+            $table->dropColumn('enable_tag_engine');
+        });
     }
 }
