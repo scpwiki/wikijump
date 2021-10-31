c3b61770f95ec9dad0de50721fb4ecc4769246d5
Ammon Smith <ammon.i.smith@gmail.com>
2021-10-30 23:42:29 -0400

    Create TagSettings model.

---
+++ web/app/Models/TagSettings.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+namespace Wikijump\Models;
+
+
+use Wikijump\Services\TagEnforcement\TagConfiguration;
+
+class TagSettings extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'site_id',
+        'configuration_name',
+        'configuration_data',
+    ];
+
+    /**
+     * Build a TagConfiguration corresponding to the data in this model.
+     */
+    public function getConfiguration(): TagConfiguration
+    {
+        return new TagConfiguration($this->configuration_data);
+    }
+}
