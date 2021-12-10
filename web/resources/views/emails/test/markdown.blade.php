7283891afbf8dc753281c19db85220005b3a3df9
Bri <34875062+Monkatraz@users.noreply.github.com>
2021-12-10 13:45:33 -0700

    add markdown test email

---
+++ web/resources/views/emails/test/markdown.blade.php
@@ -0,0 +1,10 @@
+{{--
+    It's unlikely that we'll use Markdown templates,
+    but this file is here to demonstrate using Markdown.
+--}}
+
+@component('mail::message')
+# Test Email
+
+This is a _test email_, using Markdown templates.
+@endcomponent
