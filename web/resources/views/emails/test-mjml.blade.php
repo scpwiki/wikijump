397f0c2e878e3a7248a2349a4db4b2888c65c061
Bri <34875062+Monkatraz@users.noreply.github.com>
2021-12-10 05:15:46 -0700

    add test mjml template

---
+++ web/resources/views/emails/test-mjml.blade.php
@@ -0,0 +1,11 @@
+@extends('emails.base-mjml')
+
+@section('content')
+    <mj-section background-color="#fff" padding-top="10px">
+        <mj-column>
+            <mj-text>
+                This is a test email.
+            </mj-text>
+        </mj-column>
+    </mj-section>
+@endsection
