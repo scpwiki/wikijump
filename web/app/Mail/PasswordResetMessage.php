2ccff95c7ee6d07a2df3f9e53706ac2710476609
Bri <34875062+Monkatraz@users.noreply.github.com>
2021-12-11 16:29:41 -0700

    add generic password reset message

---
+++ web/app/Mail/PasswordResetMessage.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Wikijump\Mail;
+
+/**
+ * Generic password reset message.
+ */
+class PasswordResetMessage extends MJMLMessage
+{
+    /**
+     * Constructs a new password reset message.
+     *
+     * @param string $url The URL to the password reset page.
+     * @param int $expires The number of minutes until the password reset expires.
+     */
+    public function __construct(string $url, int $expires)
+    {
+        $this->subject(__('email.reset_password.SUBJECT'))
+            ->greeting(__('email.reset_password.GREETING'))
+            ->line(__('email.reset_password.INTRO'))
+            ->action(__('email.reset_password.ACTION'), $url)
+            ->line(__('email.reset_password.EXPIRES', ['count' => $expires]))
+            ->line(__('email.reset_password.OUTRO'));
+    }
+}
