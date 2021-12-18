1fb2c60a71d245950a36b10690718bc474fb42fc
Bri <34875062+Monkatraz@users.noreply.github.com>
2021-12-18 13:59:48 -0700

    don't hardcode locales

--- web/modules/fluent/src/locales.ts
+++ web/modules/fluent/src/locales.ts
@@ -1,7 +1,6 @@
 import { FluentComponent } from "./component"
 
 export const FALLBACK_LOCALE = "en"
-export const SUPPORTED_LOCALES = ["en"] as const
 
 const filenameRegex = /[^\/]+\.ftl$/
 const componentRegex = /[^\/]+(?=\/[^\/]+\.ftl$)/
@@ -10,8 +9,6 @@ const localeRegex = /.+(?=\.ftl$)/
 // only import folders one level deep
 // TODO: do we need nested directories?
 const sources = import.meta.glob("/../locales/fluent/*/*.ftl")
-
-export type SupportedLocale = typeof FALLBACK_LOCALE | typeof SUPPORTED_LOCALES[number]
 
 export type FluentImportMap = Record<string, null | (() => Promise<string>)>
 
@@ -28,8 +25,6 @@ for (const [path, importer] of Object.entries(sources)) {
 
   if (!filename || !component || !locale) continue
 
-  if (!isSupportedLocale(locale)) continue
-
   // looks wacky but we're just getting the existing map,
   // and if there isn't one we make a new object for it and set it
   const map = directory[component] ?? (directory[component] = {})
@@ -43,14 +38,4 @@ export const LOCALE_COMPONENTS = new Map<string, FluentComponent>()
 // populate map with all of the components we found
 for (const [component, map] of Object.entries(directory)) {
   LOCALE_COMPONENTS.set(component, new FluentComponent(component, map))
-}
-
-/**
- * Checks if a locale is supported.
- *
- * @param locale - The locale string to check.
- */
-export function isSupportedLocale(locale: string): locale is SupportedLocale {
-  // TODO: more complicated parsing
-  return SUPPORTED_LOCALES.includes(locale as any)
 }
