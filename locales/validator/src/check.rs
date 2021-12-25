e7ce0da6d4b9cf221d7b1de87f7408e31aca30de
Ammon Smith <ammon.i.smith@gmail.com>
2021-12-24 22:21:10 -0800

    Start localization directory iteration.

--- locales/validator/src/check.rs
+++ locales/validator/src/check.rs
@@ -18,8 +18,9 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-use std::fs;
+use std::collections::HashMap;
 use std::path::Path;
+use std::{fs, process};
 use unic_langid::LanguageIdentifier;
 
 /// The "primary" locale, to compare other locales against.
@@ -34,6 +35,78 @@ const PRIMARY_LOCALE: LanguageIdentifier = langid!("en");
 
 pub fn run<P: AsRef<Path>>(directory: P) {
     let directory = directory.as_ref();
+    let mut return_code = 0;
 
-    todo!();
+    macro_rules! fail {
+        ($(arg:tt)*) => {{
+            return_code = 1;
+            eprint!("!! ");
+            eprintln!($(arg)*);
+        }};
+    }
+
+    let mut components = Vec::new();
+    let mut locales = HashMap::new();
+    print_real_path(directory);
+
+    // Walk through all the component directories
+    for entry in fs::read_dir(directory).expect("Unable to read localization directory") {
+        let path = entry.path();
+        if !path.is_dir() {
+            fail!("Found non-directory in localizations: {}", path.display());
+            continue;
+        }
+
+        // Walk through all the locales for a component
+        print_real_path(path);
+        for entry in fs::read_dir(path).expect("Unable to read component directory") {
+            let path = entry.path();
+            if !path.is_file() {
+                fail!("Found non-file in component directory: {}", path.display());
+                continue;
+            }
+
+            match path.extension() {
+                Some(ext) => {
+                    let ext = ext.to_str().expect("Path is not valid UTF-8");
+
+                    if !ext.eq_ignore_ascii_case("ftl") {
+                        fail!(
+                            "Found file with non-Fluent file extension: {} ({})",
+                            ext,
+                            path.display(),
+                        );
+                    }
+                }
+                None => {
+                    fail!("Found file with no extension: {}", path.display());
+                    continue;
+                }
+            }
+
+            let locale_name = path
+                .file_stem()
+                .expect("No base name in locale path")
+                .to_str()
+                .expect("Path is not valid UTF-8");
+
+            let locale: LanguageIdentifier = match locale_name.parse() {
+                Ok(locale) => locale,
+                Err(error) => {
+                    fail!("Directory name is not a valid locale: {}", locale_name);
+                    fail!("Error: {}", error);
+                    continue;
+                }
+            };
+
+            todo!();
+        }
+    }
+
+    process::exit(return_code);
+}
+
+fn print_real_path(path: &Path) {
+    let real_path = path.canonicalize().expect("Unable to canonicalize path");
+    println!("Reading through {}...", real_path.display());
 }
