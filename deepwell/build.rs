de43d6855371c02de5761adc35ef347b6ba6dd10
Ammon Smith <ammon.i.smith@gmail.com>
2021-12-26 04:10:05 -0800

    Add migration-trigger recompilation.

--- deepwell/build.rs
+++ deepwell/build.rs
@@ -1,3 +1,7 @@
 fn main() {
+    // Compile-time build information.
     built::write_built_file().expect("Failed to write build information");
+
+    // Trigger recompilation when a new migration is added.
+    println!("cargo:rerun-if-changed=migrations");
 }
