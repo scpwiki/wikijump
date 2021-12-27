cd960c6dc6f7e783d015f3a8a1e90ef419c8f921
Bri <34875062+Monkatraz@users.noreply.github.com>
2021-12-27 13:20:20 -0700

    add close menu to sidebar

--- web/resources/views/next/components/sidebar.blade.php
+++ web/resources/views/next/components/sidebar.blade.php
@@ -9,6 +9,18 @@
             aria-label="{{ __('sidebar') }}"
 >
     <div id="sidebar_sticky" role="presentation">
+        {{-- only shows up on small screens --}}
+        <div id="sidebar_close">
+            <wj-sidebar-close-button id="sidebar_close_button"
+                    type="button"
+                    aria-label="{{ __('close') }}"
+                    aria-controls="sidebar"
+            >
+                @include("next.components.sprite", [ "sprite" => "wj-close" ])
+            </wj-sidebar-close-button>
+        </div>
+
+
         @isset($navbar_items)
             @include("next.components.sidebar-nav-elements", [
                 'items' => $navbar_items,
