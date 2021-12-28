f68ede3dd2a875bdea94fab7859a72b7bc52605d
Ammon Smith <ammon.i.smith@gmail.com>
2021-12-28 00:06:11 -0800

    Add module docs for services/page/links.rs

--- deepwell/src/services/page/links.rs
+++ deepwell/src/services/page/links.rs
@@ -18,6 +18,23 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+//! This helper module updates the various kinds of backlinks supported in Wikijump.
+//!
+//! This includes "page connections" (a generic term for a relation between pages,
+//! such as includes, links, redirects, etc), which can either be present or missing,
+//! as well as external links, which are string URLs.
+//!
+//! Whenever a page is updated, a list of its backlinks is gathered by the parser,
+//! which is then presented here for processing. A diff is needed:
+//! * Any links no longer present are deleted.
+//! * Any links not previously present are inesrted.
+//! * Any links present but changed in quantity are updated.
+//! * Else, the link is up-to-date and left alone.
+//!
+//! While the logic here is similar for each case, the slight differences in keys,
+//! types, and tables make it hard to modularize. Instead, the logic is hopefully
+//! clear enough to be acceptable when repeated over a few slightly distinct cases.
+
 use super::super::prelude::*;
 use super::PageService;
 use crate::models::page_connection::{
