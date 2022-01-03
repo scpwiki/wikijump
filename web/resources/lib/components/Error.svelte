a92e8c3f5be493f04ff89fe1d2f9e5119b9dac8f
Bri <34875062+Monkatraz@users.noreply.github.com>
2022-01-03 09:24:57 -0700

    generic Error component

---
+++ web/resources/lib/components/Error.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  export let disabled = false
+</script>
+
+{#if !disabled}
+  <div class="errorbox">
+    <slot />
+  </div>
+{/if}
+
+<style global lang="scss">
+  .errorbox {
+    margin: 0.5rem 0;
+    font-size: 0.75rem;
+    color: var(--col-danger);
+    text-align: center;
+  }
+</style>
