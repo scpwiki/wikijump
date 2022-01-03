5f5c7ecec8b8d30ed9c87d505418a3526f7e327d
Bri <34875062+Monkatraz@users.noreply.github.com>
2022-01-03 06:51:09 -0700

    smart form wrapper

--- web/modules/dom/src/validation.ts
+++ web/modules/dom/src/validation.ts
@@ -4,14 +4,26 @@
  * @param inputs - The form inputs to check. Can accept `undefined` or
  *   `null` values, which will count as "invalid" inputs.
  */
-export function inputsValid(...inputs: (HTMLInputElement | undefined | null)[]) {
+export function inputsValid(
+  ...inputs: (
+    | HTMLInputElement
+    | HTMLSelectElement
+    | HTMLTextAreaElement
+    | HTMLButtonElement
+    | HTMLFieldSetElement
+    | undefined
+    | null
+  )[]
+) {
   for (const input of inputs) {
+    if (input instanceof HTMLFieldSetElement) continue
+    if (input instanceof HTMLButtonElement) continue
     if (
       !input ||
       !input.validity.valid ||
       input.value.length === 0 ||
       input.disabled ||
-      input.readOnly
+      (input as any).readOnly
     ) {
       return false
     }
