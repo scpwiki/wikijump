de26f56e7e68e6c61d6ff0e2550cf9fa64d06efd
Bri <34875062+Monkatraz@users.noreply.github.com>
2022-01-03 09:26:12 -0700

    generic dashboard panel header component

---
+++ web/resources/lib/components/dashboard/DashboardPanelHeader.svelte
@@ -0,0 +1,13 @@
+<div class="dashboard-panel-header">
+  <h1><slot /></h1>
+</div>
+
+<style global lang="scss">
+  .dashboard-panel-header {
+    display: flex;
+    align-items: center;
+    padding-bottom: 0.5rem;
+    margin-bottom: 0.5rem;
+    border-bottom: solid 0.125rem var(--col-border);
+  }
+</style>
