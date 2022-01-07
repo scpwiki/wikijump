279fc1e9584a2b54778e6b492e9dc0a4a3e0c375
Ammon Smith <ammon.i.smith@gmail.com>
2022-01-07 00:05:16 -0500

    Move database initialization to setup script.

--- install/files/postgres/setup.sh
+++ install/files/postgres/setup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -eux
 
+# Initialize database
+su postgres -c "initdb -D ${PGDATA} -A trust --locale=en_US.UTF-8"
+install -D -m644 -o postgres -g postgres /src/pg_hba.conf "${PGDATA}/pg_hba.conf"
+
 # Start database
 su postgres -c "pg_ctl -D ${PGDATA} start"
 
