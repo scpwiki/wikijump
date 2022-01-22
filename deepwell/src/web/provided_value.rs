bac4eb340d118b36550289cfeaac675d0a873a4d
Ammon Smith <ammon.i.smith@gmail.com>
2022-01-22 07:36:16 -0500

    Add Option methods to ProvidedValue.

--- deepwell/src/web/provided_value.rs
+++ deepwell/src/web/provided_value.rs
@@ -34,6 +34,25 @@ pub enum ProvidedValue<T> {
     Unset,
 }
 
+impl<T> Option<T> {
+    pub fn expect(self, message: &str) -> T {
+        match self {
+            ProvidedValue::Set(value) => value,
+            ProvidedValue::Unset => panic!("{}", message),
+        }
+    }
+}
+
+impl<T> From<ProvidedValue<T>> for Option<T> {
+    #[inline]
+    fn from(value: ProvidedValue<T>) -> Option<T> {
+        match value {
+            ProvidedValue::Set(value) => Some(value),
+            ProvidedValue::Unset => None,
+        }
+    }
+}
+
 impl<T> Default for ProvidedValue<T> {
     #[inline]
     fn default() -> Self {
