2c1a9e8d5866ed8eaf1ce080ccc7eaeac9e25d75
Ammon Smith <ammon.i.smith@gmail.com>
2022-01-25 02:21:02 -0500

    Increase the inter-job delay.
    Since we don't have a real queue, we want to wait a bit to avoid
    thrashing the database and spamming logs.

--- deepwell/src/services/job/service.rs
+++ deepwell/src/services/job/service.rs
@@ -114,7 +114,7 @@ struct JobWorker(ApiServerState);
 impl JobWorker {
     // Main worker functions
     pub async fn main_loop(state: ApiServerState) -> Void {
-        const JOB_DELAY: Duration = Duration::from_millis(10);
+        const JOB_DELAY: Duration = Duration::from_secs(10);
         tide::log::info!("Launching job worker");
 
         loop {
