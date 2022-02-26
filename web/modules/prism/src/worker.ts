bd10ccd0df4403379f946550ab70348742d04fba
Bri <34875062+Monkatraz@users.noreply.github.com>
2022-02-25 17:53:14 -0700

    move functions into module

--- web/modules/prism/src/worker.ts
+++ web/modules/prism/src/worker.ts
@@ -22,38 +22,49 @@ const encode: (src: string) => string = Prism.util.encode as any
 
 const RAW_LANGS = ["raw", "text", "none", ""]
 
-/** Returns the list of languages (and their aliases) supported by Prism. */
-export function getLanguages() {
-  return Object.keys(Prism.languages).filter(
-    lang => typeof Prism.languages[lang] !== "function"
-  )
+const module = {
+  /** Returns the list of languages (and their aliases) supported by Prism. */
+  getLanguages() {
+    return Object.keys(Prism.languages).filter(
+      lang => typeof Prism.languages[lang] !== "function"
+    )
+  },
+  /**
+   * Highlights a string of code and returns HTML, given a specified
+   * language. If the language specified isn't known by Prism, the string
+   * of code will be escaped and returned with no syntax highlighting.
+   *
+   * If the given language is `raw`, `text`, `none`, or an empty string,
+   * the string of code will be escaped and returned as is.
+   *
+   * @param code - The string to be highlighted.
+   * @param lang - The language to highlight the code with.
+   */
+  highlight(code: string, lang: string) {
+    try {
+      if (lang && !RAW_LANGS.includes(lang) && lang in Prism.languages) {
+        const grammar = Prism.languages[lang]
+        const html = Prism.highlight(code, grammar, lang)
+        return html
+      }
+    } catch {}
+
+    // fallback to just returning the escaped code
+    return encode(code)
+  },
+
+  /** Sets or gets the status of Prism's `manual` property. */
+  manual(set?: boolean) {
+    if (typeof set !== "undefined") Prism.manual = set
+    return Prism.manual
+  },
+
+  /** Sets or gets the status of Prism's `disableWorkerMessageHandler` property. */
+  disableWorkerMessageHandler(set?: boolean) {
+    if (typeof set !== "undefined") Prism.disableWorkerMessageHandler = set
+    return Prism.disableWorkerMessageHandler ?? false
+  }
 }
-
-/**
- * Highlights a string of code and returns HTML, given a specified
- * language. If the language specified isn't known by Prism, the string of
- * code will be escaped and returned with no syntax highlighting.
- *
- * If the given language is `raw`, `text`, `none`, or an empty string, the
- * string of code will be escaped and returned as is.
- *
- * @param code - The string to be highlighted.
- * @param lang - The language to highlight the code with.
- */
-export function highlight(code: string, lang: string) {
-  try {
-    if (lang && !RAW_LANGS.includes(lang) && lang in Prism.languages) {
-      const grammar = Prism.languages[lang]
-      const html = Prism.highlight(code, grammar, lang)
-      return html
-    }
-  } catch {}
-
-  // fallback to just returning the escaped code
-  return encode(code)
-}
-
-const module = { getLanguages, highlight }
 
 export type PrismModule = typeof module
 
