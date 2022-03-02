ff0a641cbe6ddbea69992a80f2446a6e7e498679
Bri <34875062+Monkatraz@users.noreply.github.com>
2022-03-02 11:07:40 -0700

    fix svelte css emit

--- web/svelte.config.cjs
+++ web/svelte.config.cjs
@@ -1,21 +1,23 @@
 const path = require("path")
-
-/** @type import("svelte-preprocess").default */
-const preprocess = require("svelte-preprocess")
+const mergeQueries = require("postcss-merge-queries")
+const autoprefixer = require("autoprefixer")
+const { globalStyle, postcss, scss, typescript } = require("svelte-preprocess")
 
 const abstracts = path.resolve(__dirname, "resources/css/abstracts.scss")
 
 module.exports = {
   preprocess: [
-    preprocess({
-      sourceMap: true,
-      scss: {
-        // faster for Dart Sass, which we're using
-        renderSync: true,
-        // automatically imports the abstracts file (mixins, variables, etc)
-        prependData: `@import "${abstracts}";\n`
-      }
-    })
+    scss({
+      // faster for Dart Sass, which we're using
+      renderSync: true,
+      // automatically imports the abstracts file (mixins, variables, etc)
+      prependData: `@import "${abstracts}";\n`
+    }),
+    postcss({
+      plugins: [autoprefixer(), mergeQueries({ sort: true })]
+    }),
+    globalStyle(),
+    typescript()
   ],
   /** @type import("svelte/types/compiler/interfaces").CompileOptions */
   compilerOptions: {
