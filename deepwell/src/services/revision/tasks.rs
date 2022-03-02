aad696a283997dc82c18f1b796f709e69fd372ff
Ammon Smith <ammon.i.smith@gmail.com>
2022-03-02 04:08:47 -0500

    Check old and new metadata values.

--- deepwell/src/services/revision/tasks.rs
+++ deepwell/src/services/revision/tasks.rs
@@ -80,9 +80,9 @@ impl RevisionTasks {
             }
         }
 
-        if let ProvidedValue::Set(ref _metadata) = changes.metadata {
-            // TODO check metadata
-            if false {
+        if let ProvidedValue::Set(ref metadata) = changes.metadata {
+            if &revision.metadata != metadata {
+                // TODO
                 tasks.render_and_update_links = true;
             }
         }
