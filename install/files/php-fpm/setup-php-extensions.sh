4f25b6813478b0db2aea726d83674fc5094bff78
Ammon Smith <ammon.i.smith@gmail.com>
2022-03-07 20:30:16 -0500

    Don't enable PHP-FFI in containers.

--- install/files/php-fpm/setup-php-extensions.sh
+++ install/files/php-fpm/setup-php-extensions.sh
@@ -4,11 +4,6 @@ set -eux
 # Setup extensions
 /src/setup-memcached.sh
 
-# Configure PHP-FFI
-apt install -y libffi-dev
-docker-php-ext-configure ffi --with-ffi
-docker-php-ext-install ffi
-
 # Configure PHP Intl
 apt install -y libicu-dev
 docker-php-ext-configure intl
