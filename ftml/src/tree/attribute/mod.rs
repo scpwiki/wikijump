1dfc26dbe08b75b916af19a2d6f81322d316a5c8
Ammon Smith <ammon.i.smith@gmail.com>
2022-04-19 15:53:59 -0400

    Add method to isolate IDs in AttributeMap.

--- ftml/src/tree/attribute/mod.rs
+++ ftml/src/tree/attribute/mod.rs
@@ -21,6 +21,7 @@
 mod safe;
 
 use super::clone::string_to_owned;
+use crate::id_prefix::isolate_ids;
 use crate::parsing::parse_boolean;
 use std::borrow::Cow;
 use std::collections::{BTreeMap, HashMap};
@@ -89,6 +90,13 @@ impl<'t> AttributeMap<'t> {
     #[inline]
     pub fn get(&self) -> &BTreeMap<Cow<'t, str>, Cow<'t, str>> {
         &self.inner
+    }
+
+    pub fn isolate_id(&mut self) {
+        if let Some(value) = self.inner.get_mut("id") {
+            debug!("Found 'id' attribute, isolating value");
+            *value = Cow::Owned(isolate_ids(&value));
+        }
     }
 
     pub fn to_owned(&self) -> AttributeMap<'static> {
