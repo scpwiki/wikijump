39c23c211e30962c17e47160704d68c679a40501
Ammon Smith <ammon.i.smith@gmail.com>
2022-04-30 01:29:36 -0400

    Expand create test.

--- deepwell/src/test/page.rs
+++ deepwell/src/test/page.rs
@@ -19,6 +19,8 @@
  */
 
 use super::prelude::*;
+use crate::models::sea_orm_active_enums::RevisionType;
+use crate::services::page::{CreatePageOutput, GetPageOutput};
 
 #[async_std::test]
 async fn exists() -> Result<()> {
@@ -53,29 +55,56 @@ async fn create() -> Result<()> {
         let env = TestEnvironment::setup().await?;
         let slug = env.random_slug();
 
-        #[derive(Deserialize, Debug)]
-        #[serde(rename_all = "camelCase")]
-        struct CreatePageOutput {
-            slug: String,
-            parser_warnings: Vec<JsonValue>,
-        }
-
-        let (output, status): (CreatePageOutput, _) = env
+        // Create page
+        let (output, status) = env
             .post(format!("/page/{WWW_SITE_ID}"))?
             .body_json(json!({
                 "wikitext": "Page contents",
                 "title": "Test page!",
                 "altTitle": null,
                 "slug": slug,
-                "revisionComments": "Create page",
+                "revisionComments": "Create page test",
                 "userId": ADMIN_USER_ID,
             }))?
-            .recv_json_serde()
+            .recv_json_serde::<CreatePageOutput>()
             .await?;
 
+        let page_id = output.page_id;
+        let revision_id = output.revision_id;
         assert_eq!(status, StatusCode::Ok);
         assert_eq!(output.slug, slug);
         assert!(output.parser_warnings.is_empty());
+
+        // Check presence
+        let status = env
+            .head(format!("/page/{WWW_SITE_ID}/slug/{}", slug))?
+            .recv()
+            .await?;
+
+        assert_eq!(status, StatusCode::NoContent);
+
+        // Get page
+        let (output, status) = env
+            .get(format!("/page/{WWW_SITE_ID}/slug/{}", slug))?
+            .recv_json_serde::<GetPageOutput>()
+            .await?;
+
+        assert_eq!(status, StatusCode::Ok);
+        assert_eq!(output.page_id, page_id);
+        assert!(output.page_updated_at.is_none());
+        assert!(output.page_deleted_at.is_none());
+        assert_eq!(output.page_revision_count, 1);
+        assert_eq!(output.site_id, WWW_SITE_ID);
+        assert_eq!(output.page_category_slug, "_default");
+        assert!(output.discussion_thread_id.is_none());
+        assert_eq!(output.revision_id, revision_id);
+        assert_eq!(output.revision_type, RevisionType::Create);
+        assert_eq!(output.revision_number, 0);
+        assert_eq!(output.revision_user_id, ADMIN_USER_ID);
+        assert_eq!(output.revision_comments, "Create page test");
+        assert_eq!(output.title, "Test page!");
+        assert!(output.alt_title.is_none());
+        assert_eq!(output.slug, slug);
 
         Ok(())
     }}
