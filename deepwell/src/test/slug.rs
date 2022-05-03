d59456fe4f86edc15f88655592863edcf76710de
Ammon Smith <ammon.i.smith@gmail.com>
2022-05-03 02:28:21 -0400

    Call helper function instead of macro.
    Hopefully this helps with the stack overflow
    issue by forcibly freeing all local stack items
    after each test.

--- deepwell/src/test/slug.rs
+++ deepwell/src/test/slug.rs
@@ -21,57 +21,66 @@
 use super::prelude::*;
 use crate::services::page::GetPageOutput;
 
-macro_rules! create_page {
-    ($runner:expr, $create_slug:expr, $expected_slug:expr $(,)?) => {{
-        let GeneratedPage { page_id, .. } = $runner.page2(
+async fn create_page(
+    runner: &Runner,
+    create_slug: &str,
+    expected_slug: &str,
+) -> Result<()> {
+    let GeneratedPage { page_id, .. } = runner
+        .page2(
             Some(WWW_SITE_ID),
             Some(ANONYMOUS_USER_ID),
-            Some(str!($create_slug)),
-        ).await?;
+            Some(str!(create_slug)),
+        )
+        .await?;
 
-        // Get page data
-        let (output, status) = $runner
-            .get(format!("/page/{WWW_SITE_ID}/id/{page_id}"))?
-            .recv_json::<GetPageOutput>()
-            .await?;
+    // Get page data
+    let (output, status) = runner
+        .get(format!("/page/{WWW_SITE_ID}/id/{page_id}"))?
+        .recv_json::<GetPageOutput>()
+        .await?;
 
-        // Ensure request worked
-        assert_eq!(status, StatusCode::Ok);
-        assert_eq!(output.page_id, page_id);
+    // Ensure request worked
+    assert_eq!(status, StatusCode::Ok);
+    assert_eq!(output.page_id, page_id);
 
-        // Actual assertion for this test
-        assert_eq!(
-            output.slug,
-            $expected_slug,
-            "Actual created page slug doesn't match expected",
-        );
+    // Actual assertion for this test
+    assert_eq!(
+        output.slug, expected_slug,
+        "Actual created page slug doesn't match expected",
+    );
 
-        // Delete afterwards so the test is idempotent on the same database.
-        // Since tests still work on the main site and don't create a dummy one.
+    // Delete afterwards so the test is idempotent on the same database.
+    // Since tests still work on the main site and don't create a dummy one.
 
-        let status = $runner
-            .delete(format!("/page/{WWW_SITE_ID}/id/{page_id}"))?
-            .body_json(json!({
-                "revisionComments": "Delete slug test page",
-                "userId": AUTOMATIC_USER_ID,
-            }))?
-            .recv()
-            .await?;
+    let status = runner
+        .delete(format!("/page/{WWW_SITE_ID}/id/{page_id}"))?
+        .body_json(json!({
+            "revisionComments": "Delete slug test page",
+            "userId": AUTOMATIC_USER_ID,
+        }))?
+        .recv()
+        .await?;
 
-        assert_eq!(status, StatusCode::Ok);
-    }};
+    assert_eq!(status, StatusCode::Ok);
+    Ok(())
 }
 
 #[async_test]
 async fn categories() -> Result<()> {
     let runner = Runner::setup().await?;
 
-    create_page!(runner, "apple", "apple");
-    create_page!(runner, "_default:apple", "apple");
-    create_page!(runner, "category:apple", "category:apple");
-    create_page!(runner, "category-with-dashes:apple", "category-with-dashes:apple");
-    create_page!(runner, "_template", "_template");
-    create_page!(runner, "_default:_template", "_template");
+    create_page(&runner, "apple", "apple").await?;
+    create_page(&runner, "_default:apple", "apple").await?;
+    create_page(&runner, "category:apple", "category:apple").await?;
+    create_page(
+        &runner,
+        "category-with-dashes:apple",
+        "category-with-dashes:apple",
+    )
+    .await?;
+    create_page(&runner, "_template", "_template").await?;
+    create_page(&runner, "_default:_template", "_template").await?;
 
     Ok(())
 }
@@ -80,49 +89,49 @@ async fn categories() -> Result<()> {
 async fn normalization() -> Result<()> {
     let runner = Runner::setup().await?;
 
-    create_page!(runner, "Apple", "apple");
-    create_page!(runner, "AppleBanana", "applebanana");
-    create_page!(runner, "Apple Banana", "apple-banana");
-    create_page!(runner, " apple ", "apple");
-    create_page!(runner, " APPLE ", "apple");
-    create_page!(runner, "-apple-", "apple");
-    create_page!(runner, "-apple", "apple");
-    create_page!(runner, "apple-", "apple");
-    create_page!(runner, "apple-banana", "apple-banana");
-    create_page!(runner, "apple--banana", "apple-banana");
-    create_page!(runner, "apple---banana", "apple-banana");
-    create_page!(runner, "__template", "_template");
-    create_page!(runner, "_template_", "_template");
-    create_page!(runner, "template_", "template");
-    create_page!(runner, "Tufto's Proposal", "tufto-s-proposal");
-    create_page!(runner, "SCP-001", "scp-001");
+    create_page(&runner, "Apple", "apple").await?;
+    create_page(&runner, "AppleBanana", "applebanana").await?;
+    create_page(&runner, "Apple Banana", "apple-banana").await?;
+    create_page(&runner, " apple ", "apple").await?;
+    create_page(&runner, " APPLE ", "apple").await?;
+    create_page(&runner, "-apple-", "apple").await?;
+    create_page(&runner, "-apple", "apple").await?;
+    create_page(&runner, "apple-", "apple").await?;
+    create_page(&runner, "apple-banana", "apple-banana").await?;
+    create_page(&runner, "apple--banana", "apple-banana").await?;
+    create_page(&runner, "apple---banana", "apple-banana").await?;
+    create_page(&runner, "__template", "_template").await?;
+    create_page(&runner, "_template_", "_template").await?;
+    create_page(&runner, "template_", "template").await?;
+    create_page(&runner, "Tufto's Proposal", "tufto-s-proposal").await?;
+    create_page(&runner, "SCP-001", "scp-001").await?;
 
-    create_page!(runner, "_default:Apple", "apple");
-    create_page!(runner, "_default:APPLE", "apple");
-    create_page!(runner, "_default::Apple", "apple");
-    create_page!(runner, "_default::APPLE", "apple");
-    create_page!(runner, "category:Apple", "category:apple");
-    create_page!(runner, "category:APPLE", "category:apple");
-    create_page!(runner, "category::apple", "category:apple");
-    create_page!(runner, "category:::apple", "category:apple");
-    create_page!(runner, "category-:-apple", "category:apple");
-    create_page!(runner, "category:-apple", "category:apple");
-    create_page!(runner, "category-:apple", "category:apple");
+    create_page(&runner, "_default:Apple", "apple").await?;
+    create_page(&runner, "_default:APPLE", "apple").await?;
+    create_page(&runner, "_default::Apple", "apple").await?;
+    create_page(&runner, "_default::APPLE", "apple").await?;
+    create_page(&runner, "category:Apple", "category:apple").await?;
+    create_page(&runner, "category:APPLE", "category:apple").await?;
+    create_page(&runner, "category::apple", "category:apple").await?;
+    create_page(&runner, "category:::apple", "category:apple").await?;
+    create_page(&runner, "category-:-apple", "category:apple").await?;
+    create_page(&runner, "category:-apple", "category:apple").await?;
+    create_page(&runner, "category-:apple", "category:apple").await?;
 
-    create_page!(runner, "category:xyz:apple", "category:xyz:apple");
-    create_page!(runner, "category::xyz::apple", "category:xyz:apple");
-    create_page!(runner, "category::xyz:apple", "category:xyz:apple");
-    create_page!(runner, "category:xyz::apple", "category:xyz:apple");
+    create_page(&runner, "category:xyz:apple", "category:xyz:apple").await?;
+    create_page(&runner, "category::xyz::apple", "category:xyz:apple").await?;
+    create_page(&runner, "category::xyz:apple", "category:xyz:apple").await?;
+    create_page(&runner, "category:xyz::apple", "category:xyz:apple").await?;
 
-    create_page!(runner, ":apple:", "apple");
-    create_page!(runner, "apple:", "apple");
-    create_page!(runner, ":apple", "apple");
-    create_page!(runner, "_default::apple:", "apple");
-    create_page!(runner, "_default:apple:", "apple");
-    create_page!(runner, "_default::apple", "apple");
-    create_page!(runner, "category::apple:", "category:apple");
-    create_page!(runner, "category:apple:", "category:apple");
-    create_page!(runner, "category::apple", "category:apple");
+    create_page(&runner, ":apple:", "apple").await?;
+    create_page(&runner, "apple:", "apple").await?;
+    create_page(&runner, ":apple", "apple").await?;
+    create_page(&runner, "_default::apple:", "apple").await?;
+    create_page(&runner, "_default:apple:", "apple").await?;
+    create_page(&runner, "_default::apple", "apple").await?;
+    create_page(&runner, "category::apple:", "category:apple").await?;
+    create_page(&runner, "category:apple:", "category:apple").await?;
+    create_page(&runner, "category::apple", "category:apple").await?;
 
     Ok(())
 }
