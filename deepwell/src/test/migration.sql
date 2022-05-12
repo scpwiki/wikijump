838354fb6bc859d0832cb721a57dc91bc6bb5e11
Ammon Smith <ammon.i.smith@gmail.com>
2022-05-12 08:47:45 -0400

    Create initial migration file for testing.

---
+++ deepwell/src/test/migration.sql
@@ -0,0 +1,7 @@
+-- Temporary ad hoc migration to add what we need for tests to take place.
+-- This will be removed when we are using native DEEPWELL migrations
+-- and can just run it as part of preparation then.
+--
+-- See https://scuttle.atlassian.net/browse/WJ-1026
+
+SELECT 1;
