8074dbef05a4fd36383eafc35f949169f8711bbe
Ammon Smith <ammon.i.smith@gmail.com>
2022-05-29 10:05:36 -0400

    Create stub file_revision service directory.

---
+++ deepwell/src/services/file_revision/mod.rs
@@ -0,0 +1,30 @@
+/*
+ * services/file_revision/mod.rs
+ *
+ * DEEPWELL - Wikijump API provider and database manager
+ * Copyright (C) 2019-2022 Wikijump Team
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+mod prelude {
+    pub use super::super::prelude::*;
+    pub use super::structs::*;
+}
+
+mod service;
+mod structs;
+
+pub use self::service::FileRevisionService;
+pub use self::structs::*;
