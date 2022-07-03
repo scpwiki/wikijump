77af36aab740e528e3e095b35af94ea5a74186eb
Ammon Smith <ammon.i.smith@gmail.com>
2022-07-03 06:21:35 -0400

    Create WikicommaImporter class.

--- deepwell/scripts/wikicomma_import.py
+++ deepwell/scripts/wikicomma_import.py
@@ -1,13 +1,92 @@
 #!/usr/bin/env python3
 
 import argparse
+import os
+
+import psycopg2
+
+
+class WikicommaImporter:
+    __slots__ = (
+        "output_file",
+        "database_url",
+        "wikicomma_directory",
+        "_file",
+        "_conn",
+        "_cur",
+    )
+
+    def __init__(self, args):
+        self.output_file = args.output_file
+        self.database_url = args.database_url
+        self.wikicomma_directory = os.path.normpath(args.wikicomma_directory)
+        self._clean()
+
+    def run(self):
+        print("Preparing...")
+
+        with open(self.output_file, "w") as self._file:
+            with psycopg2.connect(self.database_url) as self._conn:
+                with conn.cursor() as self._cur:
+                    self.pull_all()
+
+        self._clean()
+        print(f"Finished. Wrote SQL query to {self.output_file}")
+
+    def pull_all(self):
+        for site in os.listdir(self.wikicomma_directory):
+            self.pull_site(site)
+
+    def pull_site(self, site):
+        print(f"+ Pulling site {site}")
+        site_directory = os.path.join(self.wikicomma_directory, site)
+
+        self.pull_site_pages(site_directory)
+        self.pull_site_forum(site_directory)
+        self.pull_site_files(site_directory)
+
+    def pull_site_pages(self, site_directory):
+        print(f"++ Writing pages")
+        self.append_sql_section("Pages")
+
+        # TODO
+
+    def pull_site_forum(self, site_directory):
+        print(f"++ Writing forum posts")
+        self.append_sql_section("Forum")
+
+        # TODO
+        print("++ TODO")
+
+    def pull_site_files(self, site_directory):
+        print(f"++ Writing files")
+        self.append_sql_section("Files")
+
+        # TODO
+
+    def _clean(self):
+        self._file = None
+        self._file = None
+        self._cur = None
+
+    def format_sql(self, query, parameters=()):
+        return self._cur.mogrify(query, parameters)
+
+    def append_sql(self, query, parameters=()):
+        sql_line = self.format_sql(query, parameters)
+        self._file.writelines([sql_line, ";\n"])
+
+    def append_sql_section(self, name):
+        self._file.writelines(["\n\n--\n-- ", name, "\n--\n\n"])
+
 
 if __name__ == "__main__":
+    # Parse arguments
     argparser = argparse.ArgumentParser(description="WikiComma import utility")
     argparser.add_argument(
         "-o",
         "--output",
-        dest="output",
+        dest="output_file",
         default="wikicomma_ingest.sql",
         help="The path to write the output SQL file to.",
     )
@@ -27,4 +106,5 @@ if __name__ == "__main__":
     )
     args = argparser.parse_args()
 
-    print(args)
+    # Run importer
+    WikicommaImporter(args).run()
