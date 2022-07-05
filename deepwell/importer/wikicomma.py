372d8f6528b25864938f903846a6d20e897774b2
Ammon Smith <ammon.i.smith@gmail.com>
2022-07-05 06:02:56 -0400

    Start WikicommaImporter, add pages.

--- deepwell/importer/wikicomma.py
+++ deepwell/importer/wikicomma.py
@@ -1,9 +1,10 @@
-import argparse
-import hashlib
 import json
 import os
-from collections import namedtuple
 from datetime import datetime
+
+from .constants import UNKNOWN_CREATION_DATE
+from .generator import generate_seed
+from .structures import *
 
 import py7zr
 from bidict import bidict
@@ -11,52 +12,136 @@ from bidict import bidict
 REPLACE_COLON = True
 ANONYMOUS_USER_ID = 3
 
-Site = namedtuple("Site", ("slug", "wikijump_id", "directory"))
-Page = namedtuple("Page", ("slug", "wikidot_id"))
-
 
 class WikicommaImporter:
     __slots__ = (
-        "output_file",
-        "database_url",
-        "wikicomma_directory",
-        "text_hashes",
-        "pages",
-        "page_categories",
-        "last_site_id",
-        "last_category_id",
-        "_file",
-        "_conn",
-        "_cur",
+        "generator",
+        "directory",
     )
 
-    def __init__(self, args):
-        self.output_file = args.output_file
-        self.database_url = args.database_url
-        self.wikicomma_directory = os.path.normpath(args.wikicomma_directory)
+    def __init__(self, generator, directory):
+        self.generator = generator
+        self.directory = directory
 
-        self.text_hashes = set()
-        self.pages = bidict()
-        self.page_categories = {}
-        self.last_site_id = args.start_site_id
-        self.last_category_id = args.start_category_id
+    def process_all(self):
+        self.generator.section_sql("Wikicomma")
+        self.generator.section_sh("Files")
 
-        self._clean()
+        for site_slug in os.listdir(self.directory):
+            self.process_site(site_slug)
 
-    def run(self):
-        print("Preparing...")
+    def process_site(self, site_slug):
+        self.generator.section_sql(f"Site: {site_slug}")
 
-        with open(self.output_file, "w") as self._file:
-            with psycopg2.connect(self.database_url) as self._conn:
-                with self._conn.cursor() as self._cur:
-                    self.add_all()
+        # Add site
+        unknown_description = f"[NEEDS UPDATE] {site_slug}"
+        site = Site(
+            wikidot_id=None,
+            created_at=UNKNOWN_CREATION_DATE,
+            name=unknown_description,
+            slug=site_slug,
+            subtitle=unknown_description,
+            description=unknown_description,
+        )
+        self.generator.add_site(site)
 
-        self._clean()
-        print(f"Finished. Wrote SQL query to {self.output_file}")
+        # Process site internals
+        site_directory = os.path.join(self.directory, site_slug)
+        self.process_site_pages(site_slug, site_directory)
+        self.process_site_forum(site_slug, site_directory)
+        self.process_site_files(site_slug, site_directory)
 
-    def add_all(self):
-        for site_slug in os.listdir(self.wikicomma_directory):
-            self.add_site(site_slug)
+    def process_site_pages(self, site_slug: str, site_directory: str):
+        self.generator.section_sql(f"Pages: {site_slug}")
+        mapping = self.read_json(site_directory, "meta", "page_id_map.json")
+
+        for page_id, page_slug in mapping.items():
+            metadata = self.read_page_metadata(site_directory, page_slug)
+            created_at = datetime.fromtimestamp(metadata.revisions[-1]["stamp"])
+            updated_at = datetime.fromtimestamp(metadata.revisions[0]["stamp"])
+
+            page = Page(
+                wikidot_id=page_id,
+                created_at=created_at,
+                updated_at=updated_at,
+                site_id=-1, # TODO unknown
+                title=metadata["title"],
+                slug=page_slug,
+                discussion_thread_id=None,  # TODO unknown
+            )
+            self.generator.add_page(page)
+
+    def process_site_forum(self, site_slug: str, site_directory: str):
+        self.generator.section_sql(f"Forum: {site_slug}")
+        # TODO
+
+    def process_site_files(self, site_slug: str, site_directory: str):
+        self.generator.section_sql(f"Files: {site_slug}")
+        # TODO
+
+    def _add_page_votes(self, site, page, metadata):
+        for (user_spec, value) in metadata["votings"]:
+            user_id = self.get_user_id(user_spec)
+
+            if isinstance(value, bool):
+                value = +1 if value else -1
+
+            self.append_sql(
+                "INSERT INTO page_vote (created_at, page_id, user_id, value)",
+                (UNKNOWN_CREATION_DATE, page.wikidot_id, user_id, value),
+            )
+        # lock: metadata["is_locked"]:
+
+        def read_page_metadata(self, site_directory: str, page_slug: str):
+            page_metadata_filename = f"{page_slug}.json"
+
+            if REPLACE_COLON:
+                page_metadata_filename = page_metadata_filename.replace(":", "_")
+
+            page_metadata = self.read_json(
+                site_directory,
+                "meta",
+                "pages",
+                page_metadata_filename,
+            )
+
+            assert page_metadata["name"] == page_slug
+            return page_metadata
+
+    @staticmethod
+    def read_json(*path_parts):
+        path = os.path.join(*path_parts)
+
+        with open(path) as file:
+            return json.load(file)
+
+
+def run_wikicomma_import(
+    *,
+    wikicomma_directory: str,
+    sql_path: str,
+    sh_path: str,
+    s3_bucket: str,
+    postgres_url: str,
+    last_page_category_id: int = 0,
+):
+    wikicomma_directory = os.path.normpath(wikicomma_directory)
+
+    def runner(generator):
+        importer = WikicommaImporter(generator, wikicomma_directory)
+        importer.process_all()
+
+    generate_seed(
+        runner,
+        sql_path=sql_path,
+        sh_path=sh_path,
+        s3_bucket=s3_bucket,
+        postgres_url=postgres_url,
+        last_page_category_id=last_page_category_id,
+    )
+
+
+# XXX
 
     def add_site_pages(self, site):
         print(f"++ Writing pages")
@@ -125,31 +210,6 @@ class WikicommaImporter:
                 ),
             )
 
-    def add_page_votes(self, site, page, metadata):
-        for (user_spec, value) in metadata["votings"]:
-            user_id = self.get_user_id(user_spec)
-
-            if isinstance(value, bool):
-                value = +1 if value else -1
-
-            self.append_sql(
-                "INSERT INTO page_vote (created_at, page_id, user_id, value)",
-                (UNKNOWN_CREATION_DATE, page.wikidot_id, user_id, value),
-            )
-
-    def add_page_lock(self, site, page, metadata):
-        if metadata["is_locked"]:
-            self.append_sql(
-                "INSERT INTO page_lock (created_at, lock_type, page_id, user_id, reason) VALUES (%s, %s, %s, %s, %s)",
-                (
-                    UNKNOWN_CREATION_DATE,
-                    "wikidot",
-                    page.wikidot_id,
-                    ANONYMOUS_USER_ID,
-                    "Lock imported from Wikidot",
-                ),
-            )
-
     def add_site_forum(self, site):
         print(f"++ Writing forum posts")
         self.append_sql_section("Forum")
@@ -171,39 +231,3 @@ class WikicommaImporter:
             # file_data['url']
             # file_data['path']
             pass
-
-    def _clean(self):
-        self._file = None
-        self._file = None
-        self._cur = None
-
-    def add_page_category(self, page_slug):
-        category_slug = self.get_page_category(page_slug)
-
-        if category_slug not in self.page_categories:
-            self.page_categories[category_slug] = self.next_category_id()
-
-        return self.page_categories[category_slug]
-
-    def read_page_metadata(self, site, page_slug):
-        page_metadata_filename = f"{page_slug}.json"
-
-        if REPLACE_COLON:
-            page_metadata_filename = page_metadata_filename.replace(":", "_")
-
-        page_metadata = self.read_json(
-            site.directory,
-            "meta",
-            "pages",
-            page_metadata_filename,
-        )
-
-        assert page_metadata["name"] == page_slug
-        return page_metadata
-
-    @staticmethod
-    def read_json(*path_parts):
-        path = os.path.join(*path_parts)
-
-        with open(path) as file:
-            return json.load(file)
