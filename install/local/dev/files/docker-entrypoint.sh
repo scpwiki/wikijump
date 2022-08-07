8387a882f977b6777519bde69d2378727b65f226
Ammon Smith <ammon.i.smith@gmail.com>
2022-08-06 20:18:46 -0400

    Handle unset $DATA_DIR.

--- install/local/dev/files/docker-entrypoint.sh
+++ install/local/dev/files/docker-entrypoint.sh
@@ -5,12 +5,17 @@ set -eu
 # Adapted from https://gist.github.com/cxcorp/f4eec6575a815edcb63731da93362504
 #
 
+# Set variables
+
+readonly data_dir="${DATA_DIR:-/data}"
 readonly api_address='localhost:9000'
 readonly console_address='localhost:9001'
 
 readonly mc_user="${MINIO_ROOT_USER:-minioadmin}"
 readonly mc_password="${MINIO_ROOT_PASSWORD:-minioadmin}"
 readonly mc_region="${MINIO_REGION_NAME:-us-east-1}"
+
+# Helper functions
 
 function wait_for_server() {
 	until curl -fs "http://$api_address" > /dev/null; do
@@ -36,7 +41,7 @@ function create_initial_buckets() {
 	IFS=' ' read -r -a buckets <<< "${INITIAL_BUCKETS:-}"
 
 	# Start server in background
-	minio server "$DATA_DIR" \
+	minio server "$data_dir" \
 		--quiet \
 		--address "$api_address" \
 		--console-address "$console_address" \
@@ -59,7 +64,7 @@ function create_initial_buckets() {
 
 create_initial_buckets
 
-exec minio server "$DATA_DIR" \
+exec minio server "$data_dir" \
 	--quiet \
 	--address "$api_address" \
 	--console-address "$console_address"
