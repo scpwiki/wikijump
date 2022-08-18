90ade9ca6f7c68e98fe50bc6ee9ad6d7a602c749
Ammon Smith <ammon.i.smith@gmail.com>
2022-08-18 17:58:03 -0400

    Remove unused styles field from proptest generation.

--- ftml/src/test/prop.rs
+++ ftml/src/test/prop.rs
@@ -397,14 +397,12 @@ fn arb_tree() -> impl Strategy<Value = SyntaxTree<'static>> {
 
     (
         proptest::collection::vec(element, 1..100),
-        proptest::collection::vec(cow!(".*"), 0..128),
         proptest::collection::vec(toc_heading, 0..2),
         proptest::collection::vec(footnote, 0..2),
     )
         .prop_map(
-            |(elements, styles, table_of_contents, footnotes)| SyntaxTree {
+            |(elements, table_of_contents, footnotes)| SyntaxTree {
                 elements,
-                styles,
                 table_of_contents,
                 footnotes,
             },
