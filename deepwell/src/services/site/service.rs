8779b9531b978bca475759b22750ef91acd5b43c
Yossipossi <yossipossi770@gmail.com>
2022-09-04 21:26:29 -0700

    create() and check_conflicts() for SiteService
    Likely contains issues but I'll update when I iron out the kinks

--- deepwell/src/services/site/service.rs
+++ deepwell/src/services/site/service.rs
@@ -18,6 +18,8 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+use wikidot_normalize::normalize;
+
 use super::prelude::*;
 use crate::models::site::{self, Entity as Site, Model as SiteModel};
 
@@ -25,6 +27,37 @@ use crate::models::site::{self, Entity as Site, Model as SiteModel};
 pub struct SiteService;
 
 impl SiteService {
+    pub async fn create(
+        ctx: &ServiceContext<'_>,
+        CreateSite {
+            slug,
+            name,
+            subtitle,
+            description,
+            locale
+        }: CreateSite,
+    ) -> Result<CreateSiteOutput> {
+        let txn = ctx.transaction();
+
+        normalize(&mut slug);
+        Self::check_conflicts(ctx, &slug, "create").await?;
+
+        let model = site::ActiveModel {
+            slug: Set(slug),
+            name: Set(new),
+            subtitle: Set(Some(subtitle)),
+            description: Set(Some(description)),            
+            ..Default::default()
+        };
+        let site = model.insert(txn).await?;
+
+        Ok(CreateSiteOutput {
+            site_id: site.site_id,
+            slug,
+            default_page: site.default_page,
+        })
+    }
+
     #[inline]
     pub async fn exists(
         ctx: &ServiceContext<'_>,
@@ -64,5 +97,39 @@ impl SiteService {
         Self::get_optional(ctx, reference)
             .await?
             .ok_or(Error::NotFound)
+    }
+
+    /// Checks to see if a site already exists at the slug specified.
+    ///
+    /// If so, this method fails with `Error::Conflict`. Otherwise it returns nothing.
+    async fn check_conflicts(
+        ctx: &ServiceContext<'_>,
+        slug: &str,
+        action: &str,
+    ) -> Result<()> {
+        let txn = ctx.transaction();
+
+        let result = Site::find()
+            .filter(
+                Condition::all()
+                    .add(site::Column::SiteId.eq(site_id))
+                    .add(site::Column::Slug.eq(slug))
+                    .add(site::Column::DeletedAt.is_null()),
+            )
+            .one(txn)
+            .await?;
+
+        match result {
+            None => Ok(()),
+            Some(site) => {
+                tide::log::error!(
+                    "Site with slug '{}' already exists, cannot {}",
+                    slug,
+                    action,
+                );
+
+                Err(Error::Conflict)
+            }
+        }
     }
 }
