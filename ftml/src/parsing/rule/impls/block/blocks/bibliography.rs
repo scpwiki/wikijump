55c2f3a3f742a267def5a5b7f8a1cf99bcbb8463
Ammon Smith <ammon.i.smith@gmail.com>
2022-09-05 03:29:30 -0400

    Add initial BLOCK_BIBLIOGRAPHY.
    Also improve panic message.

---
+++ ftml/src/parsing/rule/impls/block/blocks/bibliography.rs
@@ -0,0 +1,45 @@
+/*
+ * parsing/rule/impls/block/blocks/bibliography.rs
+ *
+ * ftml - Library to parse Wikidot text
+ * Copyright (C) 2019-2022 Wikijump Team
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+use super::prelude::*;
+
+pub const BLOCK_BIBLIOGRAPHY: BlockRule = BlockRule {
+    name: "block-bibliography",
+    accepts_names: &["bibliography"],
+    accepts_star: false,
+    accepts_score: false,
+    accepts_newlines: true,
+    parse_fn,
+};
+
+fn parse_fn<'r, 't>(
+    parser: &mut Parser<'r, 't>,
+    name: &'t str,
+    flag_star: bool,
+    flag_score: bool,
+    in_head: bool,
+) -> ParseResult<'r, 't, Elements<'t>> {
+    info!("Parsing bibliography block (name '{name}', in-head {in_head}, score {flag_score})");
+    assert!(!flag_star, "Bibliography doesn't allow star flag");
+    assert!(!flag_score, "Bibliography doesn't allow score flag");
+    assert_block_name(&BLOCK_BIBLIOGRAPHY, name);
+
+    todo!()
+}
