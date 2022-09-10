82caeeee9da2701d39cf96851528e13eff262a2f
Ammon Smith <ammon.i.smith@gmail.com>
2022-09-09 21:51:55 -0400

    Delete old models, if present.

--- deepwell/scripts/generate-models.sh
+++ deepwell/scripts/generate-models.sh
@@ -4,6 +4,9 @@ set -eu
 # Switch to service root
 cd "${0%/*}/.."
 
+# Delete old models, if present
+[[ -d src/models ]] && rm -r src/models
+
 # Generate models
 sea-orm-cli generate entity \
 	-u postgres://wikijump:wikijump@localhost/wikijump \
