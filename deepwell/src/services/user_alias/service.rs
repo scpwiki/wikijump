252f08d1359000c8762de7ee5ef5cb8a090f588d
Ammon Smith <ammon.i.smith@gmail.com>
2022-09-30 18:45:16 -0400

    Remove temporary warning suppression.

--- deepwell/src/services/user_alias/service.rs
+++ deepwell/src/services/user_alias/service.rs
@@ -28,7 +28,6 @@ use crate::web::Reference;
 pub struct UserAliasService;
 
 impl UserAliasService {
-    #[allow(dead_code)] // TODO
     pub async fn create(
         ctx: &ServiceContext<'_>,
         input: CreateUserAlias,
