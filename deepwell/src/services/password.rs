242c009e10fb8ec60870ed1ea05ea9c2b48b32bb
Ammon Smith <ammon.i.smith@gmail.com>
2022-10-26 02:51:25 -0400

    Add PasswordService::verify().
    Also has verify_internal(), and the delay in between failed requests
    (0.1 seconds, or 100 milliseconds).

--- deepwell/src/services/password.rs
+++ deepwell/src/services/password.rs
@@ -20,17 +20,30 @@
 
 use super::prelude::*;
 use crate::utils::assert_is_csprng;
-use argon2::{password_hash::SaltString, Argon2, PasswordHasher};
+use argon2::{
+    password_hash::SaltString, Argon2, PasswordHash, PasswordHasher, PasswordVerifier,
+};
+use async_std::task;
 use rand::thread_rng;
+use std::time::Duration;
+
+/// Time to sleep in between failed authentication requests.
+const AUTHENTICATION_FAILURE_DELAY: Duration = Duration::from_millis(100);
 
 #[derive(Debug)]
 pub struct PasswordService;
 
 impl PasswordService {
+    /// Produces a new password hash from the input string.
+    ///
+    /// Generates a salt securely and performs Argon-2 hashing
+    /// and yields a string in PHC format.
     pub fn new_hash(password: &str) -> Result<String> {
+        // Create and verify CSPRNG
         let mut rng = thread_rng();
         assert_is_csprng(&rng);
 
+        // Create Argon-2 context, salt, and then hash the password
         let argon2 = Argon2::default();
         let salt = SaltString::generate(&mut rng);
         let hash = argon2
@@ -38,5 +51,51 @@ impl PasswordService {
             .to_string();
 
         Ok(hash)
+    }
+
+    /// Verifies that the inputted password matches the provided password hash.
+    ///
+    /// The password hash is expected to be in PHC format.
+    ///
+    /// On failure, the method will sleep a bit to avoid brute-forcing.
+    pub async fn verify(password: &str, hash: &str) -> Result<()> {
+        tide::log::info!("Attempting to verify password");
+        let result = Self::verify_internal(password, hash);
+
+        match result {
+            Ok(()) => Ok(()),
+            Err(error) => {
+                match error {
+                    // Simply the wrong password
+                    Error::InvalidAuthentication => {
+                        tide::log::warn!("Invalid password entered, verification failed");
+                    }
+
+                    // Some kind of server error
+                    _ => {
+                        tide::log::error!(
+                            "Unexpected error while verifying password: {error}",
+                        );
+                    }
+                }
+
+                // Delay a bit on failure to prevent brute-force attacks.
+                task::sleep(AUTHENTICATION_FAILURE_DELAY).await;
+
+                // Always return the same error for authentication methods,
+                // to not expose internal state to an adversary.
+                Err(Error::InvalidAuthentication)
+            }
+        }
+    }
+
+    fn verify_internal(password: &str, hash: &str) -> Result<()> {
+        // Parse PHC string
+        let hash = PasswordHash::new(hash)?;
+
+        // Create Argon-2 context, then verify the password
+        let argon2 = Argon2::default();
+        argon2.verify_password(password.as_bytes(), &hash)?;
+        Ok(())
     }
 }
