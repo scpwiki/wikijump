e9d4fea139ba814e721f671f92374eba8f84c24f
Ammon Smith <ammon.i.smith@gmail.com>
2022-11-24 22:11:13 -0700

    Add TODO to test.ts

--- framerail/tests/test.ts
+++ framerail/tests/test.ts
@@ -1,6 +1,7 @@
 import { expect, test } from '@playwright/test';
 
 test('index page has expected h1', async ({ page }) => {
-	await page.goto('/');
-	expect(await page.textContent('h1')).toBe('Welcome to SvelteKit');
+  // TODO
+  await page.goto('/');
+  expect(await page.textContent('h1')).toBe('Welcome to SvelteKit');
 });
