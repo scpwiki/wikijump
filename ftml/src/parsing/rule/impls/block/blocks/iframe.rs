060cd91b85b0bc22e14394df9a76f0eddace49b2
Ammon Smith <ammon.i.smith@gmail.com>
2022-12-11 17:24:10 -0500

    Log URL in warning.

--- ftml/src/parsing/rule/impls/block/blocks/iframe.rs
+++ ftml/src/parsing/rule/impls/block/blocks/iframe.rs
@@ -44,7 +44,7 @@ fn parse_fn<'r, 't>(
 
     let (url, arguments) = parser.get_head_name_map(&BLOCK_IFRAME, in_head)?;
     if !is_url(url) {
-        warn!("Iframe block references non-URL");
+        warn!("Iframe block references non-URL: {url}");
         return Err(parser.make_err(ParseErrorKind::BlockMalformedArguments));
     }
 
