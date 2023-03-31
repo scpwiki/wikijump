b8e9dfdb55083d3688427a25bec2233b52fb3db9
Emmie Maeda <emmie.maeda@gmail.com>
2023-03-31 15:34:11 -0400

    Export to port 3000.
    We need to add an nginx container.

--- framerail/vite.config.ts
+++ framerail/vite.config.ts
@@ -2,6 +2,11 @@ import { sveltekit } from "@sveltejs/kit/vite"
 import type { UserConfig } from "vite"
 
 const config: UserConfig = {
+  server: {
+    host: '::',
+    port: 3000,
+    strictPort: true
+  },
   plugins: [sveltekit()]
 }
 
