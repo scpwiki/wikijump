5b51a9d68b01ee099864678cac6c3ec1d354b44f
Emmie Maeda <emmie.maeda@gmail.com>
2023-04-08 22:35:29 -0400

    Remove try_from* methods from Reference.

--- deepwell/src/web/reference.rs
+++ deepwell/src/web/reference.rs
@@ -23,10 +23,7 @@
 //! For instance, a page can be referenced via its ID, or in the context of a site,
 //! via its page slug.
 
-use crate::api::ApiRequest;
 use std::borrow::Cow;
-use std::convert::TryFrom;
-use tide::{Error, StatusCode};
 
 #[derive(Serialize, Deserialize, Debug, Clone, Hash, PartialEq, Eq)]
 #[serde(untagged)]
@@ -45,37 +42,6 @@ pub enum Reference<'a> {
     Slug(Cow<'a, str>),
 }
 
-impl<'a> Reference<'a> {
-    pub fn try_from_fields_key(
-        req: &'a ApiRequest,
-        value_type_key: &str,
-        value_key: &str,
-    ) -> Result<Self, Error> {
-        let value_type = req.param(value_type_key)?;
-        let value = req.param(value_key)?;
-
-        Reference::try_from_fields(value_type, value)
-    }
-
-    pub fn try_from_fields(value_type: &str, value: &'a str) -> Result<Self, Error> {
-        match value_type {
-            "slug" => {
-                tide::log::debug!("Reference via slug, {value}");
-                Ok(Reference::Slug(Cow::Borrowed(value)))
-            }
-            "id" => {
-                tide::log::debug!("Reference via ID, {value}");
-                let id = value.parse()?;
-                Ok(Reference::Id(id))
-            }
-            _ => Err(Error::from_str(
-                StatusCode::BadRequest,
-                "May only specify object by 'id' or 'slug'",
-            )),
-        }
-    }
-}
-
 impl From<i64> for Reference<'static> {
     #[inline]
     fn from(id: i64) -> Reference<'static> {
@@ -87,14 +53,5 @@ impl<'a> From<&'a str> for Reference<'a> {
     #[inline]
     fn from(slug: &'a str) -> Reference<'a> {
         Reference::Slug(Cow::Borrowed(slug))
-    }
-}
-
-impl<'a> TryFrom<&'a ApiRequest> for Reference<'a> {
-    type Error = Error;
-
-    #[inline]
-    fn try_from(req: &'a ApiRequest) -> Result<Reference<'a>, Error> {
-        Reference::try_from_fields_key(req, "type", "id_or_slug")
     }
 }
