3c1a027083f90376b9b986e77941770189165d56
Emmie Maeda <emmie.maeda@gmail.com>
2023-04-10 22:44:29 -0400

    Add serde rename camelCase to all table models.

--- deepwell/src/models/site_alias.rs
+++ deepwell/src/models/site_alias.rs
@@ -4,6 +4,7 @@ use sea_orm::entity::prelude::*;
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, PartialEq, DeriveEntityModel, Eq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
 #[sea_orm(table_name = "site_alias")]
 pub struct Model {
     #[sea_orm(primary_key)]
