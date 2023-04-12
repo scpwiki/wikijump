f22ad6bc2a89749582b57defc748c8b9c7f00676
Emmie Maeda <emmie.maeda@gmail.com>
2023-04-12 01:43:28 -0400

    Remove unused items in hash/text.rs

--- deepwell/src/hash/text.rs
+++ deepwell/src/hash/text.rs
@@ -18,9 +18,7 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-use arraystring::ArrayString;
 use tiny_keccak::{Hasher, KangarooTwelve};
-use typenum::U32;
 
 /// The expected length of a text hash digest.
 ///
@@ -30,12 +28,6 @@ pub const TEXT_HASH_LENGTH: usize = 16;
 /// The array type for a text hash digest;
 pub type TextHash = [u8; 16];
 
-/// The stack string type for a hex representation of a text hash.
-///
-/// Because it is hexadecimal, it must be double the size of the
-/// actual byte buffer it represents.
-pub type TextHexHash = ArrayString<U32>;
-
 /// Produces a byte array containing the KangaroTwelve hash for the given data.
 pub fn k12_hash(data: &[u8]) -> TextHash {
     let mut bytes = [0; 16];
@@ -43,20 +35,4 @@ pub fn k12_hash(data: &[u8]) -> TextHash {
     hasher.update(data);
     hasher.finalize(&mut bytes);
     bytes
-}
-
-/// Converts the given SHA-512 hash into a hex array string.
-pub fn text_hash_to_hex(hash: &[u8]) -> TextHexHash {
-    debug_assert_eq!(
-        hash.len(),
-        TEXT_HASH_LENGTH,
-        "KangaroTwelve hash buffer of incorrect length",
-    );
-
-    let mut hex_bytes = [0; 32];
-
-    hex::encode_to_slice(hash, &mut hex_bytes)
-        .expect("Encoding hash to hex slice failed");
-
-    ArrayString::from_utf8(hex_bytes).expect("Encoded hash was not UTF-8")
 }
