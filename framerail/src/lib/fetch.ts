81aa5740af75d207d6fd111640233967f9dc322d
Emmie Maeda <emmie.maeda@gmail.com>
2023-04-13 23:56:55 -0400

    Create lib/fetch.rs

---
+++ framerail/src/lib/fetch.ts
@@ -0,0 +1,16 @@
+// Wrapper around fetch() to provide timeouts.
+
+export const DEFAULT_TIMEOUT = 2000;
+
+export function wjfetch(url, options = {}) {
+  let timeout = DEFAULT_TIMEOUT;
+  if (options.timeout) {
+    timeout = options.timeout;
+    delete options.timeout;
+  }
+
+  return fetch(
+    url,
+    { signal: AbortSignal.timeout(timeout), ...options },
+  );
+}
