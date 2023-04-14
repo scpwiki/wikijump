2ff0b4fa86303a2f28bc6b85ac3b313fc5287cd5
Emmie Maeda <emmie.maeda@gmail.com>
2023-04-14 00:07:01 -0400

    Use wjfetch in deepwell.server.ts

--- framerail/src/lib/deepwell.server.ts
+++ framerail/src/lib/deepwell.server.ts
@@ -1,11 +1,13 @@
 // TODO refactor into proper TS service
 
+import { wjfetch } from '$lib/fetch.ts';
+
 const DEEPWELL_HOST = process.env.DEEPWELL_HOST || 'localhost';
 const DEEPWELL_PORT = 2747;
 const DEEPWELL_ROUTE = `http://${DEEPWELL_HOST}:${DEEPWELL_PORT}/api/trusted`;
 
 export async function ping(): void {
-  const response = await fetch(`${DEEPWELL_ROUTE}/ping`);
+  const response = await wjfetch(`${DEEPWELL_ROUTE}/ping`);
   if (!response.ok) {
     throw new Error("Cannot ping DEEPWELL!")
   }
