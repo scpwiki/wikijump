7d928be199d0c8c1d7524848b5449f78b304decf
Zokhoi <20432565+Zokhoi@users.noreply.github.com>
2023-09-10 01:36:35 +0800

    Move slug page to its own component
    [WJ-1179] Add svelte component for page view

--- framerail/src/routes/[slug]/[...extra]/+page.svelte
+++ framerail/src/routes/[slug]/[...extra]/+page.svelte
@@ -1,30 +1,6 @@
 <script lang="ts">
   export let data
+  import Page from "./page.svelte"
 </script>
 
-<h1>UNTRANSLATED:Loaded slug page</h1>
-<p class="spin-yay">UNTRANSLATED:This is some other page on the site</p>
-<p>UNTRANSLATED:Response <textarea class="debug">{JSON.stringify(data)}</textarea></p>
-
-{@html data.compiledHtml}
-
-<style global lang="scss">
-  @keyframes spin {
-    from {
-      transform: rotate(0deg);
-    }
-    to {
-      transform: rotate(360deg);
-    }
-  }
-
-  .spin-yay {
-    display: inline-block;
-    animation: spin 2s linear infinite;
-  }
-
-  .debug {
-    width: 80vw;
-    height: 60vh;
-  }
-</style>
+<Page data={data} />
---
+++ framerail/src/routes/[slug]/[...extra]/page.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  export let data
+</script>
+
+<h1>UNTRANSLATED:Loaded page</h1>
+<p class="spin-yay">
+  UNTRANSLATED:This is a generic page renderer loaded as a component.
+</p>
+<p>UNTRANSLATED:Response <textarea class="debug">{JSON.stringify(data, null, 2)}</textarea></p>
+
+{@html data.compiledHtml}
+
+<style global lang="scss">
+  @keyframes spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+
+  .spin-yay {
+    display: inline-block;
+    animation: spin 2s linear infinite;
+  }
+
+  .debug {
+    width: 80vw;
+    height: 60vh;
+  }
+</style>
