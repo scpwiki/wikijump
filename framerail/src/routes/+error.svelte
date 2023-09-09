7cd73320e803eb71c9ff9470f3ddd03e6b4b07f7
Zokhoi <20432565+Zokhoi@users.noreply.github.com>
2023-09-10 01:37:20 +0800

    Beautify debug output

--- framerail/src/routes/+error.svelte
+++ framerail/src/routes/+error.svelte
@@ -4,7 +4,7 @@
 
 <h1>UNTRANSLATED:Svelte Error</h1>
 
-<p><textarea class="debug">{JSON.stringify($page)}</textarea></p>
+<p><textarea class="debug">{JSON.stringify($page, null, 2)}</textarea></p>
 
 <!--
 Use svelte-switch-case package with {#switch data.view}
@@ -22,3 +22,10 @@ as soon as we can figure out prettier support for it.
 {:else}
   UNTRANSLATED:Fallback error, something really went wrong
 {/if}
+
+<style global lang="scss">
+  .debug {
+    width: 80vw;
+    height: 60vh;
+  }
+</style>
