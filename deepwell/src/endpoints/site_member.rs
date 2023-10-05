5d49ae01d53e7c7353675c6bebd33851be5540d8
Emmie Maeda <emmie.maeda@gmail.com>
2023-10-05 02:02:52 -0400

    Update endpoint for new interaction scheme.

--- deepwell/src/endpoints/site_member.rs
+++ deepwell/src/endpoints/site_member.rs
@@ -21,41 +21,17 @@
 use super::prelude::*;
 use crate::services::interaction::{
     InteractionObject, InteractionReference, InteractionType, SiteMemberAccepted,
-    SiteMemberData,
+    SiteMemberData, CreateSiteMember,
 };
-
-#[derive(Serialize, Deserialize, Debug, Clone)]
-#[serde(rename_all = "camelCase")]
-pub struct SiteMembership {
-    pub site_id: i64,
-    pub user_id: i64,
-}
-
-#[derive(Deserialize, Debug, Clone)]
-#[serde(rename_all = "camelCase")]
-pub struct CreateSiteMembership {
-    pub site_id: i64,
-    pub site_user_id: i64,
-    pub acting_user_id: i64,
-    pub accepted: SiteMemberAccepted,
-}
-
-#[derive(Deserialize, Debug, Clone)]
-#[serde(rename_all = "camelCase")]
-pub struct DeleteSiteMembership {
-    pub site_id: i64,
-    pub site_user_id: i64,
-    pub acting_user_id: i64,
-}
 
 pub async fn membership_retrieve(mut req: ApiRequest) -> ApiResponse {
     let txn = req.database().begin().await?;
     let ctx = ServiceContext::new(&req, &txn);
 
-    let SiteMembership { site_id, user_id } = req.body_json().await?;
-    let output = InteractionService::get_site_member(&ctx, site_id, user_id).await?;
-    txn.commit().await?;
+    let input: GetSiteMember = req.body_json().await?;
+    let output = InteractionService::get_site_member(&ctx, input).await?;
 
+    txn.commit().await?;
     build_json_response(&output, StatusCode::Ok)
 }
 
@@ -63,21 +39,8 @@ pub async fn membership_put(mut req: ApiRequest) -> ApiResponse {
     let txn = req.database().begin().await?;
     let ctx = ServiceContext::new(&req, &txn);
 
-    let CreateSiteMembership {
-        site_id,
-        site_user_id,
-        acting_user_id,
-        accepted,
-    } = req.body_json().await?;
-
-    let output = InteractionService::add_site_member(
-        &ctx,
-        site_id,
-        site_user_id,
-        acting_user_id,
-        &SiteMemberData { accepted },
-    )
-    .await?;
+    let input: CreateSiteMember = req.body_json().await?;
+    let output = InteractionService::add_site_member(&ctx, input).await?;
 
     txn.commit().await?;
     build_json_response(&output, StatusCode::Created)
@@ -87,22 +50,8 @@ pub async fn membership_delete(mut req: ApiRequest) -> ApiResponse {
     let txn = req.database().begin().await?;
     let ctx = ServiceContext::new(&req, &txn);
 
-    let DeleteSiteMembership {
-        site_id,
-        site_user_id,
-        acting_user_id,
-    } = req.body_json().await?;
-
-    let output = InteractionService::remove(
-        &ctx,
-        InteractionReference::Relationship {
-            interaction_type: InteractionType::SiteMember,
-            dest: InteractionObject::Site(site_id),
-            from: InteractionObject::User(site_user_id),
-        },
-        acting_user_id,
-    )
-    .await?;
+    let input: DeleteSiteMember = req.body_json().await?;
+    let output = InteractionService::remove_site_member(&ctx, input).await?;
 
     txn.commit().await?;
     build_json_response(&output, StatusCode::Ok)
