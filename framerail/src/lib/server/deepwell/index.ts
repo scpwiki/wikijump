fe42d397d06c2e369817bc26d41e1bc593e92cce
Emmie Maeda <emmie.maeda@gmail.com>
2023-10-17 01:11:47 -0400

    Update deepwell index to ping new JSON-RPC server.

--- framerail/src/lib/server/deepwell/index.ts
+++ framerail/src/lib/server/deepwell/index.ts
@@ -1,23 +1,15 @@
 // TODO refactor into proper TS service
 
-import { wjfetch } from "$lib/fetch.ts"
+import * as jsonrpc from "json-rpc-client"
 
 const DEEPWELL_HOST = process.env.DEEPWELL_HOST || "localhost"
 const DEEPWELL_PORT = 2747
-const DEEPWELL_ROUTE = `http://${DEEPWELL_HOST}:${DEEPWELL_PORT}/api/trusted`
-
-export function wellfetch(path, options = {}) {
-  if (!path.startsWith("/")) {
-    throw new Error(`DEEPWELL path does not start with /: ${path}`)
-  }
-
-  const url = `${DEEPWELL_ROUTE}${path}`
-  return wjfetch(url, options)
-}
+const DEEPWELL_CLIENT = new jsonrpc({ host: DEEPWELL_HOST, port: DEEPWELL_PORT })
+DEEPWELL_CLIENT.connect()
 
 export async function ping(): void {
-  const response = await wellfetch("/ping")
-  if (!response.ok) {
+  const reply = DEEPWELL_CLIENT.send("ping")
+  if (reply.error) {
     throw new Error("Cannot ping DEEPWELL!")
   }
 }
