113d84a99103724857820625a672895a0c977d09
Zokhoi <20432565+Zokhoi@users.noreply.github.com>
2023-11-03 23:46:23 +0800

    Remove spinning text

--- framerail/src/routes/[slug]/[...extra]/page.svelte
+++ framerail/src/routes/[slug]/[...extra]/page.svelte
@@ -105,9 +105,6 @@
 </script>
 
 <h1>UNTRANSLATED:Loaded page</h1>
-<p class="spin-yay">
-  UNTRANSLATED:This is a generic page renderer loaded as a component.
-</p>
 <p>
   UNTRANSLATED:Response <textarea class="debug">{JSON.stringify($page, null, 2)}</textarea
   >
@@ -351,20 +348,6 @@
 {/if}
 
 <style global lang="scss">
-  @keyframes spin {
-    from {
-      transform: rotate(0deg);
-    }
-    to {
-      transform: rotate(360deg);
-    }
-  }
-
-  .spin-yay {
-    display: inline-block;
-    animation: spin 2s linear infinite;
-  }
-
   .debug {
     width: 80vw;
     height: 60vh;
