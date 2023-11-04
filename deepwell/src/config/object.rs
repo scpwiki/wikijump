70e6c65a36fa3916f68e75cda790dff918c01016
Emmie Maeda <emmie.maeda@gmail.com>
2023-11-04 14:47:39 -0400

    Add rerender_skip to Config.

--- deepwell/src/config/object.rs
+++ deepwell/src/config/object.rs
@@ -148,6 +148,17 @@ pub struct Config {
     /// Maximum run time for a render request.
     pub render_timeout: StdDuration,
 
+    /// In what circumstances a page rerender should be skipped.
+    ///
+    /// A list of rerender job depths and durations. If any item in this
+    /// list matches, then the rerender is skipped and subsequent rerender
+    /// jobs are not proliferated.
+    ///
+    /// The condition means that the current job depth is equal or greater
+    /// than the specified depth value, _and_ that the page revision was
+    /// last updated in the duration value specified.
+    pub rerender_skip: Vec<(u32, StdDuration)>,
+
     /// Prefix for "special pages". Default: `_`
     pub special_page_prefix: String,
 
