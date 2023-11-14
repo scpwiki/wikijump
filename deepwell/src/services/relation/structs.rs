9d453239d6509fe1e23025b253fa2eba77ec71e3
Emmie Maeda <emmie.maeda@gmail.com>
2023-11-13 19:17:30 -0500

    Run cargo fmt.

--- deepwell/src/services/relation/structs.rs
+++ deepwell/src/services/relation/structs.rs
@@ -148,7 +148,7 @@ impl RelationType {
     /// of `(dest_type, from_type, relation_value)`.
     pub fn value(self) -> &'static str {
         match self {
-            RelationType::SiteUser => "site-user",  // for the 'site' user_type
+            RelationType::SiteUser => "site-user", // for the 'site' user_type
             RelationType::SiteBan => "ban",
             RelationType::SiteApplication => "application",
             RelationType::SiteMember => "member",
