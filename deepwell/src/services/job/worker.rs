b4708de20682e3836a0a0a8b3b14f804bf8bc837
Emmie Maeda <emmie.maeda@gmail.com>
2024-08-08 21:16:44 -0400

    Add both transactions to context.

--- deepwell/src/services/job/worker.rs
+++ deepwell/src/services/job/worker.rs
@@ -171,7 +171,7 @@ impl JobWorker {
         debug!("Received job from queue: {job:?}");
         trace!("Setting up ServiceContext for job processing");
         let txn = self.state.database_seaorm.begin().await?;
-        let ctx = &ServiceContext::new(&self.state, &txn);
+        let ctx = &ServiceContext::new(&self.state, &txn, todo!());
 
         trace!("Beginning job processing");
         let next = match job {
