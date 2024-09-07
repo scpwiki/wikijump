d730b25abc7607973f94b3255c1492f9b4ca6909
Zokhoi <20432565+Zokhoi@users.noreply.github.com>
2024-09-08 01:03:23 +0800

    Add redirect for invalid special page

---
+++ framerail/src/routes/[x+2d]/+page.svelte
@@ -0,0 +1,7 @@
+<!-- /-/ should not be a valid page, therefore redirects to home page -->
+
+<script lang="ts">
+  import Redirect from "./[slug]/redirect.svelte"
+</script>
+
+<Redirect />
---
+++ framerail/src/routes/[x+2d]/[slug]/redirect.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { goto } from "$app/navigation"
+  import { onMount } from "svelte"
+
+  onMount(() => {
+    goto("/", {
+      replaceState: true
+    })
+  })
+</script>
