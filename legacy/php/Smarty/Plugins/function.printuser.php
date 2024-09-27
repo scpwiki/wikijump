561a2f62c988142bcfb98cf783d2cb2fd7d6efdd
Emmie Maeda <emmie.maeda@gmail.com>
2024-09-27 06:11:16 -0400

    Delete unnecessary legacy php/ files.

--- legacy/php/Smarty/Plugins/function.printuser.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Wikidot\Utils\WDRenderUtils;
-
-function smarty_function_printuser($params, &$smarty)
-{
-    $user = $params['user'];
-    return WDRenderUtils::renderUser($user, $params);
-}
