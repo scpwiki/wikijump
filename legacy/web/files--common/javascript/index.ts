b4b0035c72b08ccb64c7c7d3ba3b4e70aff2b62b
Emmie Maeda <emmie.maeda@gmail.com>
2024-09-27 06:23:05 -0400

    Delete unnecessary legacy client JS.

--- legacy/web/files--common/javascript/index.ts
+++
@@ -1,9 +0,0 @@
-import Wikijump from "@/javascript/Wikijump";
-import OZONE from "@/javascript/OZONE";
-
-window.Wikijump = Wikijump;
-window.OZONE = OZONE;
-
-Wikijump.page.init();
-
-export { Wikijump, OZONE };
--- legacy/web/files--common/javascript/yahooui/combine.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cat yahoo-min.js event-min.js dom-min.js dragdrop-min.js connection-min.js autocomplete-min.js ../moofx/prototype.lite.js ../moofx/moo.fx.js ../moofx/moo.fx.pack.js > combined.js
