0ae012570960bc4128aee12b8ccc8b2449fc3e65
Emmie Smith <emmie.maeda@gmail.com>
2026-05-27 00:47:52 -0400

    Handle User in MessageService.

--- deepwell/src/services/message/service.rs
+++ deepwell/src/services/message/service.rs
@@ -404,7 +404,8 @@ impl MessageService {
             .or_raise(make_error)?;
 
             // If recipient is a site user, then forward to corresponding site staff.
-            let user = UserService::get(ctx, Reference::Id(recipient_user_id))
+            // It is only possible to send messages to Wikijump users.
+            let user = UserService::get_wikijump(ctx, Reference::Id(recipient_user_id))
                 .await
                 .or_raise(make_error)?;
 
