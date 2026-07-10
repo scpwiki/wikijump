ddc82d4a800431e28938a86385fa17365b2f22e9
Rokurolize <1701388+Rokurolize@users.noreply.github.com>
2026-07-10 23:00:10 +0900

    fix: pass public profile data to user page

--- framerail/src/routes/[x+2d]/user/[slug]/+page.svelte
+++ framerail/src/routes/[x+2d]/user/[slug]/+page.svelte
@@ -1,9 +1,24 @@
 <script lang="ts">
   import Page from "./page.svelte"
 
+  import type { userEditSchema } from "$lib/server/load/user"
   import type { PageProps } from "./$types"
+  import type { InferOutput } from "valibot"
 
   let { data }: PageProps = $props()
+
+  const userData: InferOutput<typeof userEditSchema> = {
+    name: data.user?.name ?? "",
+    realName: data.user?.real_name ?? "",
+    email: data.user?.email ?? "",
+    gender: data.user?.gender ?? "",
+    birthday: data.user?.birthday ?? "",
+    location: data.user?.location ?? "",
+    website: data.user?.website ?? "",
+    userPage: data.user?.user_page ?? "",
+    biography: data.user?.biography ?? "",
+    locales: data.user?.locales?.join(" ") ?? ""
+  }
 </script>
 
-<Page {data} />
+<Page {data} {userData} />
