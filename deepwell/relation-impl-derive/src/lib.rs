3ab212cefbec5523f4fccf35a67123acda2c9bbd
Emmie Smith <git@emmie.tech>
2026-07-25 18:52:00 -0400

    First compiling version of macro crate.

--- deepwell/relation-impl-derive/src/lib.rs
+++ deepwell/relation-impl-derive/src/lib.rs
@@ -1,8 +1,18 @@
+mod case;
+mod expand;
+mod parse;
+
+use self::expand::expand_stream;
+use self::parse::RelationSettings;
 use proc_macro::TokenStream;
+use syn::parse::Parse;
+use syn::parse_macro_input;
 
 #[proc_macro]
 pub fn impl_relation(stream: TokenStream) -> TokenStream {
-    "fn answer() -> u32 { 42 }".parse().unwrap()
+    let mut input = parse_macro_input!(stream as RelationSettings);
+    let generated = expand_stream(input);
+    generated
 }
 
 // TODO
