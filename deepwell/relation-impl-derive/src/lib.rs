c2e0131757acd08acfa27d211300b0d17f0840b9
Emmie Smith <git@emmie.tech>
2026-07-25 21:13:38 -0400

    Suppress some warnings.

--- deepwell/relation-impl-derive/src/lib.rs
+++ deepwell/relation-impl-derive/src/lib.rs
@@ -7,12 +7,11 @@ mod util;
 use self::expand::expand_stream;
 use self::parse::RelationSettings;
 use proc_macro::TokenStream;
-use syn::parse::Parse;
 use syn::parse_macro_input;
 
 #[proc_macro]
 pub fn impl_relation(stream: TokenStream) -> TokenStream {
-    let mut input = parse_macro_input!(stream as RelationSettings);
+    let input = parse_macro_input!(stream as RelationSettings);
     let generated = expand_stream(input);
     generated
 }
