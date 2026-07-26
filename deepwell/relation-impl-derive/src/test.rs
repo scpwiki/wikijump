011ec4fa4e3044e2db152969229868cbf4016ea9
Emmie Smith <git@emmie.tech>
2026-07-25 22:15:47 -0400

    Add another unit test case.

--- deepwell/relation-impl-derive/src/test.rs
+++ deepwell/relation-impl-derive/src/test.rs
@@ -1,6 +1,6 @@
 use crate::parse::RelationSettings;
 use crate::types::GenerateMethod;
-use syn::{Type, TypePath};
+use syn::Type;
 
 /// Validate that `RelationSettings` are being parsed as expected.
 #[test]
@@ -14,6 +14,27 @@ fn parse() {
     {
         let settings = parse_settings(
             "
+            name => Foo,
+            dest => first: User,
+            from => second: User,
+            define_struct => false
+            ",
+        );
+        assert_eq!(settings.relation_name.to_string(), "Foo");
+        assert_eq!(settings.field_name.to_string(), "foo");
+        assert_eq!(settings.dest.0.to_string(), "first");
+        assert!(matches!(settings.dest.1, Type::Path(_)));
+        assert_eq!(settings.from.0.to_string(), "second");
+        assert!(matches!(settings.from.1, Type::Path(_)));
+        assert!(settings.data_type.is_none());
+        assert_eq!(settings.create_fn, GenerateMethod::ImplPublic);
+        assert_eq!(settings.remove_fn, GenerateMethod::ImplPublic);
+        assert!(!settings.define_struct);
+    }
+
+    {
+        let settings = parse_settings(
+            "
             name => UserBlock,
             dest => blocked_user: User,
             from => blocking_user: User,
@@ -23,11 +44,13 @@ fn parse() {
         assert_eq!(settings.relation_name.to_string(), "UserBlock");
         assert_eq!(settings.field_name.to_string(), "user_block");
         assert_eq!(settings.dest.0.to_string(), "blocked_user");
-        assert!(matches!(settings.dest.1, Type::Path(TypePath { .. })));
+        assert!(matches!(settings.dest.1, Type::Path(_)));
         assert_eq!(settings.from.0.to_string(), "blocking_user");
-        assert!(matches!(settings.from.1, Type::Path(TypePath { .. })));
+        assert!(matches!(settings.from.1, Type::Path(_)));
+        assert!(matches!(settings.data_type, Some(Type::Path(_))));
         assert_eq!(settings.create_fn, GenerateMethod::ImplPublic);
         assert_eq!(settings.remove_fn, GenerateMethod::ImplPublic);
+        assert!(settings.define_struct);
     }
 }
 
