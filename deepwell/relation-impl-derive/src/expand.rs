59ea6b0bf7e1b4da2fe02d05687a76680b5a893c
Emmie Smith <git@emmie.tech>
2026-09-10 01:19:13 -0400

    Fix generation for create_fn => private_only with no metadata.

--- deepwell/relation-impl-derive/src/expand.rs
+++ deepwell/relation-impl-derive/src/expand.rs
@@ -261,16 +261,14 @@ fn generate_create_defs(
             }
         }
         None => {
-            if generate_pub_struct {
-                create_struct_def = Some(quote! {
-                    #[derive(Deserialize, Debug, Clone)]
-                    pub struct #create_struct {
-                        pub #dest_name: i64,
-                        pub #from_name: i64,
-                        pub created_by: i64,
-                    }
-                });
-            }
+            create_struct_def = Some(quote! {
+                #[derive(Deserialize, Debug, Clone)]
+                #fn_visibility struct #create_struct {
+                    pub #dest_name: i64,
+                    pub #from_name: i64,
+                    pub created_by: i64,
+                }
+            });
         }
     };
 
