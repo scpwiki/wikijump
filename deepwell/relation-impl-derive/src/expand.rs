ac7c6c569b153dfd837edb1e1438c743aeedb62b
Emmie Smith <git@emmie.tech>
2026-09-10 22:55:25 -0400

    Use generate_pub_struct for visibility.

--- deepwell/relation-impl-derive/src/expand.rs
+++ deepwell/relation-impl-derive/src/expand.rs
@@ -261,9 +261,15 @@ fn generate_create_defs(
             }
         }
         None => {
+            let vis = if generate_pub_struct {
+                public()
+            } else {
+                private()
+            };
+
             create_struct_def = Some(quote! {
                 #[derive(Deserialize, Debug, Clone)]
-                #fn_visibility struct #create_struct {
+                #vis struct #create_struct {
                     pub #dest_name: i64,
                     pub #from_name: i64,
                     pub created_by: i64,
